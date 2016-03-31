66071de2ad27103ba2e338cb33a8cd3cb3a27a3d
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-03-31 05:20:23 +0300

    Add file ring_hashlib.h

---
+++ include/ring_hashlib.h
@@ -0,0 +1,11 @@
+unsigned ring_hash(unsigned pid);
+unsigned ring_add_hash(unsigned char *key, int len);
+unsigned ring_xor_hash(unsigned char *key, int len);
+unsigned ring_rot_hash(unsigned char *key, int len);
+unsigned ring_djb_hash(unsigned char *key, int len);
+unsigned ring_sax_hash(unsigned char *key, int len);
+unsigned ring_fnv_hash(unsigned char *key, int len);
+unsigned ring_oat_hash(unsigned char *key, int len);
+unsigned ring_elf_hash(unsigned char *key, int len);
+unsigned ring_jen_hash(unsigned char *k, unsigned length, unsigned initval);
+unsigned int ring_murmur3_32(const char *key, unsigned int len, unsigned int seed) ;
