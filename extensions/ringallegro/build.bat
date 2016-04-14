feba3c9667905240dad27f8a2497113efe9b1cbd
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-04-14 23:21:43 +0300

    Add welcome.png

--- extensions/ringallegro/build.bat
+++ extensions/ringallegro/build.bat
@@ -1,4 +1,5 @@
 cls
+call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 cl /c ring_allegro.c -I"c:\allegro\include" -I"..\..\include"
 link ring_allegro.obj  ..\..\lib\ring.lib c:\allegro\lib\allegro_monolith.lib /DLL /OUT:..\..\bin\ring_allegro.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_allegro.obj
