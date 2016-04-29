ec24af4511a47a9ab62e67783cd5ef8a205faf54
Mahmoud Fayed <msfclipper@yahoo.com>
2016-04-29 17:18:46 +0300

    Add build.sh

---
+++ extensions/ringallegro/build.sh
@@ -0,0 +1,6 @@
+gcc -c -fpic ring_allegro.c -I $PWD/../../include
+gcc -shared -o $PWD/../../lib/libringallegro.so ring_allegro.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -lallegro -lallegro_image -lallegro_dialog -lallegro_ttf -lallegro_acodec
+
+ 
+
+
