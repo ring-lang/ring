0eaff883b149eadbde31c2e5e4d205360e64bb26
Mahmoud Fayed <msfclipper@yahoo.com>
2016-05-26 18:58:10 +0300

    Ring Notepad - Add run.sh to execute Ring applications in the application folder

---
+++ applications/rnote/run.sh
@@ -0,0 +1,8 @@
+# Simple script to execute Ring applications in the application folder
+# So the application can find files using relative path 
+# 2016, Mahmoud Fayed <msfclipper@yahoo.com>
+
+cd $(dirname "$1")
+ls
+ring $1
+
