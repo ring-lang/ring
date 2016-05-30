94d1f02121d68e3ac05fa045986df8043c8cb5d5
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-05-31 02:02:57 +0300

    Add Qt Library to installdep.sh

--- src/installdep.sh
+++ src/installdep.sh
@@ -1,1 +1,1 @@
-sudo apt-get install unixODBC unixODBC-dev libmysqlclient-dev libcurl4-gnutls-dev libssl-dev liballegro5-dev
+sudo apt-get install unixODBC unixODBC-dev libmysqlclient-dev libcurl4-gnutls-dev libssl-dev liballegro5-dev qtbase5-dev qtmobility-dev qtmultimedia5-dev 
