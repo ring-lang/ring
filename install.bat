bc974a77a43a0334edd536cf7bb1bf49ffb177ea
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-05-31 23:09:08 +0300

    Add install.bat

---
+++ install.bat
@@ -0,0 +1,10 @@
+# Simple Batch File to copy ring DLL files and libraries to the bin folder
+# 2016, Mahmoud Fayed <msfclipper@yahoo.com>
+
+
+copy lib\*.dll bin
+copy libdepwin\allegro\bin\*.dll bin
+copy libdepwin\libcurl\bin\*.dll bin
+copy libdepwin\mysql\lib\*.dll bin
+copy libdepwin\openssl\bin\*.dll bin
+copy C:\Qt\Qt5.5.1\5.5\mingw492_32\bin\*.dll bin
