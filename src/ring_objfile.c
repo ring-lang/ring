0bf818e396424f42781b3a08e47a751cbc5e57c4
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-07-10 18:26:24 +0300

    Ring Object File - Use Binary File - Store String Size.

--- src/ring_objfile.c
+++ src/ring_objfile.c
@@ -9,23 +9,23 @@ void ring_objfile_writefile ( RingState *pRingState )
 {
 	FILE *fObj;
 	/* Create File */
-	fObj = fopen("program.ringo" , "w+" );
-	fprintf( fObj , "# Ring Object File\n"  ) ;
-	fprintf( fObj , "# Version 1.0\n"  ) ;
+	fObj = fopen("program.ringo" , "w+b" );
+	fprintf( fObj , "# Ring Object File\r\n"  ) ;
+	fprintf( fObj , "# Version 1.0\r\n"  ) ;
 	/* Write Functions Lists */
-	fprintf( fObj , "# Functions List\n"  ) ;
+	fprintf( fObj , "# Functions List\r\n"  ) ;
 	ring_objfile_writelist(pRingState->pRingFunctionsMap,fObj);
 	/* Write Classes List */
-	fprintf( fObj , "# Classes List\n"  ) ;
+	fprintf( fObj , "# Classes List\r\n"  ) ;
 	ring_objfile_writelist(pRingState->pRingClassesMap,fObj);
 	/* Write Packages */
-	fprintf( fObj , "# Packages List\n"  ) ;
+	fprintf( fObj , "# Packages List\r\n"  ) ;
 	ring_objfile_writelist(pRingState->pRingPackagesMap,fObj);
 	/* Write Code */
-	fprintf( fObj , "# Program Code\n"  ) ;
+	fprintf( fObj , "# Program Code\r\n"  ) ;
 	ring_objfile_writelist(pRingState->pRingGenCode,fObj);
 	/* Close File */
-	fprintf( fObj , "# End of File\n"  ) ;
+	fprintf( fObj , "# End of File\r\n"  ) ;
 	fclose( fObj ) ;
 }
 
@@ -33,13 +33,15 @@ void ring_objfile_writelist ( List *pList,FILE *fObj )
 {
 	List *pList2  ;
 	int x,x2  ;
-	fprintf( fObj , "{\n"  ) ;
+	fprintf( fObj , "{\r\n"  ) ;
 	/* Write List Items */
 	for ( x = 1 ; x <= ring_list_getsize(pList) ; x++ ) {
 		pList2 = ring_list_getlist(pList,x);
 		for ( x2 = 1 ; x2 <= ring_list_getsize(pList2) ; x2++ ) {
 			if ( ring_list_isstring(pList2,x2) ) {
-				fprintf( fObj , "[S]%s#$!_EOS_!$#;" , ring_list_getstring(pList2,x2) ) ;
+				fprintf( fObj , "[S][%d]" , ring_list_getstringsize(pList2,x2) ) ;
+				fwrite( ring_list_getstring(pList2,x2) , 1 , ring_list_getstringsize(pList2,x2) , fObj );
+				fprintf( fObj , ";"  ) ;
 			}
 			else if ( ring_list_isint(pList2,x2) ) {
 				fprintf( fObj , "[I]%d;" , ring_list_getint(pList2,x2) ) ;
@@ -51,11 +53,11 @@ void ring_objfile_writelist ( List *pList,FILE *fObj )
 				fprintf( fObj , "[P]%p;" , (void *) ring_list_getpointer(pList2,x2) ) ;
 			}
 			else if ( ring_list_islist(pList2,x2) ) {
-				fprintf( fObj , "[L]\n"  ) ;
+				fprintf( fObj , "[L]\r\n"  ) ;
 				ring_objfile_writelist(ring_list_getlist(pList2,x2) ,fObj);
 			}
 		}
-		fprintf( fObj , "\n"  ) ;
+		fprintf( fObj , "\r\n"  ) ;
 	}
-	fprintf( fObj , "}\n"  ) ;
+	fprintf( fObj , "}\r\n"  ) ;
 }
