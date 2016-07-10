d0a0a5114e8619c8379e21f150104c2f4bdbe8aa
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-07-10 18:49:23 +0300

    Ring Object File - Separate items using new lines.

--- src/ring_objfile.c
+++ src/ring_objfile.c
@@ -37,28 +37,27 @@ void ring_objfile_writelist ( List *pList,FILE *fObj )
 	/* Write List Items */
 	for ( x = 1 ; x <= ring_list_getsize(pList) ; x++ ) {
 		pList2 = ring_list_getlist(pList,x);
-		fprintf( fObj , "[LI];\r\n"  ) ;
+		fprintf( fObj , "[LI]\r\n"  ) ;
 		for ( x2 = 1 ; x2 <= ring_list_getsize(pList2) ; x2++ ) {
 			if ( ring_list_isstring(pList2,x2) ) {
 				fprintf( fObj , "[S][%d]" , ring_list_getstringsize(pList2,x2) ) ;
 				fwrite( ring_list_getstring(pList2,x2) , 1 , ring_list_getstringsize(pList2,x2) , fObj );
-				fprintf( fObj , ";"  ) ;
+				fprintf( fObj , "\r\n"  ) ;
 			}
 			else if ( ring_list_isint(pList2,x2) ) {
-				fprintf( fObj , "[I]%d;" , ring_list_getint(pList2,x2) ) ;
+				fprintf( fObj , "[I]%d\r\n" , ring_list_getint(pList2,x2) ) ;
 			}
 			else if ( ring_list_isdouble(pList2,x2) ) {
-				fprintf( fObj , "[D]%f;" , ring_list_getdouble(pList2,x2) ) ;
+				fprintf( fObj , "[D]%f\r\n" , ring_list_getdouble(pList2,x2) ) ;
 			}
 			else if ( ring_list_ispointer(pList2,x2) ) {
-				fprintf( fObj , "[P]%p;" , (void *) ring_list_getpointer(pList2,x2) ) ;
+				fprintf( fObj , "[P]%p\r\n" , (void *) ring_list_getpointer(pList2,x2) ) ;
 			}
 			else if ( ring_list_islist(pList2,x2) ) {
 				fprintf( fObj , "[L]\r\n"  ) ;
 				ring_objfile_writelist(ring_list_getlist(pList2,x2) ,fObj);
 			}
 		}
-		fprintf( fObj , "\r\n"  ) ;
 	}
 	fprintf( fObj , "}\r\n"  ) ;
 }
