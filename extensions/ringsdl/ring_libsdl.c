07adf25a0290ba2ef3644422d080f45088c9d8da
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-08-17 19:00:44 +0300

    Update RingLibSDL - Add SDLNet_UDP_GetPeerAddress()

--- extensions/ringsdl/ring_libsdl.c
+++ extensions/ringsdl/ring_libsdl.c
@@ -15053,6 +15053,22 @@ RING_FUNC(ring_SDLNet_UDP_Unbind)
 		free(RING_API_GETCPOINTER(1,"UDPsocket"));
 }
 
+
+RING_FUNC(ring_SDLNet_UDP_GetPeerAddress)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETCPOINTER(SDLNet_UDP_GetPeerAddress(* (UDPsocket  *) RING_API_GETCPOINTER(1,"UDPsocket"), (int ) RING_API_GETNUMBER(2)),"IPaddress");
+	if (RING_API_ISCPOINTERNOTASSIGNED(1))
+		free(RING_API_GETCPOINTER(1,"UDPsocket"));
+}
+
 RING_DLL void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("sdl_rendercopy2",ring_SDL_RenderCopy2);
@@ -15615,6 +15631,7 @@ RING_DLL void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("sdlnet_udp_close",ring_SDLNet_UDP_Close);
 	ring_vm_funcregister("sdlnet_udp_bind",ring_SDLNet_UDP_Bind);
 	ring_vm_funcregister("sdlnet_udp_unbind",ring_SDLNet_UDP_Unbind);
+	ring_vm_funcregister("sdlnet_udp_getpeeraddress",ring_SDLNet_UDP_GetPeerAddress);
 	ring_vm_funcregister("sdl_new_sdl_assert_data",ring_sdl_new_sdl_assert_data);
 	ring_vm_funcregister("sdl_destroy_sdl_assert_data",ring_sdl_destroy_sdl_assert_data);
 	ring_vm_funcregister("sdl_new_sdl_version",ring_sdl_new_sdl_version);
