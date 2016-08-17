92758be5e40bc6ef46c9e47c68a711e237182dd1
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-08-17 19:03:38 +0300

    Update RingLibSDL - Add SDLNet_UDP_RecvV()

--- extensions/ringsdl/ring_libsdl.c
+++ extensions/ringsdl/ring_libsdl.c
@@ -15125,6 +15125,22 @@ RING_FUNC(ring_SDLNet_UDP_SendV)
 		free(RING_API_GETCPOINTER(1,"UDPsocket"));
 }
 
+
+RING_FUNC(ring_SDLNet_UDP_RecvV)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETNUMBER(SDLNet_UDP_RecvV(* (UDPsocket  *) RING_API_GETCPOINTER(1,"UDPsocket"),(UDPpacket *) RING_API_GETCPOINTER(2,"UDPpacket")));
+	if (RING_API_ISCPOINTERNOTASSIGNED(1))
+		free(RING_API_GETCPOINTER(1,"UDPsocket"));
+}
+
 RING_DLL void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("sdl_rendercopy2",ring_SDL_RenderCopy2);
@@ -15691,6 +15707,7 @@ RING_DLL void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("sdlnet_udp_send",ring_SDLNet_UDP_Send);
 	ring_vm_funcregister("sdlnet_udp_recv",ring_SDLNet_UDP_Recv);
 	ring_vm_funcregister("sdlnet_udp_sendv",ring_SDLNet_UDP_SendV);
+	ring_vm_funcregister("sdlnet_udp_recvv",ring_SDLNet_UDP_RecvV);
 	ring_vm_funcregister("sdl_new_sdl_assert_data",ring_sdl_new_sdl_assert_data);
 	ring_vm_funcregister("sdl_destroy_sdl_assert_data",ring_sdl_destroy_sdl_assert_data);
 	ring_vm_funcregister("sdl_new_sdl_version",ring_sdl_new_sdl_version);
