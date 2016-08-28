e5b69bb7496a13f7a12e996e33ed9f965f379656
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-08-28 18:42:21 +0300

    Update Scripts

--- src/buildvccompletew.bat
+++ src/buildvccompletew.bat
@@ -10,7 +10,7 @@ ring_stmt.c ring_expr.c ring_codegen.c ring_vm.c ring_vmexpr.c ring_vmvars.c ^
 ring_vmlists.c ring_vmfuncs.c ring_api.c ring_vmoop.c ring_vmcui.c ^
 ring_vmtrycatch.c ring_vmstrindex.c ring_vmjump.c ring_vmduprange.c ^
 ring_vmperformance.c ring_vmexit.c ring_vmstackvars.c ring_vmstate.c ring_vmmath.c ring_vmfile.c ring_vmos.c ring_vmlistfuncs.c ring_vmrefmeta.c ^
-ring_vmodbc.c ring_vmmysql.c ..\extensions\ringopenssl\ring_vmopenssl.c ..\extensions\ringcurl\ring_vmcurl.c ring_ext.c ring_vmdll.c ..\extensions\ringsqlite\ring_vmsqlite.c ..\extensions\ringsqlite\sqlite3.c ring_objfile.c -I"..\include" -I"..\libdepwin\libcurl\include" -I"..\libdepwin\openssl\include" -I"..\libdepwin\mysql\include" 
+ring_vmodbc.c ..\extensions\ringmysql\ring_vmmysql.c ..\extensions\ringopenssl\ring_vmopenssl.c ..\extensions\ringcurl\ring_vmcurl.c ring_ext.c ring_vmdll.c ..\extensions\ringsqlite\ring_vmsqlite.c ..\extensions\ringsqlite\sqlite3.c ring_objfile.c -I"..\include" -I"..\libdepwin\libcurl\include" -I"..\libdepwin\openssl\include" -I"..\libdepwin\mysql\include" 
 
 link /DLL /DEBUG /OUT:..\lib\ring.dll /SUBSYSTEM:CONSOLE,"5.01"  ring_string.obj ^
 ring_list.obj ring_item.obj ring_items.obj ring_hashtable.obj ring_state.obj ring_scanner.obj ring_parser.obj ring_hashlib.obj ring_vmgc.obj ^
