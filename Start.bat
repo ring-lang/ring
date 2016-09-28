11f4e6d777249a2bb8a7a8f6713634e01ff9ced7
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-09-28 03:44:06 +0300

    Add Start.bat

---
+++ Start.bat
@@ -0,0 +1,8 @@
+rem This batch file will run the Ring Notepad Application.
+rem Ring Notepad is just a sample about what we can do using Ring in little hours.
+rem Using Ring Notepad we can write and execute Ring programs quickly.
+rem Author : Mahmoud Fayed <msfclipper@yahoo.com>
+
+cd applications
+cd rnote
+"../../bin/ring.exe" rnote.ring
