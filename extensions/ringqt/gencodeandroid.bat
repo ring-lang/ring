0640d49e5dffad50ae9b05977410d2504b30b03d
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2016-10-06 05:26:36 +0300

    Update RingQt - gencodeandroid.bat

--- extensions/ringqt/gencodeandroid.bat
+++ extensions/ringqt/gencodeandroid.bat
@@ -3,3 +3,5 @@ ring ..\codegen\parsec.ring qt.cf ring_qt.cpp ring_qt.ring
 ring qtevents.ring
 copy *.cpp ..\..\android\ringqt\project
 copy *.h ..\..\android\ringqt\project
+copy qt.cf ..\..\android\ringqt\project
+copy ring_qt.ring ..\..\android\ringqt\project
