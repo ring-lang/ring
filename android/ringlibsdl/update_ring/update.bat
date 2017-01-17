8a658d5253d4de8b85f8befbe36cb3eacba625ba
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-01-17 21:38:19 +0300

    Update update.bat - Update the Game Engine in Ring Android-LibSDL Version.

--- android/ringlibsdl/update_ring/update.bat
+++ android/ringlibsdl/update_ring/update.bat
@@ -5,3 +5,10 @@ copy ..\..\..\src\*.c ..\project\jni\ring\src\
 copy ring_ext.c ..\project\jni\ring\src\
 copy ..\..\..\include\*.h ..\project\jni\ring\include\
 copy ring_ext.h ..\project\jni\ring\include\
+copy ..\..\..\ringlibs\gameengine\gameengine.ring ..\project\assets\
+copy ..\..\..\ringlibs\gameengine\gameengine.rh ..\project\assets\
+copy ..\..\..\ringlibs\gameengine\gl_allegro.ring ..\project\assets\
+copy ..\..\..\ringlibs\gameengine\gl_libsdl.ring ..\project\assets\
+copy ..\..\..\extensions\ringsdl\libsdl.ring ..\project\assets\
+copy ..\..\..\extensions\ringsdl\ring_libsdl.rh ..\project\assets\
+copy ..\..\..\extensions\ringsdl\sdl.rh ..\project\assets\
