aa1910df914bea6f6be7bb5c72c1fde394b8c9f7
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-04-12 05:37:59 +0300

    Update Tests - Add new test : retselfbyref.ring

--- tests/scripts_list.bat
+++ tests/scripts_list.bat
@@ -229,3 +229,4 @@ call ring scripts/nullpointer.ring
 call ring scripts/ptrcmp2.ring
 call ring scripts/listandstrconversion.ring
 call ring scripts/objmethodnobraceerror.ring
+call ring scripts/retselfbyref.ring
