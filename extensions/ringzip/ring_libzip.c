8aa91a9e015ca1b0e7e39fcde850197e57d57fb5
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-05-01 06:28:14 +0300

    Update RingZip - Add zip_entry_write() function.

--- extensions/ringzip/ring_libzip.c
+++ extensions/ringzip/ring_libzip.c
@@ -61,9 +61,33 @@ RING_FUNC(ring_zip_entry_open)
 	RING_API_RETNUMBER(zip_entry_open((ZIP_T *) RING_API_GETCPOINTER(1,"ZIP_T"),RING_API_GETSTRING(2)));
 }
 
+
+RING_FUNC(ring_zip_entry_write)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	RING_API_IGNORECPOINTERTYPE ;
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISSTRING(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETNUMBER(zip_entry_write((ZIP_T *) RING_API_GETCPOINTER(1,"ZIP_T"),RING_API_GETSTRING(2), (int) RING_API_GETNUMBER(3)));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("zip_open",ring_zip_open);
 	ring_vm_funcregister("zip_close",ring_zip_close);
 	ring_vm_funcregister("zip_entry_open",ring_zip_entry_open);
+	ring_vm_funcregister("zip_entry_write",ring_zip_entry_write);
 }
