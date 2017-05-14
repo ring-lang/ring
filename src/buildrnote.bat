0b5329c81fe698acd593492a1aab2d057275d377
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-05-14 19:06:07 +0300

    Update Ring - rnote.rc

--- src/buildrnote.bat
+++ src/buildrnote.bat
@@ -4,6 +4,7 @@ call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 cls
 
-cl /DEBUG rnote.c /link advapi32.lib shell32.lib /SUBSYSTEM:WINDOWS,"5.01" 
+rc rnote.rc
+cl /DEBUG rnote.c rnote.res /link advapi32.lib shell32.lib /SUBSYSTEM:WINDOWS,"5.01" 
 
 copy rnote.exe ..\
