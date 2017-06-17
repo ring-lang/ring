7a738d4ad1dd9fc2191b8d050a543b1a1dc34c83
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-06-17 06:43:53 +0300

    Update RingSQLite

--- extensions/ringsqlite/ring_vmsqlite.h
+++ extensions/ringsqlite/ring_vmsqlite.h
@@ -3,7 +3,7 @@
 #define ring_vmsqlite_h
 /* Functions */
 
-void ring_vm_sqlite_loadfunctions ( RingState *pRingState ) ;
+RING_API void ringlib_init ( RingState *pRingState ) ;
 
 void ring_vm_sqlite_init ( void *pPointer ) ;
 
