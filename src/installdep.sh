aa9a1ea33d2c4116e5979737926f9d4493b8ae9b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-07-10 01:38:30 +0300

    Update installdep.sh

--- src/installdep.sh
+++ src/installdep.sh
@@ -1,1 +1,1 @@
-sudo apt-get install unixodbc unixodbc-dev libmysqlclient-dev libcurl4-gnutls-dev libssl-dev liballegro5-dev qt5-default qtbase5-dev qtmultimedia5-dev libqt5webkit5-dev
+sudo apt-get install unixodbc unixodbc-dev libmysqlclient-dev libcurl4-gnutls-dev libssl-dev liballegro5-dev qt5-default qtbase5-dev qtmultimedia5-dev libqt5webkit5-dev libqt5serialport5-dev
