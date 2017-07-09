0aacc1d5f0edfec7f4ebcfc83d3f8ac465210bd6
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-07-10 01:25:15 +0300

    Update buildgcc.sh

--- extensions/ringopenssl/buildgcc.sh
+++ extensions/ringopenssl/buildgcc.sh
@@ -1,4 +1,4 @@
-gcc -c -fpic ring_openssl.c -I $PWD/../../include
+gcc -c -fpic ring_vmopenssl.c -I $PWD/../../include
 gcc -shared -o $PWD/../../lib/libring_openssl.so ring_vmopenssl.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -lssl -lcrypto
 
  
