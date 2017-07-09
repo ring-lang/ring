08489815ba388c2c3c3e6ff34d988f4234ad4e91
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-07-10 02:27:31 +0300

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -15,11 +15,11 @@ fi
 
 if [ -f lib/libring.so ];
 then
-echo ".`pwd`/ring \$1" > /usr/local/bin/ring
+echo ".`pwd`/ring \$1" > /usr/bin/ring
 chmod +x /usr/local/bin/ring
 
 cd ..
-cp lib/libring.so /usr/local/lib
+cp lib/libring.so /usr/lib
 fi
 	
 # Make the RingODBC library ready for use directly
@@ -31,7 +31,7 @@ fi
 
 if [ -f lib/libring_odbclib.so ];
 then
-cp lib/libring_odbclib.so /usr/local/lib
+cp lib/libring_odbclib.so /usr/lib
 fi
 
 # Make the RingMySQL library ready for use directly
@@ -43,7 +43,7 @@ fi
 
 if [ -f lib/libring_mysqllib.so ];
 then
-cp lib/libring_mysqllib.so /usr/local/lib
+cp lib/libring_mysqllib.so /usr/lib
 fi
 
 # Make the RingSQLite library ready for use directly
@@ -55,7 +55,7 @@ fi
 
 if [ -f lib/libring_sqlitelib.so ];
 then
-cp lib/libring_sqlitelib.so /usr/local/lib
+cp lib/libring_sqlitelib.so /usr/lib
 fi
 
 # Make the RingInternet library ready for use directly
@@ -67,7 +67,7 @@ fi
 
 if [ -f lib/libring_internetlib.so ];
 then
-cp lib/libring_internetlib.so /usr/local/lib
+cp lib/libring_internetlib.so /usr/lib
 fi
 
 # Make the RingOpenSSL library ready for use directly
@@ -79,7 +79,7 @@ fi
 
 if [ -f lib/libring_openssllib.so ];
 then
-cp lib/libring_openssllib.so /usr/local/lib
+cp lib/libring_openssllib.so /usr/lib
 fi
 
 # Make the RingAllegro library ready for use directly
@@ -91,7 +91,7 @@ fi
 
 if [ -f lib/libringallegro.so ];
 then
-cp lib/libringallegro.so /usr/local/lib
+cp lib/libringallegro.so /usr/lib
 fi
 
 # Make the RingQt library ready for use directly
@@ -103,7 +103,7 @@ fi
 
 if [ -f lib/libringqt.so ];
 then
-cp lib/libringqt.so /usr/local/lib
+cp lib/libringqt.so /usr/lib
 fi
 
 # Make the RingLibCurl library ready for use directly
@@ -115,18 +115,18 @@ fi
 
 if [ -f lib/libring_libcurl.so ];
 then
-cp lib/libring_libcurl.so /usr/local/lib
+cp lib/libring_libcurl.so /usr/lib
 fi
 
 # Make the RingZipLib library ready for use directly
 
 if [ -f lib/libring_ziplib.dylib ];
 then
-cp lib/libring_ziplib.dylib /usr/local/lib
+cp lib/libring_ziplib.dylib /usr/lib
 fi
 
 if [ -f lib/libring_ziplib.so ];
 then
-cp lib/libring_ziplib.so /usr/local/lib
+cp lib/libring_ziplib.so /usr/lib
 fi
 
