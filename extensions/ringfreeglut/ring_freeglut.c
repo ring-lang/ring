f8e615436d86bb1d56a194ad8c5c229a89fd534e
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-04 18:58:22 +0300

    Update RingFreeGLUT - Header Files.

--- extensions/ringfreeglut/ring_freeglut.c
+++ extensions/ringfreeglut/ring_freeglut.c
@@ -1,5 +1,14 @@
 #include "ring.h"
 
+
+/*
+	RingFreeGLUT Extension
+	Copyright (c) 2017 Mahmoud Fayed <msfclipper@yahoo.com>
+*/
+
+
+#include <GL/glut.h>
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 }
