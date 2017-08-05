e1d4c989e34a19ff6745a2a44a3e0b4f63cf07ac
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-05 12:55:05 +0300

    Update RingFreeGLUT - Add Functions void glutWireCone(double base, double height, GLint slices, GLint stacks) void glutSolidCone(double base, double height, GLint slices, GLint stacks)

--- extensions/ringfreeglut/ring_freeglut.c
+++ extensions/ringfreeglut/ring_freeglut.c
@@ -796,31 +796,29 @@ RING_FUNC(ring_glutSolidTorus)
 }
 
 
-RING_FUNC(ring_glutInitDisplayMode)
+RING_FUNC(ring_glutWireCone)
 {
-	if ( RING_API_PARACOUNT != 1 ) {
-		RING_API_ERROR(RING_API_MISS1PARA);
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
 		return ;
 	}
 	if ( ! RING_API_ISNUMBER(1) ) {
 		RING_API_ERROR(RING_API_BADPARATYPE);
 		return ;
 	}
-	glutInitDisplayMode( (int) RING_API_GETNUMBER(1));
-}
-
-
-RING_FUNC(ring_glutCreateWindow)
-{
-	if ( RING_API_PARACOUNT != 1 ) {
-		RING_API_ERROR(RING_API_MISS1PARA);
-		return ;
-	}
-	if ( ! RING_API_ISSTRING(1) ) {
+	if ( ! RING_API_ISNUMBER(2) ) {
 		RING_API_ERROR(RING_API_BADPARATYPE);
 		return ;
 	}
-	glutCreateWindow(RING_API_GETSTRING(1));
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glutWireCone( (double ) RING_API_GETNUMBER(1), (double ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3), (GLint ) RING_API_GETNUMBER(4));
 }
 
 
@@ -846,7 +844,35 @@ RING_FUNC(ring_glutSolidCone)
 		RING_API_ERROR(RING_API_BADPARATYPE);
 		return ;
 	}
-	glutSolidCone( (double) RING_API_GETNUMBER(1), (double) RING_API_GETNUMBER(2), (int) RING_API_GETNUMBER(3), (int) RING_API_GETNUMBER(4));
+	glutSolidCone( (double ) RING_API_GETNUMBER(1), (double ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3), (GLint ) RING_API_GETNUMBER(4));
+}
+
+
+RING_FUNC(ring_glutInitDisplayMode)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glutInitDisplayMode( (int) RING_API_GETNUMBER(1));
+}
+
+
+RING_FUNC(ring_glutCreateWindow)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISSTRING(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glutCreateWindow(RING_API_GETSTRING(1));
 }
 
 RING_FUNC(ring_get_gl_color_buffer_bit)
@@ -1248,9 +1274,10 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glutsolidsphere",ring_glutSolidSphere);
 	ring_vm_funcregister("glutwiretorus",ring_glutWireTorus);
 	ring_vm_funcregister("glutsolidtorus",ring_glutSolidTorus);
+	ring_vm_funcregister("glutwirecone",ring_glutWireCone);
+	ring_vm_funcregister("glutsolidcone",ring_glutSolidCone);
 	ring_vm_funcregister("glutinitdisplaymode",ring_glutInitDisplayMode);
 	ring_vm_funcregister("glutcreatewindow",ring_glutCreateWindow);
-	ring_vm_funcregister("glutsolidcone",ring_glutSolidCone);
 	ring_vm_funcregister("glclear",ring_glClear);
 	ring_vm_funcregister("glcolor3f",ring_glColor3f);
 	ring_vm_funcregister("glbegin",ring_glBegin);
