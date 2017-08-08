efce99ef398d783927907d1e1c62f4ac1f62616b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:59:28 +0300

    Update RingOpenGL - Add Function (Source Code) : void glTexCoord1fv(const GLfloat * v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -8306,6 +8306,20 @@ RING_FUNC(ring_glTexCoord1iv)
 	glTexCoord1iv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
 }
 
+
+RING_FUNC(ring_glTexCoord1fv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glTexCoord1fv((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -8719,4 +8733,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltexcoord4d",ring_glTexCoord4d);
 	ring_vm_funcregister("gltexcoord1sv",ring_glTexCoord1sv);
 	ring_vm_funcregister("gltexcoord1iv",ring_glTexCoord1iv);
+	ring_vm_funcregister("gltexcoord1fv",ring_glTexCoord1fv);
 }
