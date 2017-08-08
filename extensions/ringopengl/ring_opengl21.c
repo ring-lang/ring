90151eae62e2d83761c701361a6e4ab31d647855
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:11:40 +0300

    Update RingOpenGL - Add Function (Source Code) : void glWindowPos3f(GLfloat x,GLfloat y,GLfloat z)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -10944,6 +10944,28 @@ RING_FUNC(ring_glWindowPos3i)
 	glWindowPos3i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glWindowPos3f)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glWindowPos3f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -11482,4 +11504,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glwindowpos2d",ring_glWindowPos2d);
 	ring_vm_funcregister("glwindowpos3s",ring_glWindowPos3s);
 	ring_vm_funcregister("glwindowpos3i",ring_glWindowPos3i);
+	ring_vm_funcregister("glwindowpos3f",ring_glWindowPos3f);
 }
