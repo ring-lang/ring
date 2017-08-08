b7d3f4a3844ce06388f34ba17c4840ac806ba2c6
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:07:19 +0300

    Update RingOpenGL - Add Function (Source Code) : void glVertex4sv(const GLshort * v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -10100,6 +10100,20 @@ RING_FUNC(ring_glVertex3dv)
 	glVertex3dv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glVertex4sv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex4sv((GLshort *) RING_API_GETCPOINTER(1,"GLshort"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -10595,4 +10609,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertex3iv",ring_glVertex3iv);
 	ring_vm_funcregister("glvertex3fv",ring_glVertex3fv);
 	ring_vm_funcregister("glvertex3dv",ring_glVertex3dv);
+	ring_vm_funcregister("glvertex4sv",ring_glVertex4sv);
 }
