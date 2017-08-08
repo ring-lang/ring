dc2fe83ee161674a21a7911b1155cc78c833a806
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:41:00 +0300

    Update RingOpenGL - Add Function (Source Code) : void glDeleteProgram(GLuint program)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -2154,6 +2154,20 @@ RING_FUNC(ring_glDeleteLists)
 	glDeleteLists( (GLuint ) RING_API_GETNUMBER(1), (GLsizei )  (int) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glDeleteProgram)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glDeleteProgram( (GLuint ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -2252,4 +2266,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glcullface",ring_glCullFace);
 	ring_vm_funcregister("gldeletebuffers",ring_glDeleteBuffers);
 	ring_vm_funcregister("gldeletelists",ring_glDeleteLists);
+	ring_vm_funcregister("gldeleteprogram",ring_glDeleteProgram);
 }
