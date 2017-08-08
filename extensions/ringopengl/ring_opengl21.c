a3cb9cccc52c4fd074b7de3a1d7f0413d0f32d9b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:39:47 +0300

    Update RingOpenGL - Add Function (Source Code) : void glColor3bv(const GLbyte * v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -880,6 +880,20 @@ RING_FUNC(ring_glColor4ui)
 	glColor4ui( (GLuint ) RING_API_GETNUMBER(1), (GLuint ) RING_API_GETNUMBER(2), (GLuint ) RING_API_GETNUMBER(3), (GLuint ) RING_API_GETNUMBER(4));
 }
 
+
+RING_FUNC(ring_glColor3bv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glColor3bv((GLbyte *) RING_API_GETCPOINTER(1,"GLbyte"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -927,4 +941,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glcolor4ub",ring_glColor4ub);
 	ring_vm_funcregister("glcolor4us",ring_glColor4us);
 	ring_vm_funcregister("glcolor4ui",ring_glColor4ui);
+	ring_vm_funcregister("glcolor3bv",ring_glColor3bv);
 }
