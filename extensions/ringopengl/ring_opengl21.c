0372d007638f9c411c4ce9bade6cbf4f3360bfef
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:43:39 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetMinmaxParameteriv(GLenum target,GLenum pname,GLint * params)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -3702,6 +3702,28 @@ RING_FUNC(ring_glGetMinmaxParameterfv)
 	glGetMinmaxParameterfv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glGetMinmaxParameteriv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetMinmaxParameteriv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3880,4 +3902,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetmaterialiv",ring_glGetMaterialiv);
 	ring_vm_funcregister("glgetminmax",ring_glGetMinmax);
 	ring_vm_funcregister("glgetminmaxparameterfv",ring_glGetMinmaxParameterfv);
+	ring_vm_funcregister("glgetminmaxparameteriv",ring_glGetMinmaxParameteriv);
 }
