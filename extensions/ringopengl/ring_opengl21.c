97c0fa03659c5d74a0a642b6b036b0a7587b033e
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:04:01 +0300

    Update RingOpenGL - Add Function (Source Code) : void glUniform2iv(GLint location,GLsizei count,const GLint *value)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -9414,6 +9414,28 @@ RING_FUNC(ring_glUniform1iv)
 	glUniform1iv( (GLint ) RING_API_GETNUMBER(1), (GLsizei )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
 }
 
+
+RING_FUNC(ring_glUniform2iv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glUniform2iv( (GLint ) RING_API_GETNUMBER(1), (GLsizei )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -9875,4 +9897,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gluniform3fv",ring_glUniform3fv);
 	ring_vm_funcregister("gluniform4fv",ring_glUniform4fv);
 	ring_vm_funcregister("gluniform1iv",ring_glUniform1iv);
+	ring_vm_funcregister("gluniform2iv",ring_glUniform2iv);
 }
