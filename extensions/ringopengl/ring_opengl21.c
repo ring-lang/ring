ec445d7c46eee5b5019c3cf1f8af4dd55a127b73
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:00:45 +0300

    Update RingOpenGL - Add Function (Source Code) : void glTexCoordPointer(GLint size,GLenum type,GLsizei stride,const GLvoid * pointer)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -8502,6 +8502,32 @@ RING_FUNC(ring_glTexCoord4dv)
 	glTexCoord4dv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glTexCoordPointer)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glTexCoordPointer( (GLint ) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLsizei )  (int) RING_API_GETNUMBER(3),(GLvoid *) RING_API_GETCPOINTER(4,"GLvoid"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -8929,4 +8955,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltexcoord4iv",ring_glTexCoord4iv);
 	ring_vm_funcregister("gltexcoord4fv",ring_glTexCoord4fv);
 	ring_vm_funcregister("gltexcoord4dv",ring_glTexCoord4dv);
+	ring_vm_funcregister("gltexcoordpointer",ring_glTexCoordPointer);
 }
