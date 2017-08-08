06deb456f842f209aa20d0197c1786d4c1b70da7
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:55:48 +0300

    Update RingOpenGL - Add Function (Source Code) : void glSecondaryColor3f(GLfloat red,GLfloat green,GLfloat blue)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -7514,6 +7514,28 @@ RING_FUNC(ring_glSecondaryColor3i)
 	glSecondaryColor3i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glSecondaryColor3f)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glSecondaryColor3f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7885,4 +7907,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glsecondarycolor3b",ring_glSecondaryColor3b);
 	ring_vm_funcregister("glsecondarycolor3s",ring_glSecondaryColor3s);
 	ring_vm_funcregister("glsecondarycolor3i",ring_glSecondaryColor3i);
+	ring_vm_funcregister("glsecondarycolor3f",ring_glSecondaryColor3f);
 }
