981077c8d0f20896f7c17ed2713aacf7300b524b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:48:56 +0300

    Update RingOpenGL - Add Function (Source Code) : void glMultiTexCoord2d(GLenum target,GLdouble s,GLdouble t)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -5712,6 +5712,28 @@ RING_FUNC(ring_glMultiTexCoord2f)
 	glMultiTexCoord2f( (GLenum )  (int) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glMultiTexCoord2d)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glMultiTexCoord2d( (GLenum )  (int) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2), (GLdouble ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5991,4 +6013,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glmultitexcoord2s",ring_glMultiTexCoord2s);
 	ring_vm_funcregister("glmultitexcoord2i",ring_glMultiTexCoord2i);
 	ring_vm_funcregister("glmultitexcoord2f",ring_glMultiTexCoord2f);
+	ring_vm_funcregister("glmultitexcoord2d",ring_glMultiTexCoord2d);
 }
