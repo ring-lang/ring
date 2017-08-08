89191725bd4b23625db8afc089475cf3519ed6d9
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:43:44 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetPixelMapuiv(GLenum map,GLuint * data)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -3742,6 +3742,24 @@ RING_FUNC(ring_glGetPixelMapfv)
 	glGetPixelMapfv( (GLenum )  (int) RING_API_GETNUMBER(1),(GLfloat *) RING_API_GETCPOINTER(2,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glGetPixelMapuiv)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetPixelMapuiv( (GLenum )  (int) RING_API_GETNUMBER(1),(GLuint *) RING_API_GETCPOINTER(2,"GLuint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3922,4 +3940,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetminmaxparameterfv",ring_glGetMinmaxParameterfv);
 	ring_vm_funcregister("glgetminmaxparameteriv",ring_glGetMinmaxParameteriv);
 	ring_vm_funcregister("glgetpixelmapfv",ring_glGetPixelMapfv);
+	ring_vm_funcregister("glgetpixelmapuiv",ring_glGetPixelMapuiv);
 }
