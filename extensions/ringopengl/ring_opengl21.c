66dd025d3456cde7df378181159d5ed4f3a6ecbd
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:14:08 +0300

    Update RingOpenGL - Add Function (Source Code) : void gluGetNurbsProperty(GLUnurbs* nurb,GLenum property,GLfloat* data)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -11332,6 +11332,28 @@ RING_FUNC(ring_gluErrorString)
 	RING_API_RETCPOINTER(gluErrorString( (GLenum )  (int) RING_API_GETNUMBER(1)),"GLubyte");
 }
 
+
+RING_FUNC(ring_gluGetNurbsProperty)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	gluGetNurbsProperty((GLUnurbs *) RING_API_GETCPOINTER(1,"GLUnurbs"), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -11894,4 +11916,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gluendsurface",ring_gluEndSurface);
 	ring_vm_funcregister("gluendtrim",ring_gluEndTrim);
 	ring_vm_funcregister("gluerrorstring",ring_gluErrorString);
+	ring_vm_funcregister("glugetnurbsproperty",ring_gluGetNurbsProperty);
 }
