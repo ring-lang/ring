35e0bc77f7510cba5aa4483e9d4647dfa098cb95
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:12:29 +0300

    Update RingOpenGL - Add Function (Source Code) : void glWindowPos3fv(const GLfloat * v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -11072,6 +11072,20 @@ RING_FUNC(ring_glWindowPos3iv)
 	glWindowPos3iv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
 }
 
+
+RING_FUNC(ring_glWindowPos3fv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glWindowPos3fv((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -11618,4 +11632,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glwindowpos2dv",ring_glWindowPos2dv);
 	ring_vm_funcregister("glwindowpos3sv",ring_glWindowPos3sv);
 	ring_vm_funcregister("glwindowpos3iv",ring_glWindowPos3iv);
+	ring_vm_funcregister("glwindowpos3fv",ring_glWindowPos3fv);
 }
