541b8e4787613e4c1167f3af7319dc75c074a183
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:46:46 +0300

    Update RingOpenGL - Add Function (Source Code) : void glLineWidth(GLfloat width)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4954,6 +4954,20 @@ RING_FUNC(ring_glLineStipple)
 		free(RING_API_GETCPOINTER(2,"GLushort"));
 }
 
+
+RING_FUNC(ring_glLineWidth)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glLineWidth( (GLfloat ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5197,4 +5211,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gllightmodelfv",ring_glLightModelfv);
 	ring_vm_funcregister("gllightmodeliv",ring_glLightModeliv);
 	ring_vm_funcregister("gllinestipple",ring_glLineStipple);
+	ring_vm_funcregister("gllinewidth",ring_glLineWidth);
 }
