c26723bbd60f006b48527b216fafede652b5e687
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:21:47 +0300

    Start RingOpenGL development (OpenGL 2.1 functions).

---
+++ extensions/ringopengl/ring_opengl21.c
@@ -0,0 +1,15 @@
+#include "ring.h"
+
+
+/*
+	OpenGL 2.1 Extension
+	Copyright (c) 2017 Mahmoud Fayed <msfclipper@yahoo.com>
+*/
+
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+
+RING_API void ringlib_init(RingState *pRingState)
+{
+}
