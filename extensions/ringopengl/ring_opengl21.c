e0b53e6f825d878c45fb9912795387fce7a9afd0
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:41:47 +0300

    Update RingOpenGL - Add Function (Source Code) : void glEvalCoord2d(GLdouble u,GLdouble v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -2590,6 +2590,24 @@ RING_FUNC(ring_glEvalCoord2f)
 	glEvalCoord2f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glEvalCoord2d)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glEvalCoord2d( (GLdouble ) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -2714,4 +2732,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glevalcoord1f",ring_glEvalCoord1f);
 	ring_vm_funcregister("glevalcoord1d",ring_glEvalCoord1d);
 	ring_vm_funcregister("glevalcoord2f",ring_glEvalCoord2f);
+	ring_vm_funcregister("glevalcoord2d",ring_glEvalCoord2d);
 }
