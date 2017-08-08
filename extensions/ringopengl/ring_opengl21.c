920b4981d0d61fee9b7030a8c8d4ee744b659491
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:58:43 +0300

    Update RingOpenGL - Add Function (Source Code) : void glTexCoord3f(GLfloat s,GLfloat t,GLfloat r)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -8138,6 +8138,28 @@ RING_FUNC(ring_glTexCoord3i)
 	glTexCoord3i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glTexCoord3f)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glTexCoord3f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -8543,4 +8565,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltexcoord2d",ring_glTexCoord2d);
 	ring_vm_funcregister("gltexcoord3s",ring_glTexCoord3s);
 	ring_vm_funcregister("gltexcoord3i",ring_glTexCoord3i);
+	ring_vm_funcregister("gltexcoord3f",ring_glTexCoord3f);
 }
