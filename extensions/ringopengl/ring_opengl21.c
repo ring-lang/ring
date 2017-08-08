7f83d3b3cf46886a47e015a00ff94ccbdaaa73f4
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:44:02 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetQueryiv(GLenum target,GLenum pname,GLint * params)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -3902,6 +3902,28 @@ RING_FUNC(ring_glGetQueryObjectuiv)
 	glGetQueryObjectuiv( (GLuint ) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLuint *) RING_API_GETCPOINTER(3,"GLuint"));
 }
 
+
+RING_FUNC(ring_glGetQueryiv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetQueryiv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4090,4 +4112,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetprograminfolog",ring_glGetProgramInfoLog);
 	ring_vm_funcregister("glgetqueryobjectiv",ring_glGetQueryObjectiv);
 	ring_vm_funcregister("glgetqueryobjectuiv",ring_glGetQueryObjectuiv);
+	ring_vm_funcregister("glgetqueryiv",ring_glGetQueryiv);
 }
