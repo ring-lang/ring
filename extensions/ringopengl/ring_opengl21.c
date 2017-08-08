49a39ef97cdffa4b1f259f879c19ea023a0e03a6
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:45:06 +0300

    Update RingOpenGL - Add Function (Source Code) : void glHistogram(GLenum target,GLsizei width,GLenum internalformat,GLboolean sink)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4450,6 +4450,32 @@ RING_FUNC(ring_glHint)
 	glHint( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glHistogram)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glHistogram( (GLenum )  (int) RING_API_GETNUMBER(1), (GLsizei )  (int) RING_API_GETNUMBER(2), (GLenum )  (int) RING_API_GETNUMBER(3), (GLboolean ) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4662,4 +4688,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetvertexattribiv",ring_glGetVertexAttribiv);
 	ring_vm_funcregister("glgetvertexattribpointerv",ring_glGetVertexAttribPointerv);
 	ring_vm_funcregister("glhint",ring_glHint);
+	ring_vm_funcregister("glhistogram",ring_glHistogram);
 }
