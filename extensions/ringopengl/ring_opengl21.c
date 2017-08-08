e263b70f1126b4fdb1ade836aacd547ad0e3e3b2
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:38:53 +0300

    Update RingOpenGL - Add Function (Source Code) : void glAccum(GLenum op,GLfloat value)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -10,6 +10,25 @@
 #include <GL/glew.h>
 #include <GL/glut.h>
 
+
+RING_FUNC(ring_glAccum)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glAccum( (GLenum )  (int) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
+	ring_vm_funcregister("glaccum",ring_glAccum);
 }
