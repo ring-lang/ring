2d63acd94b479eae05b2dede040d70846359a777
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:44:22 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetTexEnviv(GLenum target,GLenum pname,GLint * params)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4068,6 +4068,28 @@ RING_FUNC(ring_glGetTexEnvfv)
 	glGetTexEnvfv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glGetTexEnviv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetTexEnviv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4263,4 +4285,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetshadersource",ring_glGetShaderSource);
 	ring_vm_funcregister("glgetstring",ring_glGetString);
 	ring_vm_funcregister("glgettexenvfv",ring_glGetTexEnvfv);
+	ring_vm_funcregister("glgettexenviv",ring_glGetTexEnviv);
 }
