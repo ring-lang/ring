02d3053d8852fecc8612a9eca611899ec702d8a9
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:48:06 +0300

    Update RingOpenGL - Add Function (Source Code) : void glMinmax(GLenum target,GLenum internalformat,GLboolean sink)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -5446,6 +5446,28 @@ RING_FUNC(ring_glMatrixMode)
 	glMatrixMode( (GLenum )  (int) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glMinmax)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glMinmax( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLboolean ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5711,4 +5733,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glmaterialf",ring_glMaterialf);
 	ring_vm_funcregister("glmateriali",ring_glMateriali);
 	ring_vm_funcregister("glmatrixmode",ring_glMatrixMode);
+	ring_vm_funcregister("glminmax",ring_glMinmax);
 }
