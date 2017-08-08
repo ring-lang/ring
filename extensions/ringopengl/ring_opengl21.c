55e9c499fa1cda0bd9d26048baaad5bd825121e8
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:40:33 +0300

    Update RingOpenGL - Add Function (Source Code) : void glConvolutionParameterfv(GLenum target,GLenum pname,const GLfloat * params)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -1680,6 +1680,28 @@ RING_FUNC(ring_glConvolutionParameteri)
 	glConvolutionParameteri( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glConvolutionParameterfv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glConvolutionParameterfv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -1761,4 +1783,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glconvolutionfilter2d",ring_glConvolutionFilter2D);
 	ring_vm_funcregister("glconvolutionparameterf",ring_glConvolutionParameterf);
 	ring_vm_funcregister("glconvolutionparameteri",ring_glConvolutionParameteri);
+	ring_vm_funcregister("glconvolutionparameterfv",ring_glConvolutionParameterfv);
 }
