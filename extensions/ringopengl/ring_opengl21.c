abe5389137203da8a8a407caeddc100d22cd17b8
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:48:20 +0300

    Update RingOpenGL - Add Function (Source Code) : void glMultTransposeMatrixf(const GLfloat * m)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -5510,6 +5510,20 @@ RING_FUNC(ring_glMultTransposeMatrixd)
 	glMultTransposeMatrixd((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glMultTransposeMatrixf)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glMultTransposeMatrixf((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5779,4 +5793,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glmultmatrixd",ring_glMultMatrixd);
 	ring_vm_funcregister("glmultmatrixf",ring_glMultMatrixf);
 	ring_vm_funcregister("glmulttransposematrixd",ring_glMultTransposeMatrixd);
+	ring_vm_funcregister("glmulttransposematrixf",ring_glMultTransposeMatrixf);
 }
