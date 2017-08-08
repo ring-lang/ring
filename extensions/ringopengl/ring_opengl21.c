d450329d46de32b61d3990fe00a3e4b8c91bee74
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:46:50 +0300

    Update RingOpenGL - Add Function (Source Code) : void glLinkProgram(GLuint program)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4968,6 +4968,20 @@ RING_FUNC(ring_glLineWidth)
 	glLineWidth( (GLfloat ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glLinkProgram)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glLinkProgram( (GLuint ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5212,4 +5226,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gllightmodeliv",ring_glLightModeliv);
 	ring_vm_funcregister("gllinestipple",ring_glLineStipple);
 	ring_vm_funcregister("gllinewidth",ring_glLineWidth);
+	ring_vm_funcregister("gllinkprogram",ring_glLinkProgram);
 }
