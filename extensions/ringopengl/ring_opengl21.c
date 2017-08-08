81c7c0fa442eb92c4952a5adfb6b4526c95eb254
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:44:56 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetVertexAttribfv(GLuint index,GLenum pname,GLfloat *params)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4366,6 +4366,28 @@ RING_FUNC(ring_glGetVertexAttribdv)
 	glGetVertexAttribdv( (GLuint ) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLdouble *) RING_API_GETCPOINTER(3,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glGetVertexAttribfv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetVertexAttribfv( (GLuint ) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4574,4 +4596,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetuniformiv",ring_glGetUniformiv);
 	ring_vm_funcregister("glgetuniformlocation",ring_glGetUniformLocation);
 	ring_vm_funcregister("glgetvertexattribdv",ring_glGetVertexAttribdv);
+	ring_vm_funcregister("glgetvertexattribfv",ring_glGetVertexAttribfv);
 }
