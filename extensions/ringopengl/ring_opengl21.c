3b2835e90fcf5930d094f111ccff592b5cb1eba3
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:57:47 +0300

    Update RingOpenGL - Add Function (Source Code) : void glStencilOpSeparate(GLenum face,GLenum sfail,GLenum dpfail,GLenum dppass)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -7952,6 +7952,32 @@ RING_FUNC(ring_glStencilOp)
 	glStencilOp( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLenum )  (int) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glStencilOpSeparate)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glStencilOpSeparate( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLenum )  (int) RING_API_GETNUMBER(3), (GLenum )  (int) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -8346,4 +8372,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glstencilmask",ring_glStencilMask);
 	ring_vm_funcregister("glstencilmaskseparate",ring_glStencilMaskSeparate);
 	ring_vm_funcregister("glstencilop",ring_glStencilOp);
+	ring_vm_funcregister("glstencilopseparate",ring_glStencilOpSeparate);
 }
