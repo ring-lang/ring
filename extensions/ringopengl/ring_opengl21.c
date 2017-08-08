c06d8dd6f678e5163471a99c2b827b9a455c988d
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:06:42 +0300

    Update RingOpenGL - Add Function (Source Code) : void glVertex2fv(const GLfloat * v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -10016,6 +10016,20 @@ RING_FUNC(ring_glVertex2iv)
 	glVertex2iv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
 }
 
+
+RING_FUNC(ring_glVertex2fv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex2fv((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -10505,4 +10519,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertex4d",ring_glVertex4d);
 	ring_vm_funcregister("glvertex2sv",ring_glVertex2sv);
 	ring_vm_funcregister("glvertex2iv",ring_glVertex2iv);
+	ring_vm_funcregister("glvertex2fv",ring_glVertex2fv);
 }
