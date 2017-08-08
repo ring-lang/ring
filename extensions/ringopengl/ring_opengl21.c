50dbf84f2cd0740d7c070b41304f797466994dd9
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:43:11 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetHistogram(GLenum target,GLboolean reset,GLenum format,GLenum type,GLvoid * values)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -3422,6 +3422,36 @@ RING_FUNC(ring_glGetError)
 	RING_API_RETNUMBER(glGetError());
 }
 
+
+RING_FUNC(ring_glGetHistogram)
+{
+	if ( RING_API_PARACOUNT != 5 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(5) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetHistogram( (GLenum )  (int) RING_API_GETNUMBER(1), (GLboolean ) RING_API_GETNUMBER(2), (GLenum )  (int) RING_API_GETNUMBER(3), (GLenum )  (int) RING_API_GETNUMBER(4),(GLvoid *) RING_API_GETCPOINTER(5,"GLvoid"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3588,4 +3618,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetconvolutionparameterfv",ring_glGetConvolutionParameterfv);
 	ring_vm_funcregister("glgetconvolutionparameteriv",ring_glGetConvolutionParameteriv);
 	ring_vm_funcregister("glgeterror",ring_glGetError);
+	ring_vm_funcregister("glgethistogram",ring_glGetHistogram);
 }
