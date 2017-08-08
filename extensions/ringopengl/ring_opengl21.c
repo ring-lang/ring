32ee86ce441dca40215a37a41b71ed986dd75955
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:06:14 +0300

    Update RingOpenGL - Add Function (Source Code) : void glVertex4i(GLint x,GLint y,GLint z,GLint w)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -9910,6 +9910,32 @@ RING_FUNC(ring_glVertex4s)
 		free(RING_API_GETCPOINTER(4,"GLshort"));
 }
 
+
+RING_FUNC(ring_glVertex4i)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex4i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3), (GLint ) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -10394,4 +10420,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertex3f",ring_glVertex3f);
 	ring_vm_funcregister("glvertex3d",ring_glVertex3d);
 	ring_vm_funcregister("glvertex4s",ring_glVertex4s);
+	ring_vm_funcregister("glvertex4i",ring_glVertex4i);
 }
