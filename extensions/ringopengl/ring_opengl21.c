78a749e49ce053ea4dad57af3b740ae2a3dfec60
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:13:32 +0300

    Update RingOpenGL - Add Function (Source Code) : void gluDisk(GLUquadric* quad,GLdouble inner,GLdouble outer,GLint slices,GLint loops)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -11232,6 +11232,36 @@ RING_FUNC(ring_gluDeleteTess)
 	gluDeleteTess((GLUtesselator *) RING_API_GETCPOINTER(1,"GLUtesselator"));
 }
 
+
+RING_FUNC(ring_gluDisk)
+{
+	if ( RING_API_PARACOUNT != 5 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(5) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	gluDisk((GLUquadric *) RING_API_GETCPOINTER(1,"GLUquadric"), (GLdouble ) RING_API_GETNUMBER(2), (GLdouble ) RING_API_GETNUMBER(3), (GLint ) RING_API_GETNUMBER(4), (GLint ) RING_API_GETNUMBER(5));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -11788,4 +11818,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gludeletenurbsrenderer",ring_gluDeleteNurbsRenderer);
 	ring_vm_funcregister("gludeletequadric",ring_gluDeleteQuadric);
 	ring_vm_funcregister("gludeletetess",ring_gluDeleteTess);
+	ring_vm_funcregister("gludisk",ring_gluDisk);
 }
