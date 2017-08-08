48bd65dc66bc7ba41375fc4c8f327b3cdc96e7e0
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:52:22 +0300

    Update RingOpenGL - Add Function (Source Code) : void glPointParameteri(GLenum pname,GLint param)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -6662,6 +6662,24 @@ RING_FUNC(ring_glPointParameterf)
 	glPointParameterf( (GLenum )  (int) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glPointParameteri)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glPointParameteri( (GLenum )  (int) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6989,4 +7007,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpixeltransferi",ring_glPixelTransferi);
 	ring_vm_funcregister("glpixelzoom",ring_glPixelZoom);
 	ring_vm_funcregister("glpointparameterf",ring_glPointParameterf);
+	ring_vm_funcregister("glpointparameteri",ring_glPointParameteri);
 }
