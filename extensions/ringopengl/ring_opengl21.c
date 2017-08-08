f917ad7925e174a0c7fe5a4d3f52331a48071fb7
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:46:19 +0300

    Update RingOpenGL - Add Function (Source Code) : void glLighti(GLenum light,GLenum pname,GLint param)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4800,6 +4800,28 @@ RING_FUNC(ring_glLightf)
 	glLightf( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glLighti)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glLighti( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5035,4 +5057,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glisshader",ring_glIsShader);
 	ring_vm_funcregister("glistexture",ring_glIsTexture);
 	ring_vm_funcregister("gllightf",ring_glLightf);
+	ring_vm_funcregister("gllighti",ring_glLighti);
 }
