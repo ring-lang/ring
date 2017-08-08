3c38a96b966e01879190815f0095d8b9764e8e8c
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:10:09 +0300

    Update RingOpenGL - Add Function (Source Code) : void glVertexAttrib4dv(GLuint index,const GLdouble *v)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -10644,6 +10644,24 @@ RING_FUNC(ring_glVertexAttrib4sv)
 	glVertexAttrib4sv( (GLuint ) RING_API_GETNUMBER(1),(GLshort *) RING_API_GETCPOINTER(2,"GLshort"));
 }
 
+
+RING_FUNC(ring_glVertexAttrib4dv)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertexAttrib4dv( (GLuint ) RING_API_GETNUMBER(1),(GLdouble *) RING_API_GETCPOINTER(2,"GLdouble"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -11167,4 +11185,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertexattrib3dv",ring_glVertexAttrib3dv);
 	ring_vm_funcregister("glvertexattrib4fv",ring_glVertexAttrib4fv);
 	ring_vm_funcregister("glvertexattrib4sv",ring_glVertexAttrib4sv);
+	ring_vm_funcregister("glvertexattrib4dv",ring_glVertexAttrib4dv);
 }
