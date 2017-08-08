b261cd9cedba110d73e1472af8c83201cc089410
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:02:51 +0300

    Update RingOpenGL - Add Function (Source Code) : void glUniform1f(GLint location,GLfloat v0)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -9112,6 +9112,24 @@ RING_FUNC(ring_glTranslatef)
 	glTranslatef( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glUniform1f)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glUniform1f( (GLint ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -9560,4 +9578,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltexsubimage3d",ring_glTexSubImage3D);
 	ring_vm_funcregister("gltranslated",ring_glTranslated);
 	ring_vm_funcregister("gltranslatef",ring_glTranslatef);
+	ring_vm_funcregister("gluniform1f",ring_glUniform1f);
 }
