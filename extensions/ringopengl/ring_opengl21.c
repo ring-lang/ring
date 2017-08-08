2224eb0de62f12c33d9e31a1661c175f224bfd1e
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:52:30 +0300

    Update RingOpenGL - Add Function (Source Code) : void glPolygonMode(GLenum face,GLenum mode)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -6694,6 +6694,24 @@ RING_FUNC(ring_glPointSize)
 	glPointSize( (GLfloat ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glPolygonMode)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glPolygonMode( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7023,4 +7041,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpointparameterf",ring_glPointParameterf);
 	ring_vm_funcregister("glpointparameteri",ring_glPointParameteri);
 	ring_vm_funcregister("glpointsize",ring_glPointSize);
+	ring_vm_funcregister("glpolygonmode",ring_glPolygonMode);
 }
