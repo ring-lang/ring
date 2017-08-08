b7fce1d1d961db60a7ffd4f4659d9bc3b9604b43
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:41:33 +0300

    Update RingOpenGL - Add Function (Source Code) : void glEdgeFlagPointer(GLsizei stride,const GLvoid * pointer)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -2492,6 +2492,24 @@ RING_FUNC(ring_glEdgeFlag)
 	glEdgeFlag( (GLboolean ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glEdgeFlagPointer)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glEdgeFlagPointer( (GLsizei )  (int) RING_API_GETNUMBER(1),(GLvoid *) RING_API_GETCPOINTER(2,"GLvoid"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -2609,4 +2627,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gldrawpixels",ring_glDrawPixels);
 	ring_vm_funcregister("gldrawrangeelements",ring_glDrawRangeElements);
 	ring_vm_funcregister("gledgeflag",ring_glEdgeFlag);
+	ring_vm_funcregister("gledgeflagpointer",ring_glEdgeFlagPointer);
 }
