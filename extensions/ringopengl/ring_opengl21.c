9038a05a3d9dfc3293f951ac8e5c3e076e17b17f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:43:21 +0300

    Update RingOpenGL - Add Function (Source Code) : void glGetLightiv(GLenum light,GLenum pname,GLint * params)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -3518,6 +3518,28 @@ RING_FUNC(ring_glGetLightfv)
 	glGetLightfv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glGetLightiv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetLightiv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3688,4 +3710,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgethistogramparameterfv",ring_glGetHistogramParameterfv);
 	ring_vm_funcregister("glgethistogramparameteriv",ring_glGetHistogramParameteriv);
 	ring_vm_funcregister("glgetlightfv",ring_glGetLightfv);
+	ring_vm_funcregister("glgetlightiv",ring_glGetLightiv);
 }
