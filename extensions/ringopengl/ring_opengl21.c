5b7989fbae6442f6878d30b1decd3e2c6d4d0aa1
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 16:11:21 +0300

    Update RingOpenGL - Add Function (Source Code) : void glWindowPos2d(GLdouble x,GLdouble y)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -10888,6 +10888,24 @@ RING_FUNC(ring_glWindowPos2f)
 	glWindowPos2f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glWindowPos2d)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glWindowPos2d( (GLdouble ) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -11423,4 +11441,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glwindowpos2s",ring_glWindowPos2s);
 	ring_vm_funcregister("glwindowpos2i",ring_glWindowPos2i);
 	ring_vm_funcregister("glwindowpos2f",ring_glWindowPos2f);
+	ring_vm_funcregister("glwindowpos2d",ring_glWindowPos2d);
 }
