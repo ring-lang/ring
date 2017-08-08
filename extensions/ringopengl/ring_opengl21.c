21099fb462f59f1553e0f63fde86db4ddbdc620f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:52:33 +0300

    Update RingOpenGL - Add Function (Source Code) : void glPolygonOffset(GLfloat factor,GLfloat units)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -6712,6 +6712,24 @@ RING_FUNC(ring_glPolygonMode)
 	glPolygonMode( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glPolygonOffset)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glPolygonOffset( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7042,4 +7060,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpointparameteri",ring_glPointParameteri);
 	ring_vm_funcregister("glpointsize",ring_glPointSize);
 	ring_vm_funcregister("glpolygonmode",ring_glPolygonMode);
+	ring_vm_funcregister("glpolygonoffset",ring_glPolygonOffset);
 }
