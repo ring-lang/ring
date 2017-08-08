435dc3a2a3feeb31a9f2f97297af0d6c789b25a0
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-08 15:45:31 +0300

    Update RingOpenGL - Add Function (Source Code) : void glIndexfv(const GLfloat * c)

--- extensions/ringopengl/ring_opengl21.c
+++ extensions/ringopengl/ring_opengl21.c
@@ -4570,6 +4570,20 @@ RING_FUNC(ring_glIndexiv)
 	glIndexiv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
 }
 
+
+RING_FUNC(ring_glIndexfv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glIndexfv((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4790,4 +4804,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glindexub",ring_glIndexub);
 	ring_vm_funcregister("glindexsv",ring_glIndexsv);
 	ring_vm_funcregister("glindexiv",ring_glIndexiv);
+	ring_vm_funcregister("glindexfv",ring_glIndexfv);
 }
