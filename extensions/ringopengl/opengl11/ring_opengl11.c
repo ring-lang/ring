4da4d3b364b6f47db5b3bb4eea6d09b0991d48c6
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:49:07 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glVertex3i(GLint x, GLint y, GLint z)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -8896,6 +8896,28 @@ RING_FUNC(ring_glVertex3fv)
 	glVertex3fv((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glVertex3i)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex3i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -9220,6 +9242,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertex3dv",ring_glVertex3dv);
 	ring_vm_funcregister("glvertex3f",ring_glVertex3f);
 	ring_vm_funcregister("glvertex3fv",ring_glVertex3fv);
+	ring_vm_funcregister("glvertex3i",ring_glVertex3i);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
