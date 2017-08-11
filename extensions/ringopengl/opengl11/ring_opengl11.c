ad9c59f07ab8d11b884e2fc5ff4ca8a798eaa193
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:49:42 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glVertex4f(GLfloat x, GLfloat y, GLfloat z, GLfloat w)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -9008,6 +9008,32 @@ RING_FUNC(ring_glVertex4dv)
 	glVertex4dv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glVertex4f)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex4f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3), (GLfloat ) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -9338,6 +9364,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertex3sv",ring_glVertex3sv);
 	ring_vm_funcregister("glvertex4d",ring_glVertex4d);
 	ring_vm_funcregister("glvertex4dv",ring_glVertex4dv);
+	ring_vm_funcregister("glvertex4f",ring_glVertex4f);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
