2777b8ca0f082de7d1dbe226abc0b39c7de8dd12
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:11:47 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_LIGHT0

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2210,6 +2210,11 @@ RING_FUNC(ring_get_gl_clip_plane5)
 	RING_API_RETNUMBER(GL_CLIP_PLANE5);
 }
 
+RING_FUNC(ring_get_gl_light0)
+{
+	RING_API_RETNUMBER(GL_LIGHT0);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -2652,4 +2657,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_clip_plane3",ring_get_gl_clip_plane3);
 	ring_vm_funcregister("get_gl_clip_plane4",ring_get_gl_clip_plane4);
 	ring_vm_funcregister("get_gl_clip_plane5",ring_get_gl_clip_plane5);
+	ring_vm_funcregister("get_gl_light0",ring_get_gl_light0);
 }
