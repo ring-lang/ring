914ec6c180707042c0578efa094e3776982932ed
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:32:02 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glFeedbackBuffer(GLsizei size, GLenum type, GLfloat *buffer)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -4380,6 +4380,28 @@ RING_FUNC(ring_glEvalPoint2)
 	glEvalPoint2( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glFeedbackBuffer)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glFeedbackBuffer( (GLsizei )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4470,6 +4492,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glevalmesh2",ring_glEvalMesh2);
 	ring_vm_funcregister("glevalpoint1",ring_glEvalPoint1);
 	ring_vm_funcregister("glevalpoint2",ring_glEvalPoint2);
+	ring_vm_funcregister("glfeedbackbuffer",ring_glFeedbackBuffer);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
