d54398cba8d1ee8c30f26f07ec760af7a1880d87
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:06:34 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_POLYGON_SMOOTH_HINT

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -975,6 +975,11 @@ RING_FUNC(ring_get_gl_line_smooth_hint)
 	RING_API_RETNUMBER(GL_LINE_SMOOTH_HINT);
 }
 
+RING_FUNC(ring_get_gl_polygon_smooth_hint)
+{
+	RING_API_RETNUMBER(GL_POLYGON_SMOOTH_HINT);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1170,4 +1175,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_perspective_correction_hint",ring_get_gl_perspective_correction_hint);
 	ring_vm_funcregister("get_gl_point_smooth_hint",ring_get_gl_point_smooth_hint);
 	ring_vm_funcregister("get_gl_line_smooth_hint",ring_get_gl_line_smooth_hint);
+	ring_vm_funcregister("get_gl_polygon_smooth_hint",ring_get_gl_polygon_smooth_hint);
 }
