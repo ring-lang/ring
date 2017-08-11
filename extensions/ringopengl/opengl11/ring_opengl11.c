0ede4371656b2a65a8e50d6d1ce71344a56a8883
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:05:08 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_CURRENT_RASTER_POSITION_VALID

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -515,6 +515,11 @@ RING_FUNC(ring_get_gl_current_raster_position)
 	RING_API_RETNUMBER(GL_CURRENT_RASTER_POSITION);
 }
 
+RING_FUNC(ring_get_gl_current_raster_position_valid)
+{
+	RING_API_RETNUMBER(GL_CURRENT_RASTER_POSITION_VALID);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -618,4 +623,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_current_raster_index",ring_get_gl_current_raster_index);
 	ring_vm_funcregister("get_gl_current_raster_texture_coords",ring_get_gl_current_raster_texture_coords);
 	ring_vm_funcregister("get_gl_current_raster_position",ring_get_gl_current_raster_position);
+	ring_vm_funcregister("get_gl_current_raster_position_valid",ring_get_gl_current_raster_position_valid);
 }
