ff2f0adc4a54327ba07d402f0fc7bd57ae4201b0
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:41:30 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glRectf(GLfloat x1, GLfloat y1, GLfloat x2, GLfloat y2)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -7244,6 +7244,32 @@ RING_FUNC(ring_glRectdv)
 	glRectdv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"),(GLdouble *) RING_API_GETCPOINTER(2,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glRectf)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glRectf( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3), (GLfloat ) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7486,6 +7512,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glreadpixels",ring_glReadPixels);
 	ring_vm_funcregister("glrectd",ring_glRectd);
 	ring_vm_funcregister("glrectdv",ring_glRectdv);
+	ring_vm_funcregister("glrectf",ring_glRectf);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
