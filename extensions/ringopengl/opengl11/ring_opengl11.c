46eb71259f4f0730314eb6adacde39b4dc520aab
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:37:54 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glPixelStoref(GLenum pname, GLfloat param)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6448,6 +6448,24 @@ RING_FUNC(ring_glPixelMapusv)
 	glPixelMapusv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLsizei )  (int) RING_API_GETNUMBER(2),(GLushort *) RING_API_GETCPOINTER(3,"GLushort"));
 }
 
+
+RING_FUNC(ring_glPixelStoref)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glPixelStoref( (GLenum )  (int) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6644,6 +6662,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpixelmapfv",ring_glPixelMapfv);
 	ring_vm_funcregister("glpixelmapuiv",ring_glPixelMapuiv);
 	ring_vm_funcregister("glpixelmapusv",ring_glPixelMapusv);
+	ring_vm_funcregister("glpixelstoref",ring_glPixelStoref);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
