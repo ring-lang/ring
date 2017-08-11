7e8a11b5a8bd53be0bbcb76dd64eea02cddcfcb9
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:40:31 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glRasterPos3sv(const GLshort *v)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6974,6 +6974,20 @@ RING_FUNC(ring_glRasterPos3s)
 	glRasterPos3s( (GLshort ) RING_API_GETNUMBER(1), (GLshort ) RING_API_GETNUMBER(2), (GLshort ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glRasterPos3sv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glRasterPos3sv((GLshort *) RING_API_GETCPOINTER(1,"GLshort"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7203,6 +7217,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glrasterpos3i",ring_glRasterPos3i);
 	ring_vm_funcregister("glrasterpos3iv",ring_glRasterPos3iv);
 	ring_vm_funcregister("glrasterpos3s",ring_glRasterPos3s);
+	ring_vm_funcregister("glrasterpos3sv",ring_glRasterPos3sv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
