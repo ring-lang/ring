ada12079e4b4ab4fe76e01a0872c71bfd4424845
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:33:56 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glIndexMask(GLuint mask)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -5182,6 +5182,20 @@ RING_FUNC(ring_glHint)
 	glHint( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glIndexMask)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glIndexMask( (GLuint ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5313,6 +5327,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgettexparameterfv",ring_glGetTexParameterfv);
 	ring_vm_funcregister("glgettexparameteriv",ring_glGetTexParameteriv);
 	ring_vm_funcregister("glhint",ring_glHint);
+	ring_vm_funcregister("glindexmask",ring_glIndexMask);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
