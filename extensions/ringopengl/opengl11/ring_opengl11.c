e11ce1fa6226eec52b1d1a0694ae658215a5e228
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:31:58 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glEvalPoint1(GLint i)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -4348,6 +4348,20 @@ RING_FUNC(ring_glEvalMesh2)
 	glEvalMesh2( (GLenum )  (int) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3), (GLint ) RING_API_GETNUMBER(4), (GLint ) RING_API_GETNUMBER(5));
 }
 
+
+RING_FUNC(ring_glEvalPoint1)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glEvalPoint1( (GLint ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4436,6 +4450,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glevalcoord2fv",ring_glEvalCoord2fv);
 	ring_vm_funcregister("glevalmesh1",ring_glEvalMesh1);
 	ring_vm_funcregister("glevalmesh2",ring_glEvalMesh2);
+	ring_vm_funcregister("glevalpoint1",ring_glEvalPoint1);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
