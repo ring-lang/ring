9b0820e7435e4c4385f8ed427673195d633179ea
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:38:57 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glPushClientAttrib(GLbitfield mask)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6678,6 +6678,20 @@ RING_FUNC(ring_glPushAttrib)
 	glPushAttrib( (GLbitfield )  (int) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glPushClientAttrib)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glPushClientAttrib( (GLbitfield )  (int) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6889,6 +6903,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpopname",ring_glPopName);
 	ring_vm_funcregister("glprioritizetextures",ring_glPrioritizeTextures);
 	ring_vm_funcregister("glpushattrib",ring_glPushAttrib);
+	ring_vm_funcregister("glpushclientattrib",ring_glPushClientAttrib);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
