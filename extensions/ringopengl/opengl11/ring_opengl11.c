3b819381586b42972b8540561a4d78863f707ebe
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:33:04 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glGetMaterialiv(GLenum face, GLenum pname, GLint *params)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -4806,6 +4806,28 @@ RING_FUNC(ring_glGetMaterialfv)
 	glGetMaterialfv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLfloat *) RING_API_GETCPOINTER(3,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glGetMaterialiv)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetMaterialiv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4919,6 +4941,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetmapfv",ring_glGetMapfv);
 	ring_vm_funcregister("glgetmapiv",ring_glGetMapiv);
 	ring_vm_funcregister("glgetmaterialfv",ring_glGetMaterialfv);
+	ring_vm_funcregister("glgetmaterialiv",ring_glGetMaterialiv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
