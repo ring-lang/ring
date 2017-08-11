810b95b85df609ca167b6a8fc2534333194cbf2a
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:38:07 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glPixelTransferi(GLenum pname, GLint param)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6502,6 +6502,24 @@ RING_FUNC(ring_glPixelTransferf)
 	glPixelTransferf( (GLenum )  (int) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glPixelTransferi)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glPixelTransferi( (GLenum )  (int) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6701,6 +6719,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpixelstoref",ring_glPixelStoref);
 	ring_vm_funcregister("glpixelstorei",ring_glPixelStorei);
 	ring_vm_funcregister("glpixeltransferf",ring_glPixelTransferf);
+	ring_vm_funcregister("glpixeltransferi",ring_glPixelTransferi);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
