db98921c0e4a194e679fdde66a1f27ebb4622d9f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:05:18 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_LINE_STIPPLE

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -565,6 +565,11 @@ RING_FUNC(ring_get_gl_line_width_granularity)
 	RING_API_RETNUMBER(GL_LINE_WIDTH_GRANULARITY);
 }
 
+RING_FUNC(ring_get_gl_line_stipple)
+{
+	RING_API_RETNUMBER(GL_LINE_STIPPLE);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -678,4 +683,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_line_width",ring_get_gl_line_width);
 	ring_vm_funcregister("get_gl_line_width_range",ring_get_gl_line_width_range);
 	ring_vm_funcregister("get_gl_line_width_granularity",ring_get_gl_line_width_granularity);
+	ring_vm_funcregister("get_gl_line_stipple",ring_get_gl_line_stipple);
 }
