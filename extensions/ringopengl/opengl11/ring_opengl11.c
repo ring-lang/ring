dac72f1319e3492a2f34644e4e5e554fd6156004
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:42:43 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glStencilMask(GLuint mask)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -7566,6 +7566,20 @@ RING_FUNC(ring_glStencilFunc)
 	glStencilFunc( (GLenum )  (int) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLuint ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glStencilMask)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glStencilMask( (GLuint ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7823,6 +7837,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glselectbuffer",ring_glSelectBuffer);
 	ring_vm_funcregister("glshademodel",ring_glShadeModel);
 	ring_vm_funcregister("glstencilfunc",ring_glStencilFunc);
+	ring_vm_funcregister("glstencilmask",ring_glStencilMask);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
