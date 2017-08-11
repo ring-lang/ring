b407154da2a33a37585d4aa3374baffdb0234cba
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:07:57 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_NAME_STACK_DEPTH

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1365,6 +1365,11 @@ RING_FUNC(ring_get_gl_accum_alpha_bits)
 	RING_API_RETNUMBER(GL_ACCUM_ALPHA_BITS);
 }
 
+RING_FUNC(ring_get_gl_name_stack_depth)
+{
+	RING_API_RETNUMBER(GL_NAME_STACK_DEPTH);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1638,4 +1643,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_accum_green_bits",ring_get_gl_accum_green_bits);
 	ring_vm_funcregister("get_gl_accum_blue_bits",ring_get_gl_accum_blue_bits);
 	ring_vm_funcregister("get_gl_accum_alpha_bits",ring_get_gl_accum_alpha_bits);
+	ring_vm_funcregister("get_gl_name_stack_depth",ring_get_gl_name_stack_depth);
 }
