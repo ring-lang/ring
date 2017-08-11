879b64e3e1abcb4782201dcbb1b084b9f667782a
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:35:09 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glLightf(GLenum light, GLenum pname, GLfloat param)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -5504,6 +5504,28 @@ RING_FUNC(ring_glLightModeliv)
 	glLightModeliv( (GLenum )  (int) RING_API_GETNUMBER(1),(GLint *) RING_API_GETCPOINTER(2,"GLint"));
 }
 
+
+RING_FUNC(ring_glLightf)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glLightf( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5656,6 +5678,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gllightmodelfv",ring_glLightModelfv);
 	ring_vm_funcregister("gllightmodeli",ring_glLightModeli);
 	ring_vm_funcregister("gllightmodeliv",ring_glLightModeliv);
+	ring_vm_funcregister("gllightf",ring_glLightf);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
