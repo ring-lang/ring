83361dca41a737381446107af298d9f98669f2ac
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:30:11 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glColor4ui(GLuint red, GLuint green, GLuint blue, GLuint alpha)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -3530,6 +3530,32 @@ RING_FUNC(ring_glColor4ubv)
 	glColor4ubv((GLubyte *) RING_API_GETCPOINTER(1,"GLubyte"));
 }
 
+
+RING_FUNC(ring_glColor4ui)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glColor4ui( (GLuint ) RING_API_GETNUMBER(1), (GLuint ) RING_API_GETNUMBER(2), (GLuint ) RING_API_GETNUMBER(3), (GLuint ) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3577,6 +3603,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glcolor4sv",ring_glColor4sv);
 	ring_vm_funcregister("glcolor4ub",ring_glColor4ub);
 	ring_vm_funcregister("glcolor4ubv",ring_glColor4ubv);
+	ring_vm_funcregister("glcolor4ui",ring_glColor4ui);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
