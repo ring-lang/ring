3c6844f574d61131f14e6d4245ff79d88e019091
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:05:33 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_LIGHT_MODEL_AMBIENT

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -650,6 +650,11 @@ RING_FUNC(ring_get_gl_light_model_two_side)
 	RING_API_RETNUMBER(GL_LIGHT_MODEL_TWO_SIDE);
 }
 
+RING_FUNC(ring_get_gl_light_model_ambient)
+{
+	RING_API_RETNUMBER(GL_LIGHT_MODEL_AMBIENT);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -780,4 +785,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_lighting",ring_get_gl_lighting);
 	ring_vm_funcregister("get_gl_light_model_local_viewer",ring_get_gl_light_model_local_viewer);
 	ring_vm_funcregister("get_gl_light_model_two_side",ring_get_gl_light_model_two_side);
+	ring_vm_funcregister("get_gl_light_model_ambient",ring_get_gl_light_model_ambient);
 }
