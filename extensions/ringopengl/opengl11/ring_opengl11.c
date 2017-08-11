ff815820397216aac8f2d9e3337109490344ff0f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:11:26 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_C4F_N3F_V3F

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2140,6 +2140,11 @@ RING_FUNC(ring_get_gl_n3f_v3f)
 	RING_API_RETNUMBER(GL_N3F_V3F);
 }
 
+RING_FUNC(ring_get_gl_c4f_n3f_v3f)
+{
+	RING_API_RETNUMBER(GL_C4F_N3F_V3F);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -2568,4 +2573,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_c4ub_v3f",ring_get_gl_c4ub_v3f);
 	ring_vm_funcregister("get_gl_c3f_v3f",ring_get_gl_c3f_v3f);
 	ring_vm_funcregister("get_gl_n3f_v3f",ring_get_gl_n3f_v3f);
+	ring_vm_funcregister("get_gl_c4f_n3f_v3f",ring_get_gl_c4f_n3f_v3f);
 }
