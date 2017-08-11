12c0f6ea09fe94c123ff90376c8e9dd56bcb98c3
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:33:17 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glGetPolygonStipple(GLubyte *mask)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -4900,6 +4900,20 @@ RING_FUNC(ring_glGetPointerv)
 	glGetPointerv( (GLenum )  (int) RING_API_GETNUMBER(1),(void *) RING_API_GETCPOINTER(2,"void"));
 }
 
+
+RING_FUNC(ring_glGetPolygonStipple)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glGetPolygonStipple((GLubyte *) RING_API_GETCPOINTER(1,"GLubyte"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5018,6 +5032,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgetpixelmapuiv",ring_glGetPixelMapuiv);
 	ring_vm_funcregister("glgetpixelmapusv",ring_glGetPixelMapusv);
 	ring_vm_funcregister("glgetpointerv",ring_glGetPointerv);
+	ring_vm_funcregister("glgetpolygonstipple",ring_glGetPolygonStipple);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
