dedb060e0abfb5249c15f9fb94d652161942c3ab
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:06:01 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_MODELVIEW_STACK_DEPTH

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -800,6 +800,11 @@ RING_FUNC(ring_get_gl_viewport)
 	RING_API_RETNUMBER(GL_VIEWPORT);
 }
 
+RING_FUNC(ring_get_gl_modelview_stack_depth)
+{
+	RING_API_RETNUMBER(GL_MODELVIEW_STACK_DEPTH);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -960,4 +965,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_matrix_mode",ring_get_gl_matrix_mode);
 	ring_vm_funcregister("get_gl_normalize",ring_get_gl_normalize);
 	ring_vm_funcregister("get_gl_viewport",ring_get_gl_viewport);
+	ring_vm_funcregister("get_gl_modelview_stack_depth",ring_get_gl_modelview_stack_depth);
 }
