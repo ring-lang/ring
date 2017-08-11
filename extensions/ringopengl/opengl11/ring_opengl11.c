dd4b35f67042dd15cee8bf5eb90fe8bbd073ba0c
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:06:58 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_PIXEL_MAP_G_TO_G_SIZE

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1090,6 +1090,11 @@ RING_FUNC(ring_get_gl_pixel_map_r_to_r_size)
 	RING_API_RETNUMBER(GL_PIXEL_MAP_R_TO_R_SIZE);
 }
 
+RING_FUNC(ring_get_gl_pixel_map_g_to_g_size)
+{
+	RING_API_RETNUMBER(GL_PIXEL_MAP_G_TO_G_SIZE);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1308,4 +1313,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_pixel_map_i_to_b_size",ring_get_gl_pixel_map_i_to_b_size);
 	ring_vm_funcregister("get_gl_pixel_map_i_to_a_size",ring_get_gl_pixel_map_i_to_a_size);
 	ring_vm_funcregister("get_gl_pixel_map_r_to_r_size",ring_get_gl_pixel_map_r_to_r_size);
+	ring_vm_funcregister("get_gl_pixel_map_g_to_g_size",ring_get_gl_pixel_map_g_to_g_size);
 }
