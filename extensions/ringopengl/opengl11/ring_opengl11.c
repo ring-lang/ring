a70a6257ed91aaf984bbe3178d8496faa4a096a2
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:36:40 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glMultMatrixd(const GLdouble *m)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6086,6 +6086,20 @@ RING_FUNC(ring_glMatrixMode)
 	glMatrixMode( (GLenum )  (int) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glMultMatrixd)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glMultMatrixd((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6263,6 +6277,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glmateriali",ring_glMateriali);
 	ring_vm_funcregister("glmaterialiv",ring_glMaterialiv);
 	ring_vm_funcregister("glmatrixmode",ring_glMatrixMode);
+	ring_vm_funcregister("glmultmatrixd",ring_glMultMatrixd);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
