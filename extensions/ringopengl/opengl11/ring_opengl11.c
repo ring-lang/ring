25133b2381bc8c7ff1207b1536db04b57fd7e69e
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:42:28 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glSelectBuffer(GLsizei size, GLuint *buffer)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -7512,6 +7512,24 @@ RING_FUNC(ring_glScissor)
 	glScissor( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLsizei )  (int) RING_API_GETNUMBER(3), (GLsizei )  (int) RING_API_GETNUMBER(4));
 }
 
+
+RING_FUNC(ring_glSelectBuffer)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glSelectBuffer( (GLsizei )  (int) RING_API_GETNUMBER(1),(GLuint *) RING_API_GETCPOINTER(2,"GLuint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7766,6 +7784,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glscaled",ring_glScaled);
 	ring_vm_funcregister("glscalef",ring_glScalef);
 	ring_vm_funcregister("glscissor",ring_glScissor);
+	ring_vm_funcregister("glselectbuffer",ring_glSelectBuffer);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
