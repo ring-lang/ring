8b34fbf97b25149946c47ebe2f81c5fccebe0ddb
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:07:39 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_MAX_NAME_STACK_DEPTH

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1280,6 +1280,11 @@ RING_FUNC(ring_get_gl_max_modelview_stack_depth)
 	RING_API_RETNUMBER(GL_MAX_MODELVIEW_STACK_DEPTH);
 }
 
+RING_FUNC(ring_get_gl_max_name_stack_depth)
+{
+	RING_API_RETNUMBER(GL_MAX_NAME_STACK_DEPTH);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1536,4 +1541,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_max_pixel_map_table",ring_get_gl_max_pixel_map_table);
 	ring_vm_funcregister("get_gl_max_attrib_stack_depth",ring_get_gl_max_attrib_stack_depth);
 	ring_vm_funcregister("get_gl_max_modelview_stack_depth",ring_get_gl_max_modelview_stack_depth);
+	ring_vm_funcregister("get_gl_max_name_stack_depth",ring_get_gl_max_name_stack_depth);
 }
