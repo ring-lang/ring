75126652a04aba3e25be5429f97eb3932a19ce46
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:29:36 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glColor3ubv(const GLubyte *v)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -3204,6 +3204,20 @@ RING_FUNC(ring_glColor3ub)
 	glColor3ub( (GLubyte ) RING_API_GETNUMBER(1), (GLubyte ) RING_API_GETNUMBER(2), (GLubyte ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glColor3ubv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glColor3ubv((GLubyte *) RING_API_GETCPOINTER(1,"GLubyte"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3234,6 +3248,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glcolor3s",ring_glColor3s);
 	ring_vm_funcregister("glcolor3sv",ring_glColor3sv);
 	ring_vm_funcregister("glcolor3ub",ring_glColor3ub);
+	ring_vm_funcregister("glcolor3ubv",ring_glColor3ubv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
