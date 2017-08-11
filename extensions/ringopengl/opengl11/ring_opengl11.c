8b3286a7e6fe8098a0440ed63a4363f1b09f0d29
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:12:20 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_LUMINANCE16_ALPHA16

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2335,6 +2335,11 @@ RING_FUNC(ring_get_gl_luminance12_alpha12)
 	RING_API_RETNUMBER(GL_LUMINANCE12_ALPHA12);
 }
 
+RING_FUNC(ring_get_gl_luminance16_alpha16)
+{
+	RING_API_RETNUMBER(GL_LUMINANCE16_ALPHA16);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -2802,4 +2807,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_luminance8_alpha8",ring_get_gl_luminance8_alpha8);
 	ring_vm_funcregister("get_gl_luminance12_alpha4",ring_get_gl_luminance12_alpha4);
 	ring_vm_funcregister("get_gl_luminance12_alpha12",ring_get_gl_luminance12_alpha12);
+	ring_vm_funcregister("get_gl_luminance16_alpha16",ring_get_gl_luminance16_alpha16);
 }
