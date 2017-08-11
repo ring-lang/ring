8c78176116a5038b0b4c616adf4afbf32c49d61b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:10:47 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_OBJECT_LINEAR

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2005,6 +2005,11 @@ RING_FUNC(ring_get_gl_eye_linear)
 	RING_API_RETNUMBER(GL_EYE_LINEAR);
 }
 
+RING_FUNC(ring_get_gl_object_linear)
+{
+	RING_API_RETNUMBER(GL_OBJECT_LINEAR);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -2406,4 +2411,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_texture_env_color",ring_get_gl_texture_env_color);
 	ring_vm_funcregister("get_gl_texture_env",ring_get_gl_texture_env);
 	ring_vm_funcregister("get_gl_eye_linear",ring_get_gl_eye_linear);
+	ring_vm_funcregister("get_gl_object_linear",ring_get_gl_object_linear);
 }
