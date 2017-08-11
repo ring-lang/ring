52013763941483d29765edf2a330cccf8bd69123
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:09:23 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_COPY

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1695,6 +1695,11 @@ RING_FUNC(ring_get_gl_and_reverse)
 	RING_API_RETNUMBER(GL_AND_REVERSE);
 }
 
+RING_FUNC(ring_get_gl_copy)
+{
+	RING_API_RETNUMBER(GL_COPY);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -2034,4 +2039,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_clear",ring_get_gl_clear);
 	ring_vm_funcregister("get_gl_and",ring_get_gl_and);
 	ring_vm_funcregister("get_gl_and_reverse",ring_get_gl_and_reverse);
+	ring_vm_funcregister("get_gl_copy",ring_get_gl_copy);
 }
