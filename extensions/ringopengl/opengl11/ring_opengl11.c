7c01ef61ba2d9e01bf42b06ad12d1997c8350ba9
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:08:25 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_MAP2_GRID_DOMAIN

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1475,6 +1475,11 @@ RING_FUNC(ring_get_gl_map1_grid_segments)
 	RING_API_RETNUMBER(GL_MAP1_GRID_SEGMENTS);
 }
 
+RING_FUNC(ring_get_gl_map2_grid_domain)
+{
+	RING_API_RETNUMBER(GL_MAP2_GRID_DOMAIN);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1770,4 +1775,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_map2_vertex_4",ring_get_gl_map2_vertex_4);
 	ring_vm_funcregister("get_gl_map1_grid_domain",ring_get_gl_map1_grid_domain);
 	ring_vm_funcregister("get_gl_map1_grid_segments",ring_get_gl_map1_grid_segments);
+	ring_vm_funcregister("get_gl_map2_grid_domain",ring_get_gl_map2_grid_domain);
 }
