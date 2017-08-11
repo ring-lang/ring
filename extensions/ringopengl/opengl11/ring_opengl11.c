9c86b1c86dc6cb95395ce57ad69d5e1f16e4572f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:34:32 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glIndexubv(const GLubyte *c)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -5344,6 +5344,20 @@ RING_FUNC(ring_glIndexub)
 	glIndexub( (GLubyte ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glIndexubv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glIndexubv((GLubyte *) RING_API_GETCPOINTER(1,"GLubyte"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5486,6 +5500,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glindexs",ring_glIndexs);
 	ring_vm_funcregister("glindexsv",ring_glIndexsv);
 	ring_vm_funcregister("glindexub",ring_glIndexub);
+	ring_vm_funcregister("glindexubv",ring_glIndexubv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
