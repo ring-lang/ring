7fb1daa8aee712b723e8747a845e8e17554c0c61
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:39:15 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glRasterPos2dv(const GLdouble *v)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6734,6 +6734,20 @@ RING_FUNC(ring_glRasterPos2d)
 	glRasterPos2d( (GLdouble ) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glRasterPos2dv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glRasterPos2dv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6949,6 +6963,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glpushmatrix",ring_glPushMatrix);
 	ring_vm_funcregister("glpushname",ring_glPushName);
 	ring_vm_funcregister("glrasterpos2d",ring_glRasterPos2d);
+	ring_vm_funcregister("glrasterpos2dv",ring_glRasterPos2dv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
