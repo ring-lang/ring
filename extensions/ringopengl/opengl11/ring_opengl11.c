b2ebd6b7a77974f830aef8f09b5f852c806f6489
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:41:56 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glRectsv(const GLshort *v1, const GLshort *v2)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -7358,6 +7358,24 @@ RING_FUNC(ring_glRects)
 	glRects( (GLshort ) RING_API_GETNUMBER(1), (GLshort ) RING_API_GETNUMBER(2), (GLshort ) RING_API_GETNUMBER(3), (GLshort ) RING_API_GETNUMBER(4));
 }
 
+
+RING_FUNC(ring_glRectsv)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glRectsv((GLshort *) RING_API_GETCPOINTER(1,"GLshort"),(GLshort *) RING_API_GETCPOINTER(2,"GLshort"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -7605,6 +7623,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glrecti",ring_glRecti);
 	ring_vm_funcregister("glrectiv",ring_glRectiv);
 	ring_vm_funcregister("glrects",ring_glRects);
+	ring_vm_funcregister("glrectsv",ring_glRectsv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
