fb1b9a9816b5e95a5fa6c9aa49fab2c92a40f55b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:47:56 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glVertex2dv(const GLdouble *v)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -8714,6 +8714,20 @@ RING_FUNC(ring_glVertex2d)
 	glVertex2d( (GLdouble ) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glVertex2dv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex2dv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -9027,6 +9041,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltranslated",ring_glTranslated);
 	ring_vm_funcregister("gltranslatef",ring_glTranslatef);
 	ring_vm_funcregister("glvertex2d",ring_glVertex2d);
+	ring_vm_funcregister("glvertex2dv",ring_glVertex2dv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
