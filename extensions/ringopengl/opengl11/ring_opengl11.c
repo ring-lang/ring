99629d0fa0662fe44d7bbebffae8415dc941d49d
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:33:59 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glIndexPointer(GLenum type, GLsizei stride, const void *pointer)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -5196,6 +5196,28 @@ RING_FUNC(ring_glIndexMask)
 	glIndexMask( (GLuint ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glIndexPointer)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glIndexPointer( (GLenum )  (int) RING_API_GETNUMBER(1), (GLsizei )  (int) RING_API_GETNUMBER(2),(void *) RING_API_GETCPOINTER(3,"void"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5328,6 +5350,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glgettexparameteriv",ring_glGetTexParameteriv);
 	ring_vm_funcregister("glhint",ring_glHint);
 	ring_vm_funcregister("glindexmask",ring_glIndexMask);
+	ring_vm_funcregister("glindexpointer",ring_glIndexPointer);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
