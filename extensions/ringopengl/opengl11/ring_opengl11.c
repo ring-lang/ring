f2cc5ca0aca3a746bc30d3c0b3388d6aea57637d
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:30:54 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glDisable(GLenum cap)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -3962,6 +3962,20 @@ RING_FUNC(ring_glDepthRange)
 	glDepthRange( (GLclampd ) RING_API_GETNUMBER(1), (GLclampd ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glDisable)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glDisable( (GLenum )  (int) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4027,6 +4041,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gldepthfunc",ring_glDepthFunc);
 	ring_vm_funcregister("gldepthmask",ring_glDepthMask);
 	ring_vm_funcregister("gldepthrange",ring_glDepthRange);
+	ring_vm_funcregister("gldisable",ring_glDisable);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
