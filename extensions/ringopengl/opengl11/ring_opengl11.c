232d2120a656977703e787bc32f1aa5106ae12be
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:34:15 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glIndexi(GLint c)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -5274,6 +5274,20 @@ RING_FUNC(ring_glIndexfv)
 	glIndexfv((GLfloat *) RING_API_GETCPOINTER(1,"GLfloat"));
 }
 
+
+RING_FUNC(ring_glIndexi)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glIndexi( (GLint ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -5411,6 +5425,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glindexdv",ring_glIndexdv);
 	ring_vm_funcregister("glindexf",ring_glIndexf);
 	ring_vm_funcregister("glindexfv",ring_glIndexfv);
+	ring_vm_funcregister("glindexi",ring_glIndexi);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
