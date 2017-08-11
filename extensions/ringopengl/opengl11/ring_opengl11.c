0da0c88404e13e50dc9bf1b07a64bca0e31fcd19
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:29:28 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glColor3s(GLshort red, GLshort green, GLshort blue)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -3146,6 +3146,28 @@ RING_FUNC(ring_glColor3iv)
 	glColor3iv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
 }
 
+
+RING_FUNC(ring_glColor3s)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glColor3s( (GLshort ) RING_API_GETNUMBER(1), (GLshort ) RING_API_GETNUMBER(2), (GLshort ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -3173,6 +3195,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glcolor3fv",ring_glColor3fv);
 	ring_vm_funcregister("glcolor3i",ring_glColor3i);
 	ring_vm_funcregister("glcolor3iv",ring_glColor3iv);
+	ring_vm_funcregister("glcolor3s",ring_glColor3s);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
