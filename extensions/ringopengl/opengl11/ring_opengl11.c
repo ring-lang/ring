c3bb2c34c66f93a113e349538cad1a836d67b5e3
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:08:32 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_SELECTION_BUFFER_POINTER

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1510,6 +1510,11 @@ RING_FUNC(ring_get_gl_feedback_buffer_type)
 	RING_API_RETNUMBER(GL_FEEDBACK_BUFFER_TYPE);
 }
 
+RING_FUNC(ring_get_gl_selection_buffer_pointer)
+{
+	RING_API_RETNUMBER(GL_SELECTION_BUFFER_POINTER);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1812,4 +1817,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_feedback_buffer_pointer",ring_get_gl_feedback_buffer_pointer);
 	ring_vm_funcregister("get_gl_feedback_buffer_size",ring_get_gl_feedback_buffer_size);
 	ring_vm_funcregister("get_gl_feedback_buffer_type",ring_get_gl_feedback_buffer_type);
+	ring_vm_funcregister("get_gl_selection_buffer_pointer",ring_get_gl_selection_buffer_pointer);
 }
