78941d73242f3966c8039ca67b118e33c3119ab4
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:13:22 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_COLOR_ARRAY_SIZE

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2545,6 +2545,11 @@ RING_FUNC(ring_get_gl_normal_array_stride)
 	RING_API_RETNUMBER(GL_NORMAL_ARRAY_STRIDE);
 }
 
+RING_FUNC(ring_get_gl_color_array_size)
+{
+	RING_API_RETNUMBER(GL_COLOR_ARRAY_SIZE);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -3054,4 +3059,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_vertex_array_stride",ring_get_gl_vertex_array_stride);
 	ring_vm_funcregister("get_gl_normal_array_type",ring_get_gl_normal_array_type);
 	ring_vm_funcregister("get_gl_normal_array_stride",ring_get_gl_normal_array_stride);
+	ring_vm_funcregister("get_gl_color_array_size",ring_get_gl_color_array_size);
 }
