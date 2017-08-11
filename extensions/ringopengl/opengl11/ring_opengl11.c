7b94026e809691480f3d0e96836de78288a7b265
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:36:11 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glMapGrid1f(GLint un, GLfloat u1, GLfloat u2)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -5894,6 +5894,28 @@ RING_FUNC(ring_glMapGrid1d)
 	glMapGrid1d( (GLint ) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2), (GLdouble ) RING_API_GETNUMBER(3));
 }
 
+
+RING_FUNC(ring_glMapGrid1f)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glMapGrid1f( (GLint ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6063,6 +6085,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glmap2d",ring_glMap2d);
 	ring_vm_funcregister("glmap2f",ring_glMap2f);
 	ring_vm_funcregister("glmapgrid1d",ring_glMapGrid1d);
+	ring_vm_funcregister("glmapgrid1f",ring_glMapGrid1f);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
