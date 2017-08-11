cb9eb94d7b20acd6bf34fe5e66f9696d057727bb
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:37:07 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glNormal3f(GLfloat nx, GLfloat ny, GLfloat nz)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -6204,6 +6204,28 @@ RING_FUNC(ring_glNormal3dv)
 	glNormal3dv((GLdouble *) RING_API_GETCPOINTER(1,"GLdouble"));
 }
 
+
+RING_FUNC(ring_glNormal3f)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glNormal3f( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -6388,6 +6410,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glnormal3bv",ring_glNormal3bv);
 	ring_vm_funcregister("glnormal3d",ring_glNormal3d);
 	ring_vm_funcregister("glnormal3dv",ring_glNormal3dv);
+	ring_vm_funcregister("glnormal3f",ring_glNormal3f);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
