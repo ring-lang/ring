896349e095d069f5ecdbc3d398aa3895b6a416ae
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:44:08 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glTexCoord2iv(const GLint *v)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -7796,6 +7796,20 @@ RING_FUNC(ring_glTexCoord2i)
 	glTexCoord2i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glTexCoord2iv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glTexCoord2iv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -8068,6 +8082,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltexcoord2f",ring_glTexCoord2f);
 	ring_vm_funcregister("gltexcoord2fv",ring_glTexCoord2fv);
 	ring_vm_funcregister("gltexcoord2i",ring_glTexCoord2i);
+	ring_vm_funcregister("gltexcoord2iv",ring_glTexCoord2iv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
