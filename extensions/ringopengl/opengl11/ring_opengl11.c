89e1c9833933ada28a20c12b4c0151c8257f6ba1
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:10:25 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_DECR

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1925,6 +1925,11 @@ RING_FUNC(ring_get_gl_incr)
 	RING_API_RETNUMBER(GL_INCR);
 }
 
+RING_FUNC(ring_get_gl_decr)
+{
+	RING_API_RETNUMBER(GL_DECR);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -2310,4 +2315,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_keep",ring_get_gl_keep);
 	ring_vm_funcregister("get_gl_replace",ring_get_gl_replace);
 	ring_vm_funcregister("get_gl_incr",ring_get_gl_incr);
+	ring_vm_funcregister("get_gl_decr",ring_get_gl_decr);
 }
