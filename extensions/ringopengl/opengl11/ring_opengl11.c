dacc84f90bd813167caf4e73f036ca412229e017
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:06:42 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_PIXEL_MAP_I_TO_R

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -1015,6 +1015,11 @@ RING_FUNC(ring_get_gl_pixel_map_s_to_s)
 	RING_API_RETNUMBER(GL_PIXEL_MAP_S_TO_S);
 }
 
+RING_FUNC(ring_get_gl_pixel_map_i_to_r)
+{
+	RING_API_RETNUMBER(GL_PIXEL_MAP_I_TO_R);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -1218,4 +1223,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_texture_gen_q",ring_get_gl_texture_gen_q);
 	ring_vm_funcregister("get_gl_pixel_map_i_to_i",ring_get_gl_pixel_map_i_to_i);
 	ring_vm_funcregister("get_gl_pixel_map_s_to_s",ring_get_gl_pixel_map_s_to_s);
+	ring_vm_funcregister("get_gl_pixel_map_i_to_r",ring_get_gl_pixel_map_i_to_r);
 }
