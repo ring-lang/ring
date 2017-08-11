6c4cbcbcb08a3d34140b5b44d0a05124bb6ef1db
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:32:26 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : GLuint glGenLists(GLsizei range)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -4542,6 +4542,20 @@ RING_FUNC(ring_glFrustum)
 	glFrustum( (GLdouble ) RING_API_GETNUMBER(1), (GLdouble ) RING_API_GETNUMBER(2), (GLdouble ) RING_API_GETNUMBER(3), (GLdouble ) RING_API_GETNUMBER(4), (GLdouble ) RING_API_GETNUMBER(5), (GLdouble ) RING_API_GETNUMBER(6));
 }
 
+
+RING_FUNC(ring_glGenLists)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETNUMBER(glGenLists( (GLsizei )  (int) RING_API_GETNUMBER(1)));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4641,6 +4655,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glfogiv",ring_glFogiv);
 	ring_vm_funcregister("glfrontface",ring_glFrontFace);
 	ring_vm_funcregister("glfrustum",ring_glFrustum);
+	ring_vm_funcregister("glgenlists",ring_glGenLists);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
