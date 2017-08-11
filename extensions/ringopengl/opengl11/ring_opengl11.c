ef363c2b349227247ee22466eda78b989baa6b14
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:32:15 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glFogfv(GLenum pname, const GLfloat *params)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -4440,6 +4440,24 @@ RING_FUNC(ring_glFogf)
 	glFogf( (GLenum )  (int) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_glFogfv)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glFogfv( (GLenum )  (int) RING_API_GETNUMBER(1),(GLfloat *) RING_API_GETCPOINTER(2,"GLfloat"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -4534,6 +4552,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glfinish",ring_glFinish);
 	ring_vm_funcregister("glflush",ring_glFlush);
 	ring_vm_funcregister("glfogf",ring_glFogf);
+	ring_vm_funcregister("glfogfv",ring_glFogfv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
