aec007d62182ddb3a03cf837f8b497ac2a554be8
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:05:34 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_SHADE_MODEL

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -655,6 +655,11 @@ RING_FUNC(ring_get_gl_light_model_ambient)
 	RING_API_RETNUMBER(GL_LIGHT_MODEL_AMBIENT);
 }
 
+RING_FUNC(ring_get_gl_shade_model)
+{
+	RING_API_RETNUMBER(GL_SHADE_MODEL);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -786,4 +791,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_light_model_local_viewer",ring_get_gl_light_model_local_viewer);
 	ring_vm_funcregister("get_gl_light_model_two_side",ring_get_gl_light_model_two_side);
 	ring_vm_funcregister("get_gl_light_model_ambient",ring_get_gl_light_model_ambient);
+	ring_vm_funcregister("get_gl_shade_model",ring_get_gl_shade_model);
 }
