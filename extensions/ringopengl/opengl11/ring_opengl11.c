71f5fbf97ae71d8c92427dbf606c8803cc4904ca
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:28:55 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glBegin(GLenum mode)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2752,12 +2752,27 @@ RING_FUNC(ring_glArrayElement)
 	glArrayElement( (GLint ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glBegin)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glBegin( (GLenum )  (int) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
 	ring_vm_funcregister("glalphafunc",ring_glAlphaFunc);
 	ring_vm_funcregister("glaretexturesresident",ring_glAreTexturesResident);
 	ring_vm_funcregister("glarrayelement",ring_glArrayElement);
+	ring_vm_funcregister("glbegin",ring_glBegin);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
