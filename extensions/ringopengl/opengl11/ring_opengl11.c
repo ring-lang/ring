d7b59ed8c8f955a7029965723f418aba623ab2a2
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:49:59 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glVertex4iv(const GLint *v)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -9074,6 +9074,20 @@ RING_FUNC(ring_glVertex4i)
 	glVertex4i( (GLint ) RING_API_GETNUMBER(1), (GLint ) RING_API_GETNUMBER(2), (GLint ) RING_API_GETNUMBER(3), (GLint ) RING_API_GETNUMBER(4));
 }
 
+
+RING_FUNC(ring_glVertex4iv)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glVertex4iv((GLint *) RING_API_GETCPOINTER(1,"GLint"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -9407,6 +9421,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glvertex4f",ring_glVertex4f);
 	ring_vm_funcregister("glvertex4fv",ring_glVertex4fv);
 	ring_vm_funcregister("glvertex4i",ring_glVertex4i);
+	ring_vm_funcregister("glvertex4iv",ring_glVertex4iv);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
