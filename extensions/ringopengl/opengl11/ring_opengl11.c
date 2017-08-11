259a4533a677ae5c5255c3ea3abc41b2d623cd6f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:46:17 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glTexGend(GLenum coord, GLenum pname, GLdouble param)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -8260,6 +8260,28 @@ RING_FUNC(ring_glTexEnviv)
 	glTexEnviv( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2),(GLint *) RING_API_GETCPOINTER(3,"GLint"));
 }
 
+
+RING_FUNC(ring_glTexGend)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glTexGend( (GLenum )  (int) RING_API_GETNUMBER(1), (GLenum )  (int) RING_API_GETNUMBER(2), (GLdouble ) RING_API_GETNUMBER(3));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -8556,6 +8578,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("gltexenvfv",ring_glTexEnvfv);
 	ring_vm_funcregister("gltexenvi",ring_glTexEnvi);
 	ring_vm_funcregister("gltexenviv",ring_glTexEnviv);
+	ring_vm_funcregister("gltexgend",ring_glTexGend);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
