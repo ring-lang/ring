3f8b0ccf0dccfbb77ff6922c9e072e95752552f4
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:29:06 +0300

    Update RingOpenGL 1.1 - Add Function (Source Code) : void glClearAccum(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -2890,6 +2890,32 @@ RING_FUNC(ring_glClear)
 	glClear( (GLbitfield )  (int) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_glClearAccum)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(4) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	glClearAccum( (GLfloat ) RING_API_GETNUMBER(1), (GLfloat ) RING_API_GETNUMBER(2), (GLfloat ) RING_API_GETNUMBER(3), (GLfloat ) RING_API_GETNUMBER(4));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("glaccum",ring_glAccum);
@@ -2903,6 +2929,7 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("glcalllist",ring_glCallList);
 	ring_vm_funcregister("glcalllists",ring_glCallLists);
 	ring_vm_funcregister("glclear",ring_glClear);
+	ring_vm_funcregister("glclearaccum",ring_glClearAccum);
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
 	ring_vm_funcregister("get_gl_false",ring_get_gl_false);
 	ring_vm_funcregister("get_gl_logic_op",ring_get_gl_logic_op);
