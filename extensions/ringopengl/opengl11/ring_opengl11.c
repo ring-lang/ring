2f3b08fc776c69ba1a38f70ae04d2694d3626def
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-11 05:05:26 +0300

    Update RingOpenGL 1.1 - Add Constant (Source Code) : GL_POLYGON_STIPPLE

--- extensions/ringopengl/opengl11/ring_opengl11.c
+++ extensions/ringopengl/opengl11/ring_opengl11.c
@@ -610,6 +610,11 @@ RING_FUNC(ring_get_gl_polygon_smooth)
 	RING_API_RETNUMBER(GL_POLYGON_SMOOTH);
 }
 
+RING_FUNC(ring_get_gl_polygon_stipple)
+{
+	RING_API_RETNUMBER(GL_POLYGON_STIPPLE);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_gl_zero",ring_get_gl_zero);
@@ -732,4 +737,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_gl_list_index",ring_get_gl_list_index);
 	ring_vm_funcregister("get_gl_polygon_mode",ring_get_gl_polygon_mode);
 	ring_vm_funcregister("get_gl_polygon_smooth",ring_get_gl_polygon_smooth);
+	ring_vm_funcregister("get_gl_polygon_stipple",ring_get_gl_polygon_stipple);
 }
