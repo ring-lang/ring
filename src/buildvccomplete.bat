2bdd33dc6f566f775b465697890ca05bc3eff240
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-16 00:36:45 +0300

    Update buildvccomplete.bat

--- src/buildvccomplete.bat
+++ src/buildvccomplete.bat
@@ -3,7 +3,8 @@ rem call vc.bat
 call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
 
 cls
-cl /DEBUG /Z7 /c /Ox ring.c  ^
+rem /DEBUG /Z7 
+cl /c /Ox ring.c  ^
 ring_string.c ring_list.c ring_item.c ring_items.c ring_hashtable.c ring_state.c ring_scanner.c ring_parser.c ring_hashlib.c ring_vmgc.c ^
 ring_stmt.c ring_expr.c ring_codegen.c ring_vm.c ring_vmexpr.c ring_vmvars.c ^
 ring_vmlists.c ring_vmfuncs.c ring_api.c ring_vmoop.c ring_vmcui.c ^
@@ -22,7 +23,8 @@ ring_ext.obj ring_vmdll.obj ring_objfile.obj
 
 copy ..\lib\ring.dll ..\bin\
 
-cl /DEBUG /Z7 ring.c ..\lib\ring.lib -I"..\include" /link /SUBSYSTEM:CONSOLE,"5.01" /OUT:..\bin\ring.exe 
+rem /DEBUG /Z7
+cl ring.c ..\lib\ring.lib -I"..\include" /link /SUBSYSTEM:CONSOLE,"5.01" /OUT:..\bin\ring.exe 
 
 del *.obj
 
