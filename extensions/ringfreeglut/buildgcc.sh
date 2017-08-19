6d5dd01a2e270d98faba0cdc566b3d9a8da4718f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 05:50:54 +0300

    Update RingFreeGLUT - build scripts.

--- extensions/ringfreeglut/buildgcc.sh
+++ extensions/ringfreeglut/buildgcc.sh
@@ -1,5 +1,5 @@
 gcc -c -fpic ring_freeglut.c -I $PWD/../../include
-gcc -shared -o $PWD/../../lib/libringfreeglut.so ring_freeglut.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -lGL -lGLU -lglut 
+gcc -shared -o $PWD/../../lib/libring_freeglut.so ring_freeglut.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -lGL -lGLU -lglut 
 
  
 
