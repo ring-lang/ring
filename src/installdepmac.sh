d1b8a4bf7792e3a1181e7d460298eb5796078fe2
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 10:37:09 +0300

    Update installdepmac.sh

--- src/installdepmac.sh
+++ src/installdepmac.sh
@@ -1,2 +1,2 @@
-brew install unixodbc mysql-connector-c allegro openssl homebrew/versions/qt55 freeglut
+brew install unixodbc mysql-connector-c allegro openssl homebrew/versions/qt55 freeglut glew
 brew link --force qt55
