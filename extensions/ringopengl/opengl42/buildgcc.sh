f79504d45378ea423735e02750794c853c4b2602
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 09:13:03 +0300

    Update RingOpenGL 4.2 - Build scripts for Linux.

---
+++ extensions/ringopengl/opengl42/buildgcc.sh
@@ -0,0 +1,6 @@
+gcc -c -fpic ring_opengl42.c -I $PWD/../../../include
+gcc -shared -o $PWD/../../../lib/libring_opengl42.so ring_opengl42.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -lGL -lGLU -lglut
+
+ 
+
+
