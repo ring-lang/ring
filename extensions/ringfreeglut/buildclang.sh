ad4ff3f8b7c119c7a80ad24a6f45babfedc624f7
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 10:43:24 +0300

    Update RingFreeGLUT - buildclang.sh

--- extensions/ringfreeglut/buildclang.sh
+++ extensions/ringfreeglut/buildclang.sh
@@ -1,5 +1,5 @@
 clang -c -fpic ring_freeglut.c -I $PWD/../../include -I /usr/local/include
-clang -dynamiclib -o $PWD/../../lib/libring_freeglut.dylib ring_freeglut.o -framework GLUT -L $PWD/../../lib  -lring 
+clang -dynamiclib -o $PWD/../../lib/libring_freeglut.dylib ring_freeglut.o -framework OpenGL -framework GLUT -L $PWD/../../lib  -lring 
 
  
 
