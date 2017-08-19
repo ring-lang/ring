6ea51dce4d6225aedf3cb9a88243dd90f430756c
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 12:25:51 +0300

    Update RingOpenGL 3.2 - Build script for MacOS X

---
+++ extensions/ringopengl/opengl32/buildclang.sh
@@ -0,0 +1,6 @@
+clang -c -fpic ring_opengl32.c -I $PWD/../../../include -I /usr/local/include
+clang -dynamiclib -o $PWD/../../../lib/libring_opengl32.dylib ring_opengl32.o /usr/local/lib/libglew.dylib -framework OpenGL -framework GLUT -L $PWD/../../../lib  -lring 
+
+ 
+
+
