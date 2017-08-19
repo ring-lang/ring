5f9a33ba86c6ab6ea37e0afc32c778a6d971ea03
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 06:00:24 +0300

    Update RingOpenGL - Add buildgcc.sh

---
+++ extensions/ringopengl/opengl21/buildgcc.sh
@@ -0,0 +1,6 @@
+gcc -c -fpic ring_opengl21.c -I $PWD/../../../include
+gcc -shared -o $PWD/../../../lib/libring_opengl21.so ring_opengl21.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -lGL -lGLU -lglut 
+
+ 
+
+
