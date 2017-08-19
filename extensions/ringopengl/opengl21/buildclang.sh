8a58227e2512eb5528138e45e84a07cfca90e35f
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-19 10:40:40 +0300

    Update RingOpenGL 2.1 - buildclang.sh

--- extensions/ringopengl/opengl21/buildclang.sh
+++ extensions/ringopengl/opengl21/buildclang.sh
@@ -1,5 +1,5 @@
 clang -c -fpic ring_opengl21.c -I $PWD/../../../include -I /usr/local/include
-clang -dynamiclib -o $PWD/../../../lib/libring_opengl21.dylib ring_opengl21.o -framework OpenGL -framework GLUT -L $PWD/../../../lib  -lring 
+clang -dynamiclib -o $PWD/../../../lib/libring_opengl21.dylib ring_opengl21.o /usr/local/lib/libglew.dylib -framework OpenGL -framework GLUT -L $PWD/../../../lib  -lring 
 
  
 
