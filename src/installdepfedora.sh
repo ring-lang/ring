34bdf520812dc6d6efa2afed833ce3cf9d629bc5
Mahmoud Fayed <mahmoudfayed@localhost.localdomain>
2017-08-25 04:58:41 +0300

    Update installdepfedora.sh - Add unixODBC and unixODBC-devel

--- src/installdepfedora.sh
+++ src/installdepfedora.sh
@@ -1,1 +1,1 @@
-sudo dnf install binutils gcc-c++ qt5-qtbase-devel qt5-qtmultimedia-devel qt5-qtwebkit-devel qt5-qtserialport-devel qt5-qtconnectivity-devel   
+sudo dnf install binutils gcc-c++ unixODBC unixODBC-devel qt5-qtbase-devel qt5-qtmultimedia-devel qt5-qtwebkit-devel qt5-qtserialport-devel qt5-qtconnectivity-devel   
