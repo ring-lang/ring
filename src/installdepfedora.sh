04f89db3dab8d0b81787a5f12edaaf07ac2a39a4
Mahmoud Fayed <mahmoudfayed@localhost.localdomain>
2017-08-25 04:12:42 +0300

    Update installdepfedora - Add gcc-c++

--- src/installdepfedora.sh
+++ src/installdepfedora.sh
@@ -1,1 +1,1 @@
-sudo yum install    qt5-qtbase-devel qt5-qtmultimedia-devel qt5-qtwebkit-devel qt5-qtserialport-devel qt5-qtconnectivity-devel   
+sudo dnf install gcc-c++ qt5-qtbase-devel qt5-qtmultimedia-devel qt5-qtwebkit-devel qt5-qtserialport-devel qt5-qtconnectivity-devel   
