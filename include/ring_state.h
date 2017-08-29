2ad504f332f98a6cfc49a5c349bcbd1c0285fbd9
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-08-29 08:39:56 +0300

    Update RingVM - Update Ring Version to 1.5.3

--- include/ring_state.h
+++ include/ring_state.h
@@ -64,7 +64,7 @@ RING_API void ring_state_runfile ( RingState *pRingState,char *cFileName ) ;
 RING_API void ring_state_runobjectfile ( RingState *pRingState,const char *cFileName ) ;
 /* MACRO */
 #define RING_STATE_CHECKPRINTRULES if ( pParser->pRingState->nPrintRules )
-#define RING_VERSION "1.5.2"
+#define RING_VERSION "1.5.3"
 /* General Functions */
 
 int ring_fexists ( const char *cFileName ) ;
