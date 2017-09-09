0efcb4ac1361705d1e869ef2921fb213e603f886
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-09-09 17:27:29 +0300

    Update Ring For Android.

--- android/ringlibsdl/project/jni/ring/src/ring_vmstate.c
+++ android/ringlibsdl/project/jni/ring/src/ring_vmstate.c
@@ -32,6 +32,7 @@ void ring_vm_savestate ( VM *pVM,List *pList )
 	ring_list_addpointer_gc(pVM->pRingState,pList,pVM->aPCBlockFlag);
 	ring_list_addint_gc(pVM->pRingState,pList,pVM->nLineNumber);
 	ring_list_addint_gc(pVM->pRingState,pList,pVM->nInClassRegion);
+	ring_list_addint_gc(pVM->pRingState,pList,pVM->nPrivateFlag);
 }
 
 void ring_vm_restorestate ( VM *pVM,List *pList,int nPos,int nFlag )
@@ -98,6 +99,7 @@ void ring_vm_restorestate ( VM *pVM,List *pList,int nPos,int nFlag )
 	}
 	pVM->nLineNumber = ring_list_getint(pList,26) ;
 	pVM->nInClassRegion = ring_list_getint(pList,27) ;
+	pVM->nPrivateFlag = ring_list_getint(pList,28) ;
 }
 /* Save/Restore State 2 - Used by Function Call & Return */
 
