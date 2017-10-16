43d90a5164e9680c8d3f287ed1dce539c730090a
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2017-10-16 15:46:32 +0300

    Update Tests

--- tests/scripts_list.bat
+++ tests/scripts_list.bat
@@ -255,3 +255,4 @@ call ring scripts/underscoreinnumber2.ring
 call ring scripts/fafternumber.ring
 call ring scripts/newtrytest.ring
 call ring scripts/newtrytest2.ring
+call ring scripts/manygetter.ring
