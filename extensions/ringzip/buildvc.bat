c21a6db29246bbf4ca957d512343482b60688723
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:05:31 +0300

    Update RingZip - buildvc.bat - use locatevc.bat

--- extensions/ringzip/buildvc.bat
+++ extensions/ringzip/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_libzip.c -I"..\ringzip" -I"..\..\include"
 link /DEBUG ring_libzip.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_libzip.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_libzip.obj
