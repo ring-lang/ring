f7ae7a45bfae096dc59da78147bd80e6bf8b0798
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:01:21 +0300

    Update RingLibCurl - buildvc.bat - use locatevc.bat

--- extensions/ringcurl/buildvc.bat
+++ extensions/ringcurl/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_libcurl.c -I"..\..\libdepwin\libcurl\include" -I"..\..\include"
 link /DEBUG ring_libcurl.obj  ..\..\lib\ring.lib ..\..\libdepwin\libcurl\lib\libcurl.lib /DLL /OUT:..\..\bin\ring_libcurl.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_libcurl.obj
