91a679ef7f7ea9e145bd6575e2eecaefd2c8710f
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:01:49 +0300

    Update RingFreeGLUT - buildvc.bat - use locatevc.bat

--- extensions/ringfreeglut/buildvc.bat
+++ extensions/ringfreeglut/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_freeglut.c -I "..\..\libdepwin\freeglut\include" -I"..\..\include"
 link /DEBUG ring_freeglut.obj  ..\..\lib\ring.lib ..\..\libdepwin\freeglut\lib\freeglut.lib /DLL /OUT:..\..\bin\ring_freeglut.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_freeglut.obj
