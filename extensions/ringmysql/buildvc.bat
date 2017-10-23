0058d014ec1808c368143b8d4799e995095a409e
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:02:44 +0300

    Update RingMySQL - buildvc.bat - use locatevc.bat

--- extensions/ringmysql/buildvc.bat
+++ extensions/ringmysql/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_vmmysql.c -I"..\..\include" -I"..\..\libdepwin\mysql\include" 
 link /DEBUG ring_vmmysql.obj  ..\..\lib\ring.lib ..\..\libdepwin\MySQL\lib\libmysql.lib /DLL /OUT:..\..\bin\ring_mysql.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_vmmysql.obj
