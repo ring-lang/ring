12101549c50c62963a550241cc2edb9a4e633fce
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:03:14 +0300

    Update RingODBC - buildvc.bat - use locatevc.bat

--- extensions/ringodbc/buildvc.bat
+++ extensions/ringodbc/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_vmodbc.c -I"..\..\include"
 link /DEBUG ring_vmodbc.obj  ..\..\lib\ring.lib odbc32.lib /DLL /OUT:..\..\bin\ring_odbc.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_vmodbc.obj
