a614c15ff52341405c9ea8eaf6dfd11bfc5447f5
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:00:41 +0300

    Update RingConsoleColors - buildvc.bat - use locatevc.bat

--- extensions/ringconsolecolors/buildvc.bat
+++ extensions/ringconsolecolors/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_consolecolors.c -I"..\..\include"
 link /DEBUG ring_consolecolors.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_consolecolors.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_consolecolors.obj
