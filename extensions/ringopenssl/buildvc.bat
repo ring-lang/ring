995af9aed73e4e2cc995f838710dc2b608b1efcf
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:03:44 +0300

    Update RingOpenSSL - buildvc.bat - use locatevc.bat

--- extensions/ringopenssl/buildvc.bat
+++ extensions/ringopenssl/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_vmopenssl.c -I"..\..\include" -I"..\..\libdepwin\OPENSSL\include"
 link /DEBUG ring_vmopenssl.obj  ..\..\lib\ring.lib ..\..\libdepwin\OpenSSL\lib\libeay32.lib /DLL /OUT:..\..\bin\ring_openssl.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_vmopenssl.obj
