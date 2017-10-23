f840ee57c8ce96e65c89c9152a754c1d17ce48a7
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:02:16 +0300

    Update RingInternet - buildvc.bat - use locatevc.bat

--- extensions/ringinternet/buildvc.bat
+++ extensions/ringinternet/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../src/locatevc.bat
 cl /c /DEBUG ring_internet.c -I"..\..\libdepwin\libcurl\include" -I"..\..\include"
 link /DEBUG ring_internet.obj  ..\..\lib\ring.lib ..\..\libdepwin\libcurl\lib\libcurl.lib /DLL /OUT:..\..\bin\ring_internet.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_internet.obj
