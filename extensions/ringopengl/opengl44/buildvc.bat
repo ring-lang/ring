297d9dc96d1c310a9ac1695c3878d7b4bd5bdd80
Mahmoud Fayed <msfclipper@yahoo.com>
2017-10-23 14:14:36 +0300

    Update RingOpenGL 4.4 - buildvc.bat - use locatevc.bat

--- extensions/ringopengl/opengl44/buildvc.bat
+++ extensions/ringopengl/opengl44/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call ../../../src/locatevc.bat
 cl /c /DEBUG ring_opengl44.c -I "..\..\..\libdepwin\glew\include" -I "..\..\..\libdepwin\freeglut\include" -I"..\..\..\include"
 link /DEBUG ring_opengl44.obj  ..\..\..\lib\ring.lib ..\..\..\libdepwin\glew\lib\release\win32\glew32.lib ..\..\..\libdepwin\freeglut\lib\freeglut.lib /DLL /OUT:..\..\..\bin\ring_opengl44.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_opengl44.obj
