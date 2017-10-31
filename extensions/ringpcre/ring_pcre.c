aafaa019adcbec6968348cc1cbb955482d41d51e
hassan <hah.alex@yahoo.com>
2017-10-31 20:48:12 +0200

    pcre2 extension

---
+++ extensions/ringpcre/ring_pcre.c
@@ -0,0 +1,13 @@
+/*
+Copyright (c) 2013-2017
+Mahmoud Fayed <msfclipper@yahoo.com>
+Hassan Ahmed <hsn@outlook.hu>
+*/
+
+#include "ring.h"
+#include "ring_pcre.h"
+
+RING_API void ringlib_init ( RingState *pRingState )
+{
+	
+}
---
+++ extensions/ringpcre/ring_pcre.h
@@ -0,0 +1,7 @@
+/*
+Copyright (c) 2013-2017
+Mahmoud Fayed <msfclipper@yahoo.com>
+Hassan Ahmed <hsn@outlook.hu>
+*/
+
+RING_API void ringlib_init(RingState *pRingState);
