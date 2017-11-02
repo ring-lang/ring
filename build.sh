97eddfef1fd959635d9d8a6c59def0254ee1bb75
hassan <hah.alex@yahoo.com>
2017-11-02 23:46:05 +0200

    add some build steps

--- build.sh
+++ build.sh
@@ -1,16 +1,37 @@
 #! /bin/sh
 
-
+####################################
+##      Set global variables      ##
+####################################
 VERSION=1.4.5
 BASE_DIR="/usr/local/lib/ring"
-MOD_DIR="$BASE_DIR/modules/"
+LIB_DIR="$BASE_DIR/lib/"
+EXT_DIR="$BASE_DIR/extensions/"
 
+## AT_* : autotools variables
+AT_BIN=autoreconf
+AT_FLAGS=-i
+
+####################################
+##   Make required directories    ##
+####################################
 if [ ! -d $BASE_DIR ]; then
     mkdir $BASE_DIR || exit
     echo "$BASE_DIR Directory created successfully"
 fi
 
-if [ ! -d $MOD_DIR ]; then
-    mkdir $MOD_DIR || exit
-    echo "$MOD_DIR Directory created successfully"
+if [ ! -d $LIB_DIR ]; then
+    mkdir $LIB_DIR || exit
+    echo "$LIB_DIR Directory created successfully"
 fi
+
+if [ ! -d $EXT_DIR ]; then
+    mkdir $EXT_DIR || exit
+    echo "$EXT_DIR Directory created successfully"
+fi
+
+
+#####################################
+## Build the configuration files  ##
+####################################
+$AT_BIN $AT_FLAGS || exit
