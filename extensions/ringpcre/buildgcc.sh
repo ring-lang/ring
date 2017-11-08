71a0654d71c5f259666e1eed7b8c085a1d8c2206
hassan <hah.alex@yahoo.com>
2017-11-08 19:17:30 +0200

    fix directory name, include ring headers and create temp lib directory

--- extensions/ringpcre/buildgcc.sh
+++ extensions/ringpcre/buildgcc.sh
@@ -1,15 +1,19 @@
 # TODO
 # this should be converted into autotools script
 
-gcc -c -fpic ring_pcre.c pcrelib/pcre2_auto_possess.c pcrelib/pcre2_chartables.c \
-    pcrelib/pcre2_compile.c pcrelib/pcre2_config.c pcrelib/pcre2_context.c \
-    pcrelib/pcre2_dfa_match.c pcrelib/pcre2_error.c pcrelib/pcre2_find_bracket.c \
-     pcrelib/pcre2_jit_compile.c pcrelib/pcre2_maketables.c \
-     pcrelib/pcre2_match.c pcrelib/pcre2_match_data.c pcrelib/pcre2_newline.c \
-     pcrelib/pcre2_ord2utf.c pcrelib/pcre2_pattern_info.c \
-     pcrelib/pcre2_serialize.c pcrelib/pcre2_string_utils.c \
-     pcrelib/pcre2_study.c pcrelib/pcre2_substitute.c \
-     pcrelib/pcre2_substring.c pcrelib/pcre2_tables.c pcrelib/pcre2_ucd.c \
-     pcrelib/pcre2_valid_utf.c pcrelib/pcre2_xclass.c \
-     -DHAVE_CONFIG_H -DPCRE2_CODE_UNIT_WIDTH=8 -g -Wall -I.
-gcc --shared -o ringpcre2.so *.o
+mkdir lib/
+
+gcc -c -fpic ring_pcre.c pcre2lib/pcre2_auto_possess.c pcre2lib/pcre2_chartables.c \
+    pcre2lib/pcre2_compile.c pcre2lib/pcre2_config.c pcre2lib/pcre2_context.c \
+    pcre2lib/pcre2_dfa_match.c pcre2lib/pcre2_error.c pcre2lib/pcre2_find_bracket.c \
+     pcre2lib/pcre2_jit_compile.c pcre2lib/pcre2_maketables.c \
+     pcre2lib/pcre2_match.c pcre2lib/pcre2_match_data.c pcre2lib/pcre2_newline.c \
+     pcre2lib/pcre2_ord2utf.c pcre2lib/pcre2_pattern_info.c \
+     pcre2lib/pcre2_serialize.c pcre2lib/pcre2_string_utils.c \
+     pcre2lib/pcre2_study.c pcre2lib/pcre2_substitute.c \
+     pcre2lib/pcre2_substring.c pcre2lib/pcre2_tables.c pcre2lib/pcre2_ucd.c \
+     pcre2lib/pcre2_valid_utf.c pcre2lib/pcre2_xclass.c \
+     -DHAVE_CONFIG_H -DPCRE2_CODE_UNIT_WIDTH=8 -g -Wall -I $PWD/../../include -I.
+gcc --shared -o lib/ringpcre2.so *.o
+
+rm *.o
