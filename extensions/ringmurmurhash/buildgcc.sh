4b1ac184302cae9fa89b25942fb34762fe35f7cb
hassan <hah.alex@yahoo.com>
2017-11-21 18:40:07 +0200

    update buildgcc.sh to include lib folder and move the so file to the right path

--- extensions/ringmurmurhash/buildgcc.sh
+++ extensions/ringmurmurhash/buildgcc.sh
@@ -4,8 +4,10 @@ mkdir lib/
 
 gcc -c -fpic ring_murmurhash.c libmurmurhash/MurmurHash1.c \
     libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c \
-    -g -Wall -I $PWD/../../include -I. -O3
+    -g -Wall -O3 -I $PWD/../../include -I libmurmurhash/
 
 gcc --shared -o lib/libringmurmurhash.so *.o
 
 rm *.o
+
+cp lib/libringmurmurhash.so /usr/local/lib/ring/libring_murmurhash.so
