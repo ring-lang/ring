8e3aa3667aec3b7e7d2ab0a00ce2f8c45bc947a4
hassan <hah.alex@yahoo.com>
2017-11-22 20:36:32 +0200

    add optional paramters

--- extensions/ringmurmurhash/ring_murmurhash.c
+++ extensions/ringmurmurhash/ring_murmurhash.c
@@ -8,15 +8,27 @@ RING_API void ringlib_init(RingState *pRingState)
     ring_vm_funcregister("murmurhash1_aligned", ring_murmurhash1_aligned);
 }
 
+int is_bool(int tmp)
+{
+    if (
+        tmp == 1 ||
+        tmp == 0
+    ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 #define LONG2HEX(dest, val) sprintf(dest, "%x", val)
 
 #define MH_RETURN(ret_val, ret_type) { \
     if (ret_type) { \
         char tmp[50]; \
         LONG2HEX(tmp, ret_val); \
-        RING_API_RETSTRING(tmp); \
+        RING_API_RETSTRING(tmp); return; \
     } else { \
-        RING_API_RETNUMBER(ret_val); \
+        RING_API_RETNUMBER(ret_val); return; \
     } \
 }
 
@@ -27,19 +39,20 @@ void ring_murmurhash1(void *pPointer)
     int keylen;
     int seed = 0;
     uint32_t out;
+    int ret_type = 0;
 
-    if (RING_API_PARACOUNT != 2) {
+    if (RING_API_PARACOUNT < 2 || RING_API_PARACOUNT > 3) {
 		RING_API_ERROR(RING_API_MISS2PARA);
 		return ;
 	}
 
     if (!RING_API_ISSTRING(1)) {
-        RING_API_ERROR("murmurhash1 expects the first parameter to be a string");
+        RING_API_ERROR("murmurhash1 expects the first parameter to be a string\n");
         return;
     }
 
     if (!RING_API_ISNUMBER(2)) {
-        RING_API_ERROR("murmurhash1 expects the first parameter to be an integer");
+        RING_API_ERROR("murmurhash1 expects the first parameter to be an integer\n");
         return;
     }
 
@@ -47,10 +60,20 @@ void ring_murmurhash1(void *pPointer)
     keylen = strlen(key);
     seed = RING_API_GETNUMBER(2);
 
+    if (RING_API_PARACOUNT == 3) {
+        if (RING_API_ISNUMBER(3)) {
+            ret_type = RING_API_GETNUMBER(3);
+            if (!is_bool(ret_type)) {
+                RING_API_ERROR("Third parameter should be boolean value\n");
+            }
+        } else {
+            RING_API_ERROR("murmurhash1 expects the third parameter to be an integer\n");
+        }
+    }
+
     out = MurmurHash1(key, keylen, seed);
 
-    MH_RETURN(out, 0);
-    return;
+    MH_RETURN(out, ret_type);
 }
 
 void ring_murmurhash1_aligned(void *pPointer)
@@ -59,19 +82,20 @@ void ring_murmurhash1_aligned(void *pPointer)
     int keylen;
     int seed = 0;
     uint32_t out;
+    int ret_type = 0;
 
-    if (RING_API_PARACOUNT != 2) {
+    if (RING_API_PARACOUNT < 2 || RING_API_PARACOUNT > 3) {
 		RING_API_ERROR(RING_API_MISS2PARA);
 		return ;
 	}
 
     if (!RING_API_ISSTRING(1)) {
-        RING_API_ERROR("murmurhash1 expects the first parameter to be a string");
+        RING_API_ERROR("murmurhash1_aligned expects the first parameter to be a string");
         return;
     }
 
     if (!RING_API_ISNUMBER(2)) {
-        RING_API_ERROR("murmurhash1 expects the first parameter to be an integer");
+        RING_API_ERROR("murmurhash1_aligned expects the first parameter to be an integer");
         return;
     }
 
@@ -79,8 +103,18 @@ void ring_murmurhash1_aligned(void *pPointer)
     keylen = strlen(key);
     seed = RING_API_GETNUMBER(2);
 
+    if (RING_API_PARACOUNT == 3) {
+        if (RING_API_ISNUMBER(3)) {
+            ret_type = RING_API_GETNUMBER(3);
+            if (!is_bool(ret_type)) {
+                RING_API_ERROR("Third parameter should be boolean value\n");
+            }
+        } else {
+            RING_API_ERROR("murmurhash1_aligned expects the third parameter to be an integer\n");
+        }
+    }
+
     out = MurmurHash1Aligned(key, keylen, seed);
 
-    MH_RETURN(out, 0);
-    return;
+    MH_RETURN(out, ret_type);
 }
