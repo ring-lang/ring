4a5c6e915dfb6f244bdb095da08f906d158cbcf3
Mahmoud Fayed <msfclipper@yahoo.com>
2017-11-25 18:52:10 +0300

    Update Ring2EXE - Scripts

--- ring2exe/buildring2exe.bat
+++ ring2exe/buildring2exe.bat
@@ -1,4 +1,6 @@
 call ../src/locatevc.bat
-ring ring2exe.ring ring2exe.ring
+ring ring2exe.ring ring2exe.ring -static
 copy ring2exe.exe ..\bin\ring2exe.exe
+ring ring2exe.ring folder2qrc.ring -static
+copy folder2qrc.exe ..\bin\folder2qrc.exe
 cleartemp
