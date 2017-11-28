ba7eedd719322d46119e2baab555c92c0c8f0a9a
Mahmoud Fayed <msfclipper@yahoo.com>
2017-11-28 12:59:34 +0300

    Update RingMurmurHash - buildvc.bat

--- extensions/ringmurmurhash/buildvc.bat
+++ extensions/ringmurmurhash/buildvc.bat
@@ -1,11 +1,7 @@
-MD lib
-
 cls
 call ../../src/locatevc.bat
-cl /c /DEBUG ring_murmurhash.c libmurmurhash/MurmurHash1.c \
-    libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c \
+cl /c /DEBUG ring_murmurhash.c libmurmurhash/MurmurHash1.c ^
+    libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c ^
      -I"../../include" -I"libmurmurhash/"
-
-
-link /DEBUG *.obj ..\..\lib\ring.lib /DLL /OUT:lib\ring_murmurhash.dll /SUBSYSTEM:CONSOLE,"5.01" 
+link /DEBUG *.obj ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_murmurhash.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del *.obj
