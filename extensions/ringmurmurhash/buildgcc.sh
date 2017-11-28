d4ac7c1a8d1f6edbffa370f501cb444c451aae59
Mahmoud Fayed <msfclipper@yahoo.com>
2017-11-28 14:14:48 +0300

    Update RingMurmurHash - buildgcc.sh

--- extensions/ringmurmurhash/buildgcc.sh
+++ extensions/ringmurmurhash/buildgcc.sh
@@ -1,13 +1,7 @@
-# TODO : convert into autotools
-
-mkdir lib/
-
 gcc -c -fpic ring_murmurhash.c libmurmurhash/MurmurHash1.c \
     libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c \
     -g -Wall -O3 -I $PWD/../../include -I libmurmurhash/
 
-gcc --shared -o lib/libringmurmurhash.so *.o
+gcc -shared -o $PWD/../../lib/libring_murmurhash.so *.o -L $PWD/../../lib -lring
 
 rm *.o
-
-cp lib/libringmurmurhash.so /usr/local/lib/ring/libring_murmurhash.so
