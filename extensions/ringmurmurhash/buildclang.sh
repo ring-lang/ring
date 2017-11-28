ac81b18a98f7e6d415f976343cd378e94db2a8e4
Mahmoud Fayed <msfclipper@yahoo.com>
2017-11-28 14:04:58 +0300

    Update RingMurmurHash - buildclang.sh

--- extensions/ringmurmurhash/buildclang.sh
+++ extensions/ringmurmurhash/buildclang.sh
@@ -1,13 +1,5 @@
-# TODO : convert into autotools
-
-mkdir lib/
-
-clang -c -fpic ring_murmurhash.c libmurmurhash/MurmurHash1.c \
-    libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c \
-    -g -Wall -O3 -I $PWD/../../include -I libmurmurhash/
-
-clang -dynamiclib -o lib/libring_murmurhash.dylib *.o
-
+clang -c -fpic ring_murmurhash.c libmurmurhash/MurmurHash1.c libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c -I $PWD/../../include -I $PWD/libmurmurhash/
+clang -dynamiclib -o $PWD/../../lib/libring_murmurhash.dylib *.o
 rm *.o
 
-cp lib/libring_murmurhash.dylib /usr/local/lib/ring/libring_murmurhash.dylib
+
