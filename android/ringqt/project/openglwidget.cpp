12bbfa325e28893eee3032bd2ce03887edfac6b7
Mahmoud Fayed <msfclipper@yahoo.com>
2018-01-17 11:53:04 +0300

    Update Ring For Android

--- android/ringqt/project/openglwidget.cpp
+++ android/ringqt/project/openglwidget.cpp
@@ -1,16 +1,86 @@
-#include <QOpenGLWidget>
+/* Copyright (c) 2013-2018 Mahmoud Fayed <msfclipper@yahoo.com> */
 
+extern "C" {
+#include "ring.h"
+}
+
+#include <QOpenGLWidget>
 #include "openglwidget.h"
 
 void OpenGLWidget::initializeGL()
 {
+	if (strcmp(this->cInitEvent,"")==0)
+		return ;
+
+	ring_vm_runcode(this->pVM,this->cInitEvent);
 }
 
 void OpenGLWidget::paintGL()
 {
+	if (strcmp(this->cPaintEvent,"")==0)
+		return ;
+
+	ring_vm_runcode(this->pVM,this->cPaintEvent);
 }
 
 void OpenGLWidget::resizeGL(int width, int height)
 {
+	if (strcmp(this->cResizeEvent,"")==0)
+		return ;
+
+	ring_vm_runcode(this->pVM,this->cResizeEvent);
 }
 
+OpenGLWidget::OpenGLWidget(QWidget *parent,VM *pVM)  : QOpenGLWidget(parent)
+{
+	this->pVM = pVM;
+	this->pParaList = ring_list_new(0);
+	strcpy(this->cInitEvent,"");
+	strcpy(this->cPaintEvent,"");
+	strcpy(this->cResizeEvent,"");
+}
+
+OpenGLWidget::~OpenGLWidget()
+{
+	ring_list_delete(this->pParaList);
+}
+
+void OpenGLWidget::geteventparameters(void)
+{
+	void *pPointer;
+	pPointer = this->pVM;
+	RING_API_RETLIST(this->pParaList);
+}
+
+void OpenGLWidget::setInitEvent(const char *cStr)
+{
+	if (strlen(cStr)<100)
+		strcpy(this->cInitEvent,cStr);
+}
+
+const char *OpenGLWidget::getInitEvent(void)
+{
+	return this->cInitEvent;
+}
+
+void OpenGLWidget::setPaintEvent(const char *cStr)
+{
+	if (strlen(cStr)<100)
+		strcpy(this->cPaintEvent,cStr);
+}
+
+const char *OpenGLWidget::getPaintEvent(void)
+{
+	return this->cPaintEvent;
+}
+
+void OpenGLWidget::setResizeEvent(const char *cStr)
+{
+	if (strlen(cStr)<100)
+		strcpy(this->cResizeEvent,cStr);
+}
+
+const char *OpenGLWidget::getResizeEvent(void)
+{
+	return this->cResizeEvent;
+}
--- android/ringqt/project/openglwidget.h
+++ android/ringqt/project/openglwidget.h
@@ -1,13 +1,37 @@
+/* Copyright (c) 2013-2018 Mahmoud Fayed <msfclipper@yahoo.com> */
+
 #ifndef MYGLWIDGET_H
 #define MYGLWIDGET_H
 
 #include <QOpenGLWidget>
 
+extern "C" {
+#include "ring.h"
+}
+
 class OpenGLWidget : public QOpenGLWidget
 {
 
     Q_OBJECT
-	
+
+public:
+
+    OpenGLWidget(QWidget *parent,VM *pVM);
+    ~OpenGLWidget();
+
+    VM *pVM;
+    List *pParaList;
+    char cInitEvent[100];
+    char cPaintEvent[100];
+    char cResizeEvent[100];
+    void geteventparameters(void) ;
+    void setInitEvent(const char *cStr);
+    const char *getInitEvent(void);
+    void setPaintEvent(const char *cStr);
+    const char *getPaintEvent(void);
+    void setResizeEvent(const char *cStr);
+    const char *getResizeEvent(void);
+		
 protected:
     void initializeGL();
     void paintGL();
