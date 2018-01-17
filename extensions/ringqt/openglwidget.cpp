320f75f92969d6f9bea75b8947a35be39f3db27d
Mahmoud Fayed <msfclipper@yahoo.com>
2018-01-17 11:33:21 +0300

    Update RingQt - openglwidget.cpp and openglwidget.h - Init Event

--- extensions/ringqt/openglwidget.cpp
+++ extensions/ringqt/openglwidget.cpp
@@ -1,5 +1,10 @@
-#include <QOpenGLWidget>
+/* Copyright (c) 2013-2018 Mahmoud Fayed <msfclipper@yahoo.com> */
 
+extern "C" {
+#include "ring.h"
+}
+
+#include <QOpenGLWidget>
 #include "openglwidget.h"
 
 void OpenGLWidget::initializeGL()
@@ -14,3 +19,32 @@ void OpenGLWidget::resizeGL(int width, int height)
 {
 }
 
+OpenGLWidget::OpenGLWidget(QWidget *parent,VM *pVM)  : QOpenGLWidget(parent)
+{
+	this->pVM = pVM;
+	this->pParaList = ring_list_new(0);
+	strcpy(this->cInitEvent,"");
+}
+
+OpenGLWidget::~OpenGLWidget()
+{
+	ring_list_delete(this->pParaList);
+}
+
+void OpenGLWidget::geteventparameters(void)
+{
+	void *pPointer;
+	pPointer = this->pVM;
+	RING_API_RETLIST(this->pParaList);
+}
+
+void OpenGLWidget::setInitEvent(const char *cStr)
+{
+	if (strlen(cStr)<100)
+		strcpy(this->cInitEvent,cStr);
+}
+
+const char *OpenGLWidget::getInitEvent(void)
+{
+	return this->cInitEvent;
+}
--- extensions/ringqt/openglwidget.h
+++ extensions/ringqt/openglwidget.h
@@ -1,12 +1,30 @@
+/* Copyright (c) 2013-2018 Mahmoud Fayed <msfclipper@yahoo.com> */
+
 #ifndef MYGLWIDGET_H
 #define MYGLWIDGET_H
 
 #include <QOpenGLWidget>
 
+extern "C" {
+#include "ring.h"
+}
+
 class OpenGLWidget : public QOpenGLWidget
 {
 
     Q_OBJECT
+
+public:
+
+    OpenGLWidget(QWidget *parent,VM *pVM);
+    ~OpenGLWidget();
+
+    VM *pVM;
+    List *pParaList;
+    char cInitEvent[100];
+    void geteventparameters(void) ;
+    void setInitEvent(const char *cStr);
+    const char *getInitEvent(void);
 	
 protected:
     void initializeGL();
