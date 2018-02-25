5686106a7ae29b3aa75e40968bcdd8cdc3cad06b
Mahmoud Fayed <msfclipper@yahoo.com>
2018-02-25 11:35:08 +0300

    Update Ring version

--- include/ring_state.h
+++ include/ring_state.h
@@ -75,7 +75,7 @@ RING_API void ring_state_runobjectfile ( RingState *pRingState,char *cFileName )
 RING_API void ring_state_runobjectstring ( RingState *pRingState,char *cString,const char *cFileName ) ;
 /* MACRO */
 #define RING_STATE_CHECKPRINTRULES if ( pParser->pRingState->nPrintRules )
-#define RING_VERSION "1.7"
+#define RING_VERSION "1.8"
 /* General Functions */
 
 int ring_fexists ( const char *cFileName ) ;
