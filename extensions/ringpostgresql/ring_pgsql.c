da18ad0c0be9a8e333cdd8deb71cfc00db883d43
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-01 00:47:21 +0300

    Update RingPostgreSQL - PQconnectdbParams()

--- extensions/ringpostgresql/ring_pgsql.c
+++ extensions/ringpostgresql/ring_pgsql.c
@@ -4,6 +4,29 @@
 
 #include "libpq-fe.h"
 
+
+RING_FUNC(ring_PQconnectdbParams)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETCPOINTER(PQconnectdbParams((char *) RING_API_GETCPOINTER(1,"char"),(char *) RING_API_GETCPOINTER(2,"char"), (int ) RING_API_GETNUMBER(3)),"PGconn");
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
+	ring_vm_funcregister("pqconnectdbparams",ring_PQconnectdbParams);
 }
