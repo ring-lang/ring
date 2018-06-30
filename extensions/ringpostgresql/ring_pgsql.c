3c9fb09afde0985b2dbe9cf2131d012f931fdae5
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-01 00:53:14 +0300

    Update RingPostgreSQL

--- extensions/ringpostgresql/ring_pgsql.c
+++ extensions/ringpostgresql/ring_pgsql.c
@@ -108,10 +108,63 @@ RING_FUNC(ring_PQsetdb)
 	RING_API_RETCPOINTER(PQsetdb(RING_API_GETSTRING(1),RING_API_GETSTRING(2),RING_API_GETSTRING(3),RING_API_GETSTRING(4),RING_API_GETSTRING(5)),"PGconn");
 }
 
+
+RING_FUNC(ring_PQconnectStartParams)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETCPOINTER(PQconnectStartParams((char *) RING_API_GETCPOINTER(1,"char"),(char *) RING_API_GETCPOINTER(2,"char"), (int ) RING_API_GETNUMBER(3)),"PGconn");
+}
+
+
+RING_FUNC(ring_PQconnectStart)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISSTRING(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETCPOINTER(PQconnectStart(RING_API_GETSTRING(1)),"PGconn");
+}
+
+
+RING_FUNC(ring_PQconnectPoll)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETNUMBER(PQconnectPoll((PGconn *) RING_API_GETCPOINTER(1,"PGconn")));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("pqconnectdbparams",ring_PQconnectdbParams);
 	ring_vm_funcregister("pqconnectdb",ring_PQconnectdb);
 	ring_vm_funcregister("pqsetdblogin",ring_PQsetdbLogin);
 	ring_vm_funcregister("pqsetdb",ring_PQsetdb);
+	ring_vm_funcregister("pqconnectstartparams",ring_PQconnectStartParams);
+	ring_vm_funcregister("pqconnectstart",ring_PQconnectStart);
+	ring_vm_funcregister("pqconnectpoll",ring_PQconnectPoll);
 }
