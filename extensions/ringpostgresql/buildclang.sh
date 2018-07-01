f12663636cf1e956fd176d8815a21aa20fe38716
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-01 04:21:04 +0300

    Update RingPostgreSQL - extensions/ringpostgresql/buildclang.sh

--- extensions/ringpostgresql/buildclang.sh
+++ extensions/ringpostgresql/buildclang.sh
@@ -1,5 +1,5 @@
-clang -c -fpic ring_pgsql.c -I $PWD/../../include -I /usr/local/include -I /usr/local/pgsql/include
-clang -dynamiclib -o $PWD/../../lib/libring_pgsql.dylib ring_pgsql.o -L $PWD/../../lib  -lring  -L /usr/local/pgsql/lib  -lpq
+clang -c -fpic ring_pgsql.c -I $PWD/../../include -I /usr/local/include -I /usr/local/opt/libpq/include
+clang -dynamiclib -o $PWD/../../lib/libring_pgsql.dylib ring_pgsql.o -L $PWD/../../lib  -lring  -L /usr/local/opt/libpq/lib  -lpq
 
  
 
