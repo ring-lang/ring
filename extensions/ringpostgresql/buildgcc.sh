475125e65e621072934800a981c0e6a2b6045d53
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-01 03:46:25 +0300

    Update RingPostgreSQL - extensions/ringpostgresql/buildgcc.sh

--- extensions/ringpostgresql/buildgcc.sh
+++ extensions/ringpostgresql/buildgcc.sh
@@ -1,4 +1,4 @@
-gcc -c -fpic ring_pgsql.c -I $PWD/../../include -I /use/local/pgsql/include
+gcc -c -fpic ring_pgsql.c -I $PWD/../../include -I /usr/local/pgsql/include
 gcc -shared -o $PWD/../../lib/libringpgsql.so ring_pgsql.o -L $PWD/../../lib -lring -L /usr/local/pgsql/lib -lpq 
 
  
