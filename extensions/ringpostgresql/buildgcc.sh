5f0f73c65644e6dbbde851efa7efe58723c33e2c
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-01 03:56:43 +0300

    Update RingPostgreSQL - Library file name.

--- extensions/ringpostgresql/buildgcc.sh
+++ extensions/ringpostgresql/buildgcc.sh
@@ -1,5 +1,5 @@
 gcc -c -fpic ring_pgsql.c -I $PWD/../../include -I /usr/include/postgresql
-gcc -shared -o $PWD/../../lib/libringpgsql.so ring_pgsql.o -L $PWD/../../lib -lring -L /usr/local/pgsql/lib -lpq 
+gcc -shared -o $PWD/../../lib/libring_pgsql.so ring_pgsql.o -L $PWD/../../lib -lring -L /usr/local/pgsql/lib -lpq 
 
  
 
