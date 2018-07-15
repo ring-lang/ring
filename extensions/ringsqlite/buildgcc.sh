0f46a8e788ab5856b13ce0794466b3c5195a9218
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-15 16:31:27 +0300

    Update RingSQLite

--- extensions/ringsqlite/buildgcc.sh
+++ extensions/ringsqlite/buildgcc.sh
@@ -1,5 +1,5 @@
 gcc -c -fpic ring_vmsqlite.c sqlite3.c -I $PWD/../../include
-gcc -shared -o $PWD/../../lib/libring_sqlite.so ring_vmsqlite.o sqlite3.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu 
+gcc -shared -o $PWD/../../lib/libring_sqlite.so ring_vmsqlite.o sqlite3.o -L $PWD/../../lib -lring -L /usr/lib/i386-linux-gnu -pthread
 
  
 
