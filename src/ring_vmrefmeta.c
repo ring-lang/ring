f1f7c0dcfcfb69fe313f193c6930de32739998bb
Mahmoud Fayed <msfclipper@yahoo.com>
2018-07-23 02:59:27 +0300

    Update Ring VM - ringvm_cfunctionslist() function.

--- src/ring_vmrefmeta.c
+++ src/ring_vmrefmeta.c
@@ -914,7 +914,10 @@ void ring_vm_refmeta_ringvmcfunctionslist ( void *pPointer )
 	VM *pVM  ;
 	List *pList  ;
 	pVM = (VM *) pPointer ;
-	RING_API_RETLIST(pVM->pCFunctionsList);
+	pList = ring_list_new_gc(pVM->pRingState,0);
+	ring_list_copy_gc(pVM->pRingState,pList,pVM->pCFunctionsList);
+	RING_API_RETLIST(pList);
+	ring_list_delete_gc(pVM->pRingState,pList);
 }
 
 void ring_vm_refmeta_ringvmsettrace ( void *pPointer )
