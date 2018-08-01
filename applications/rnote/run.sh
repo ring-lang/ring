9fbada0ea626ce8daa188a1c2c380b7115570a23
Mahmoud Fayed <msfclipper@yahoo.com>
2018-08-01 07:09:49 +0300

    Update Ring Notepad - remove run.sh

--- applications/rnote/run.sh
+++
@@ -1,7 +0,0 @@
-# Simple script to execute Ring applications in the application folder
-# So the application can find files using relative path 
-# 2016, Mahmoud Fayed <msfclipper@yahoo.com>
-
-cd $(dirname "$1")
-ring $1
-
