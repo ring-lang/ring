35bf8a6f938077bba92f96b13280704b09094d4c
Mahmoud Fayed <msfclipper@yahoo.com>
2018-08-12 03:37:36 +0300

    Update RingQt - gencode.sh

--- extensions/ringqt/gencode.sh
+++ extensions/ringqt/gencode.sh
@@ -1,3 +1,3 @@
-../../bin/ring ../codegen/parsec.ring generator/qt.cf ring_qt.cpp ring_qt.ring
+../../bin/ring ../codegen/parsec.ring generator/qt.cf cpp/src/ring_qt.cpp ring_qt.ring
 ../../bin/ring qtevents.ring
 
