4430fef7f524d1d47979687012741edcf4d066ff
Mahmoud Fayed <msfclipper@yahoo.com>
2018-08-13 10:16:02 +0300

    Update RingQt - remove newclass folder.

--- extensions/ringqt/newclass/test.bat
+++
@@ -1,6 +0,0 @@
-set RINGQT_ANDROID=
-ring ..\..\codegen\parsec.ring qt.cf ring_qt.cpp ring_qt.ring
-ring qtevents.ring
-set PATH=C:\Qt\Qt5.5.1\5.5\mingw492_32\bin;C:\Qt\Qt5.5.1\Tools\mingw492_32\bin;%PATH%
-qmake "ring_qt.pro"  
-mingw32-make
