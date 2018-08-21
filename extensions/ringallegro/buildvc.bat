5109a3a6514c34df60f50baf080d947d837ae7b8
Mahmoud Fayed <msfclipper@yahoo.com>
2018-08-21 13:09:19 +0300

    Update RingAllegro - buildvc.bat - Use Allegro 5.2 instead of Allegro 5.1 (Nice update because the new version of Allegro support dpi awareness on Microsoft Windows) - So when we run games in full screen, it will not be in wrong scale because when the user settings uses scaling like 125% or 150% and so on.

--- extensions/ringallegro/buildvc.bat
+++ extensions/ringallegro/buildvc.bat
@@ -1,5 +1,8 @@
 cls
 call ../../src/locatevc.bat
 cl /c /DEBUG ring_allegro.c -I"..\..\libdepwin\allegro\include" -I"..\..\include"
-link /DEBUG ring_allegro.obj  ..\..\lib\ring.lib ..\..\libdepwin\allegro\lib\allegro_monolith.lib /DLL /OUT:..\..\bin\ring_allegro.dll /SUBSYSTEM:CONSOLE,"5.01" 
+link /DEBUG ring_allegro.obj  ..\..\lib\ring.lib ..\..\libdepwin\allegro\lib\allegro.lib ..\..\libdepwin\allegro\lib\allegro_image.lib ..\..\libdepwin\allegro\lib\allegro_dialog.lib ..\..\libdepwin\allegro\lib\allegro_ttf.lib ..\..\libdepwin\allegro\lib\allegro_acodec.lib ..\..\libdepwin\allegro\lib\allegro_primitives.lib ^
+..\..\libdepwin\allegro\lib\allegro_audio.lib ..\..\libdepwin\allegro\lib\allegro_color.lib ..\..\libdepwin\allegro\lib\allegro_font.lib ..\..\libdepwin\allegro\lib\allegro_video.lib ..\..\libdepwin\allegro\lib\allegro_primitives.lib ..\..\libdepwin\allegro\lib\allegro_memfile.lib ^
+		..\..\libdepwin\allegro\lib\allegro_physfs.lib ^
+	  /DLL /OUT:..\..\bin\ring_allegro.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_allegro.obj
