8206da40c5f55f44afb51863e3b47654b2d7ac4d
Mahmoud Fayed <msfclipper@yahoo.com>
2018-10-03 18:21:47 +0300

    Update RingQt - Add gencodemacos.sh

---
+++ extensions/ringqt/gencodemacos.sh
@@ -0,0 +1,7 @@
+set RINGQT_ANDROID=
+set RINGQT_NOWEBKIT=1
+../../bin/ring ../codegen/parsec.ring classes/qt.cf cpp/src/ring_qt.cpp guilib/ring_qt.ring
+cd events
+../../../bin/ring qtevents.ring
+cd ..
+
