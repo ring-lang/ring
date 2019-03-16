8e80574ff22a929307fd468a60d694ee0f301e5a
Mahmoud Fayed <msfclipper@yahoo.com>
2019-03-16 13:27:31 +0300

    Add extensions/tutorial/updatetable

---
+++ extensions/tutorial/updatetable/README.md
@@ -0,0 +1,184 @@
+Creating Extensions - Tutorial - Update Table
+=============================================
+
+In this extension we learn how to create a C function that update a table contains rows and columns
+
+In mylib.c we update the file to add 
+
+	RING_FUNC(ring_updatetable)
+	{
+		List *pList, *pRow;
+		int nRow,nCol;
+		// Check Parameters Count
+			if (RING_API_PARACOUNT != 2) {
+				RING_API_ERROR(RING_API_MISS2PARA);
+				return;
+			}
+		// Check Parameters Type
+			if ( ! ( RING_API_ISPOINTER(1) && RING_API_ISNUMBER(2) ) ) {
+				RING_API_ERROR(RING_API_BADPARATYPE);
+				return;
+			}
+		// Get the List
+			pList = RING_API_GETLIST(1);
+		// Display the List Rows and Columns
+			for (nRow = 1 ; nRow <= ring_list_getsize(pList) ; nRow++ ) {
+				if ( ring_list_islist(pList,nRow) ) {
+					pRow = ring_list_getlist(pList,nRow);
+					for (nCol = 1 ; nCol <= ring_list_getsize(pRow) ; nCol++ ) {
+						if ( ring_list_isdouble(pRow,nCol) ) {
+							ring_list_setdouble(pRow,nCol,RING_API_GETNUMBER(2));
+						} else {
+							RING_API_ERROR("Error : We expect numbers!\n");
+						}
+					}
+				} else {
+					RING_API_ERROR("Error : The parameter is not a table! \n");
+				}
+			}
+	}
+
+
+Then we register the new function
+
+	ring_vm_funcregister("updatetable",ring_updatetable);
+
+The file test.ring contains
+
+	load "stdlib.ring"
+
+	? "Loading Library"
+	load "mylib.ring"
+
+	? "Calling a C Function"
+	myfunction()
+
+	? "Sum Two Numbers (3,5)"
+	? SumTwoNumbers(3,5)
+
+	? "Say Hello"
+	SayHello("Mahmoud")
+
+	? "Sum List contains numbers from 1 to 10"
+	aList = 1:10
+	? SumList(aList)
+
+	? "Increment List Items"
+	? inclist(aList,10)
+
+	? "Filter List Items (Items > 15)"
+	? filterlist(aList,15)
+
+	aList = 1:3
+	? "Replicate list (1:3) three times then print the items (We expect 12 items)"
+	? replicatelist(aList,3)
+
+	? "Create list contains 5 items using C code"
+	aList = GenerateList(5)
+	? aList
+
+	? "Create List (3,2)"
+	aList = newList(3,2)
+	aList[1][1] = "R 1 C 1"
+	aList[1][2] = "R 1 C 2"
+	aList[2][1] = "R 2 C 1"
+	aList[2][2] = "R 2 C 2"
+	aList[3][1] = "R 3 C 1"
+	aList[3][2] = "R 3 C 2"
+	? "Print the List using Ring"
+	? aList
+	? "Print the List by calling C Code"
+	displayList(aList)
+	? ""
+
+	? "Create List (2,2)"
+	aList = newList(2,2)
+	? "Update the list using C code - set all cells to 10"
+	UpdateTable(aList,10)
+	? "aList[1][1] : " + aList[1][1]
+	? "aList[1][2] : " + aList[1][2]
+	? "aList[2][1] : " + aList[2][1]
+	? "aList[2][2] : " + aList[2][2]
+
+Then we test the function using
+
+	ring test.ring
+
+Output
+
+	Loading Library
+	Calling a C Function
+	Hello, World!
+	Sum Two Numbers (3,5)
+	8
+	Say Hello
+	Hello Mahmoud
+	Sum List contains numbers from 1 to 10
+	55
+	Increment List Items
+	11
+	12
+	13
+	14
+	15
+	16
+	17
+	18
+	19
+	20
+
+
+	Filter List Items (Items > 15)
+	16
+	17
+	18
+	19
+	20
+
+	Replicate list (1:3) three times then print the items (We expect 12 items)
+	1
+	2
+	3
+	1
+	2
+	3
+	1
+	2
+	3
+	1
+	2
+	3
+
+	Create list contains 5 items using C code
+	1
+	2
+	3
+	4
+	5
+
+	Create List (3,2)
+	Print the List using Ring
+	R 1 C 1
+	R 1 C 2
+	R 2 C 1
+	R 2 C 2
+	R 3 C 1
+	R 3 C 2
+
+	Print the List by calling C Code
+	Sub List..
+	String : R 1 C 1
+	String : R 1 C 2
+	Sub List..
+	String : R 2 C 1
+	String : R 2 C 2
+	Sub List..
+	String : R 3 C 1
+	String : R 3 C 2
+
+	Create List (2,2)
+	Update the list using C code - set all cells to 10
+	aList[1][1] : 10
+	aList[1][2] : 10
+	aList[2][1] : 10
+	aList[2][2] : 10
---
+++ extensions/tutorial/updatetable/mylib.c
@@ -0,0 +1,261 @@
+#include "ring.h"
+
+#include "stdlib.h"
+
+RING_FUNC(ring_myfunction)
+{
+	printf("Hello, World!\n");
+}
+
+RING_FUNC(ring_sumtwonumbers)
+{
+	double nNum1,nNum2,nSum;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 2) {
+			RING_API_ERROR(RING_API_MISS2PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! (RING_API_ISNUMBER(1) && RING_API_ISNUMBER(2)) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Sum Numbers 
+		nNum1 = RING_API_GETNUMBER(1);
+		nNum2 = RING_API_GETNUMBER(2);
+		nSum  = nNum1 + nNum2 ;
+	// Return Output
+		RING_API_RETNUMBER(nSum);
+}
+
+RING_FUNC(ring_sayhello)
+{
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 1) {
+			RING_API_ERROR(RING_API_MISS1PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! RING_API_ISSTRING(1) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	printf("Hello %s\n",RING_API_GETSTRING(1));
+}
+
+RING_FUNC(ring_sumlist)
+{
+	List *pList;
+	int x,nSum;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 1) {
+			RING_API_ERROR(RING_API_MISS1PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! RING_API_ISPOINTER(1) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Sum List Numbers 
+		nSum = 0;
+		pList = RING_API_GETLIST(1);
+		for(x=1 ; x <= ring_list_getsize(pList) ; x++) {
+			if ( ring_list_isdouble(pList,x) ) {
+				nSum += (int) ring_list_getdouble(pList,x) ;
+			}
+		}
+	// Return Output 
+		RING_API_RETNUMBER(nSum);
+}
+
+RING_FUNC(ring_inclist)
+{
+	List *pList;
+	int x,nSum;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 2) {
+			RING_API_ERROR(RING_API_MISS2PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! ( RING_API_ISPOINTER(1) && RING_API_ISNUMBER(2) ) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Increment List Items
+		nSum = 0;
+		pList = RING_API_GETLIST(1);
+		for(x=1 ; x <= ring_list_getsize(pList) ; x++) {
+			if ( ring_list_isdouble(pList,x) ) {
+				ring_list_setdouble(pList,x,ring_list_getdouble(pList,x)+RING_API_GETNUMBER(2)) ;
+			}
+		}
+	// Return Output
+		RING_API_RETLIST(pList);
+}
+
+RING_FUNC(ring_filterlist)
+{
+	List *pList;
+	int x;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 2) {
+			RING_API_ERROR(RING_API_MISS2PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! ( RING_API_ISPOINTER(1) && RING_API_ISNUMBER(2) ) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Filter List Items
+		pList = RING_API_GETLIST(1);
+		for(x = ring_list_getsize(pList) ; x >= 1 ; x--) 
+			if ( ring_list_isdouble(pList,x) ) 
+				if ( ! (ring_list_getdouble(pList,x) > RING_API_GETNUMBER(2)) )
+					ring_list_deleteitem(pList,x) ;
+	// Return Output
+		RING_API_RETLIST(pList);
+}
+
+RING_FUNC(ring_replicatelist)
+{
+	List *pList;
+	int x,y,nTimes,nSize;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 2) {
+			RING_API_ERROR(RING_API_MISS2PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! ( RING_API_ISPOINTER(1) && RING_API_ISNUMBER(2) ) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Replicate List Items
+		pList = RING_API_GETLIST(1);
+		nSize = ring_list_getsize(pList);
+		nTimes = (int) RING_API_GETNUMBER(2);
+		if (nTimes < 1) {
+			RING_API_ERROR("Error: The second parameter must be >= 1 \n");
+			return;
+		}
+		for(x = 1 ; x <= nTimes ; x++) 
+			for(y = 1 ; y <= nSize ; y++) 
+				if ( ring_list_isdouble(pList,y) ) 
+					ring_list_adddouble(pList,ring_list_getdouble(pList,y));
+	// Return Output
+		RING_API_RETLIST(pList);
+}
+
+RING_FUNC(ring_generatelist)
+{
+	List *pList;
+	int x,nSize;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 1) {
+			RING_API_ERROR(RING_API_MISS1PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! RING_API_ISNUMBER(1) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Create the List
+		pList = RING_API_NEWLIST;
+	// Generte List Items
+		nSize = (int) RING_API_GETNUMBER(1);
+		if (nSize < 1) {
+			RING_API_ERROR("Error: The list size must be >= 1 \n");
+			return;
+		}
+		for(x = 1 ; x <= nSize ; x++) 
+			ring_list_adddouble(pList,(double) x);
+	// Return Output
+		RING_API_RETLIST(pList);
+}
+
+void mylib_displaylist(List *pList);
+
+RING_FUNC(ring_displaylist)
+{
+	List *pList;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 1) {
+			RING_API_ERROR(RING_API_MISS1PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! RING_API_ISPOINTER(1) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Get the List
+		pList = RING_API_GETLIST(1);
+	// Display the List Items including Nested Lists
+		mylib_displaylist(pList);
+}
+
+void mylib_displaylist(List *pList) {
+	int x;
+	for (x=1; x <= ring_list_getsize(pList); x++) {
+		if ( ring_list_isdouble(pList,x) ) {
+			printf("Number : %f \n", ring_list_getdouble(pList,x) ) ;
+		} else if ( ring_list_isstring(pList,x) ) {
+			printf("String : %s \n", ring_list_getstring(pList,x) ) ;
+		} else if ( ring_list_islist(pList,x) ) {
+			printf("Sub List..\n");
+			mylib_displaylist( ring_list_getlist(pList,x) );		
+		}
+	}
+}
+
+RING_FUNC(ring_updatetable)
+{
+	List *pList, *pRow;
+	int nRow,nCol;
+	// Check Parameters Count
+		if (RING_API_PARACOUNT != 2) {
+			RING_API_ERROR(RING_API_MISS2PARA);
+			return;
+		}
+	// Check Parameters Type
+		if ( ! ( RING_API_ISPOINTER(1) && RING_API_ISNUMBER(2) ) ) {
+			RING_API_ERROR(RING_API_BADPARATYPE);
+			return;
+		}
+	// Get the List
+		pList = RING_API_GETLIST(1);
+	// Display the List Rows and Columns
+		for (nRow = 1 ; nRow <= ring_list_getsize(pList) ; nRow++ ) {
+			if ( ring_list_islist(pList,nRow) ) {
+				pRow = ring_list_getlist(pList,nRow);
+				for (nCol = 1 ; nCol <= ring_list_getsize(pRow) ; nCol++ ) {
+					if ( ring_list_isdouble(pRow,nCol) ) {
+						ring_list_setdouble(pRow,nCol,RING_API_GETNUMBER(2));
+					} else {
+						RING_API_ERROR("Error : We expect numbers!\n");
+					}
+				}
+			} else {
+				RING_API_ERROR("Error : The parameter is not a table! \n");
+			}
+		}
+}
+
+
+RING_API void ringlib_init(RingState *pRingState)
+{
+	ring_vm_funcregister("myfunction",ring_myfunction);
+	ring_vm_funcregister("sumtwonumbers",ring_sumtwonumbers);
+	ring_vm_funcregister("sayhello",ring_sayhello);
+	ring_vm_funcregister("sumlist",ring_sumlist);
+	ring_vm_funcregister("inclist",ring_inclist);
+	ring_vm_funcregister("filterlist",ring_filterlist);
+	ring_vm_funcregister("replicatelist",ring_replicatelist);
+	ring_vm_funcregister("generatelist",ring_generatelist);
+	ring_vm_funcregister("displaylist",ring_displaylist);
+	ring_vm_funcregister("updatetable",ring_updatetable);
+}
