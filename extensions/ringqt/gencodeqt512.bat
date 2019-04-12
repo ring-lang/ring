ca2df930b0c98bf62777e5be01158da1946f365f
Mahmoud Fayed <msfclipper@yahoo.com>
2019-04-12 09:28:40 +0300

    Update RingQt - extensions/ringqt/gencodeqt512.bat

--- extensions/ringqt/gencodeqt512.bat
+++ extensions/ringqt/gencodeqt512.bat
@@ -1,7 +1,8 @@
 set RINGQT_ANDROID=
 set RINGQT_NOWEBKIT=1
+set RINGQT_QT512=1
 ring ..\codegen\parsec.ring classes\qt.cf cpp\src\ring_qt.cpp guilib\ring_qt.ring
 cd events
 ring qtevents.ring
 cd ..
-
+set RINGQT_QT512=
