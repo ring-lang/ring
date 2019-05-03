51bed06fb8178d183b14c003e94b73fdf71f6783
Mahmoud Fayed <msfclipper@yahoo.com>
2019-05-03 17:40:34 +0300

    Update RingQt - Update from Qt 5.12.2 to Qt 5.12.3

--- extensions/ringqt/buildqt512.bat
+++ extensions/ringqt/buildqt512.bat
@@ -1,5 +1,5 @@
 call ..\..\src\locatevc.bat
-"C:\Qt\Qt5.12.2\5.12.2\msvc2017\bin\qmake.exe" ring_qt512.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-rem "C:\Qt\Qt5.12.2\Tools\QtCreator\bin\jom.exe" -f ../build-ring_qt512-Desktop_Qt_5_12_2_MSVC2017_32bit-Release/Makefile qmake_all
-"C:\Qt\Qt5.12.2\Tools\QtCreator\bin\jom.exe"
+"C:\Qt\Qt5.12.3\5.12.3\msvc2017\bin\qmake.exe" ring_qt512.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+rem "C:\Qt\Qt5.12.3\Tools\QtCreator\bin\jom.exe" -f ../build-ring_qt512-Desktop_Qt_5_12_3_MSVC2017_32bit-Release/Makefile qmake_all
+"C:\Qt\Qt5.12.3\Tools\QtCreator\bin\jom.exe"
 copy release\ringqt.dll ..\..\bin
