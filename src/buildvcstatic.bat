bcdfbb0a3a1bdbd289ce739b51a3738e6a27bb0c
Mahmoud Fayed <msfclipper@yahoo.com>
2019-05-25 07:22:07 +0300

    Update src/buildvcstatic.bat

--- src/buildvcstatic.bat
+++ src/buildvcstatic.bat
@@ -1,8 +1,8 @@
 echo off
-call "C:\Program Files\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+call locatevc.bat
 
 cls
-cl /Ox ring.c  ^
+cl /DEBUG:FULL /Z7 ring.c  ^
 ring_string.c ring_list.c ring_item.c ring_items.c ring_hashtable.c ring_state.c ring_scanner.c ring_parser.c ring_hashlib.c ring_vmgc.c ^
 ring_stmt.c ring_expr.c ring_codegen.c ring_vm.c ring_vmexpr.c ring_vmvars.c ^
 ring_vmlists.c ring_vmfuncs.c ring_api.c ring_vmoop.c ring_vmcui.c ^
