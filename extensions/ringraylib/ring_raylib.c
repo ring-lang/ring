3899896bb520111b8c587a3fb268f6e7f79cc52a
Mahmoud Fayed <msfclipper@yahoo.com>
2019-06-24 02:23:56 +0300

    Update RingRayLib - raylib.c - Add Function : int GetMouseWheelMove(void)

--- extensions/ringraylib/ring_raylib.c
+++ extensions/ringraylib/ring_raylib.c
@@ -1425,6 +1425,16 @@ RING_FUNC(ring_SetMouseScale)
 	SetMouseScale( (float ) RING_API_GETNUMBER(1), (float ) RING_API_GETNUMBER(2));
 }
 
+
+RING_FUNC(ring_GetMouseWheelMove)
+{
+	if ( RING_API_PARACOUNT != 0 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	RING_API_RETNUMBER(GetMouseWheelMove());
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("initwindow",ring_InitWindow);
@@ -1531,4 +1541,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("setmouseposition",ring_SetMousePosition);
 	ring_vm_funcregister("setmouseoffset",ring_SetMouseOffset);
 	ring_vm_funcregister("setmousescale",ring_SetMouseScale);
+	ring_vm_funcregister("getmousewheelmove",ring_GetMouseWheelMove);
 }
