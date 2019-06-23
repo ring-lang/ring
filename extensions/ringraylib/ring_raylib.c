be5c43b59f739a3680f091081ec17e890a9e02ba
Mahmoud Fayed <msfclipper@yahoo.com>
2019-06-24 02:26:28 +0300

    Update RingRayLib - raylib.c - Add Function : void ImageFormat(Image *image, int newFormat)

--- extensions/ringraylib/ring_raylib.c
+++ extensions/ringraylib/ring_raylib.c
@@ -2904,6 +2904,24 @@ RING_FUNC(ring_ImageToPOT)
 		ring_state_free(((VM *) pPointer)->pRingState,RING_API_GETCPOINTER(2,"Color"));
 }
 
+
+RING_FUNC(ring_ImageFormat)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	ImageFormat((Image *) RING_API_GETCPOINTER(1,"Image"), (int ) RING_API_GETNUMBER(2));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("initwindow",ring_InitWindow);
@@ -3088,4 +3106,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("updatetexture",ring_UpdateTexture);
 	ring_vm_funcregister("imagecopy",ring_ImageCopy);
 	ring_vm_funcregister("imagetopot",ring_ImageToPOT);
+	ring_vm_funcregister("imageformat",ring_ImageFormat);
 }
