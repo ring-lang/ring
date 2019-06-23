a2c88d293eebe5a267b91eaa8b9716b8c8eaa0fb
Mahmoud Fayed <msfclipper@yahoo.com>
2019-06-24 02:24:41 +0300

    Update RingRayLib - raylib.c - Add Function : void DrawLineEx(Vector2 startPos, Vector2 endPos, float thick, Color color)

--- extensions/ringraylib/ring_raylib.c
+++ extensions/ringraylib/ring_raylib.c
@@ -1766,6 +1766,26 @@ RING_FUNC(ring_DrawLineV)
 		ring_state_free(((VM *) pPointer)->pRingState,RING_API_GETCPOINTER(3,"Color"));
 }
 
+
+RING_FUNC(ring_DrawLineEx)
+{
+	if ( RING_API_PARACOUNT != 4 ) {
+		RING_API_ERROR(RING_API_MISS4PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	DrawLineEx(* (Vector2  *) RING_API_GETCPOINTER(1,"Vector2"),* (Vector2  *) RING_API_GETCPOINTER(2,"Vector2"), (float ) RING_API_GETNUMBER(3),* (Color  *) RING_API_GETCPOINTER(4,"Color"));
+	if (RING_API_ISCPOINTERNOTASSIGNED(1))
+		ring_state_free(((VM *) pPointer)->pRingState,RING_API_GETCPOINTER(1,"Vector2"));
+	if (RING_API_ISCPOINTERNOTASSIGNED(2))
+		ring_state_free(((VM *) pPointer)->pRingState,RING_API_GETCPOINTER(2,"Vector2"));
+	if (RING_API_ISCPOINTERNOTASSIGNED(4))
+		ring_state_free(((VM *) pPointer)->pRingState,RING_API_GETCPOINTER(4,"Color"));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("initwindow",ring_InitWindow);
@@ -1895,4 +1915,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("drawpixelv",ring_DrawPixelV);
 	ring_vm_funcregister("drawline",ring_DrawLine);
 	ring_vm_funcregister("drawlinev",ring_DrawLineV);
+	ring_vm_funcregister("drawlineex",ring_DrawLineEx);
 }
