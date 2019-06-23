5a330251f9ba126e222fa713e2fad62806927f31
Mahmoud Fayed <msfclipper@yahoo.com>
2019-06-24 02:30:22 +0300

    Update RingRayLib - raylib.c - Add Function : Mesh GenMeshSphere(float radius, int rings, int slices)

--- extensions/ringraylib/ring_raylib.c
+++ extensions/ringraylib/ring_raylib.c
@@ -5239,6 +5239,33 @@ RING_FUNC(ring_GenMeshCube)
 	}
 }
 
+
+RING_FUNC(ring_GenMeshSphere)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	{
+		Mesh *pValue ; 
+		pValue = (Mesh *) ring_state_malloc(((VM *) pPointer)->pRingState,sizeof(Mesh)) ;
+		*pValue = GenMeshSphere( (float ) RING_API_GETNUMBER(1), (int ) RING_API_GETNUMBER(2), (int ) RING_API_GETNUMBER(3));
+		RING_API_RETMANAGEDCPOINTER(pValue,"Mesh",ring_state_free);
+	}
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("initwindow",ring_InitWindow);
@@ -5535,4 +5562,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("genmeshpoly",ring_GenMeshPoly);
 	ring_vm_funcregister("genmeshplane",ring_GenMeshPlane);
 	ring_vm_funcregister("genmeshcube",ring_GenMeshCube);
+	ring_vm_funcregister("genmeshsphere",ring_GenMeshSphere);
 }
