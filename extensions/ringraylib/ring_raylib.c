7e7104dfc2cb986065843a21a74bc64cd5da4e3c
Mahmoud Fayed <msfclipper@yahoo.com>
2019-06-24 02:22:09 +0300

    Update RingRayLib - raylib.c - Add Function : void SetTraceLogExit(int logType)

--- extensions/ringraylib/ring_raylib.c
+++ extensions/ringraylib/ring_raylib.c
@@ -723,6 +723,20 @@ RING_FUNC(ring_SetTraceLogLevel)
 	SetTraceLogLevel( (int ) RING_API_GETNUMBER(1));
 }
 
+
+RING_FUNC(ring_SetTraceLogExit)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	SetTraceLogExit( (int ) RING_API_GETNUMBER(1));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("initwindow",ring_InitWindow);
@@ -781,4 +795,5 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("fade",ring_Fade);
 	ring_vm_funcregister("setconfigflags",ring_SetConfigFlags);
 	ring_vm_funcregister("settraceloglevel",ring_SetTraceLogLevel);
+	ring_vm_funcregister("settracelogexit",ring_SetTraceLogExit);
 }
