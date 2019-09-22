35683008b2c551fc90d8254e04709a7e28537baa
Mahmoud Fayed <msfclipper@yahoo.com>
2019-09-22 08:04:42 +0300

    Add gencodeqt512.sh

---
+++ extensions/ringqt/gencodeqt512.sh
@@ -0,0 +1,8 @@
+export RINGQT_ANDROID=
+export RINGQT_NOWEBKIT=1
+export RINGQT_QT512=1
+../../bin/ring ../codegen/parsec.ring classes/qt.cf cpp/src/ring_qt.cpp guilib/ring_qt.ring
+cd events
+../../../bin/ring qtevents.ring
+cd ..
+export RINGQT_QT512=
