a6e719ffe23eff036c2c6a0e6c22d529904b9202
Mahmoud Fayed <msfclipper@yahoo.com>
2020-01-02 13:24:17 +0300

    Update RingQt - batch files - use Qt 5.12.6

--- extensions/ringqt/buildqt512_nobluetooth.bat
+++ extensions/ringqt/buildqt512_nobluetooth.bat
@@ -1,5 +1,5 @@
 call ..\..\src\locatevc.bat
-"C:\Qt\Qt5.12.3\5.12.3\msvc2017\bin\qmake.exe" ring_qt512_nobluetooth.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
-rem "C:\Qt\Qt5.12.3\Tools\QtCreator\bin\jom.exe" -f ../build-ring_qt512-Desktop_Qt_5_12_3_MSVC2017_32bit-Release/Makefile qmake_all
-"C:\Qt\Qt5.12.3\Tools\QtCreator\bin\jom.exe"
+"C:\Qt\Qt5.12.6\5.12.6\msvc2017\bin\qmake.exe" ring_qt512_nobluetooth.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+rem "C:\Qt\Qt5.12.6\Tools\QtCreator\bin\jom.exe" -f ../build-ring_qt512-Desktop_Qt_5_12_3_MSVC2017_32bit-Release/Makefile qmake_all
+"C:\Qt\Qt5.12.6\Tools\QtCreator\bin\jom.exe"
 copy release\ringqt.dll ..\..\bin
