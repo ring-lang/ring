5d12231ef2c6425461d44cd178c1fc6c1e353660
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-14 19:37:26 +0300

    Update extensions/ringopengl/opengl32/buildvc.bat

--- extensions/ringopengl/opengl32/buildvc.bat
+++ extensions/ringopengl/opengl32/buildvc.bat
@@ -1,5 +1,5 @@
 cls
 call ../../../src/locatevc.bat
-cl /c /DEBUG ring_opengl32.c -I "..\..\..\libdepwin\glew\include" -I "..\..\..\libdepwin\freeglut\include" -I"..\..\..\include"
-link /DEBUG ring_opengl32.obj  ..\..\..\lib\ring.lib ..\..\..\libdepwin\glew\lib\release\win32\glew32.lib ..\..\..\libdepwin\freeglut\lib\freeglut.lib /DLL /OUT:..\..\..\bin\ring_opengl32.dll /SUBSYSTEM:CONSOLE,"5.01" 
+cl /c /DEBUG ring_opengl32.c -I "..\..\..\extensions\libdepwin\glew\include" -I "..\..\..\extensions\libdepwin\freeglut\include" -I"..\..\..\include"
+link /DEBUG ring_opengl32.obj  ..\..\..\lib\ring.lib ..\..\..\extensions\libdepwin\glew\lib\release\win32\glew32.lib ..\..\..\extensions\libdepwin\freeglut\lib\freeglut.lib /DLL /OUT:..\..\..\bin\ring_opengl32.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_opengl32.obj
