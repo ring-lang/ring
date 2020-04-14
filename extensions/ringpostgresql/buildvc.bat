b475d1f30bac3c159a5da5400ba5f4746e1fd509
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-14 19:27:05 +0300

    Update RingPostgreSQL - extensions/ringpostgresql/buildvc.bat

--- extensions/ringpostgresql/buildvc.bat
+++ extensions/ringpostgresql/buildvc.bat
@@ -1,5 +1,5 @@
 cls
 call ../../src/locatevc.bat
-cl /c /DEBUG ring_pgsql.c -I"..\..\libdepwin\pgsql\include" -I"..\..\include"
-link /DEBUG ring_pgsql.obj  ..\..\lib\ring.lib ..\..\libdepwin\pgsql\lib\libpq.lib /DLL /OUT:..\..\bin\ring_pgsql.dll /SUBSYSTEM:CONSOLE,"5.01" 
+cl /c /DEBUG ring_pgsql.c -I"..\..\extensions\libdepwin\pgsql\include" -I"..\..\include"
+link /DEBUG ring_pgsql.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\pgsql\lib\libpq.lib /DLL /OUT:..\..\bin\ring_pgsql.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_pgsql.obj
