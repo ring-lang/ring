7300d8b76138a344fcb87f49d1844d9b69d753d8
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-14 19:14:51 +0300

    Update RingAllegro - extensions/ringallegro/buildvc_allegro5.2.4.bat

--- extensions/ringallegro/buildvc_allegro5.2.4.bat
+++ extensions/ringallegro/buildvc_allegro5.2.4.bat
@@ -1,8 +1,8 @@
 cls
 call ../../src/locatevc.bat
-cl /c /DEBUG ring_allegro.c -I"..\..\libdepwin\allegro5.2.4\include" -I"..\..\include"
-link /DEBUG ring_allegro.obj  ..\..\lib\ring.lib ..\..\libdepwin\allegro5.2.4\lib\allegro.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_image.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_dialog.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_ttf.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_acodec.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_primitives.lib ^
-..\..\libdepwin\allegro5.2.4\lib\allegro_audio.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_color.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_font.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_video.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_primitives.lib ..\..\libdepwin\allegro5.2.4\lib\allegro_memfile.lib ^
-		..\..\libdepwin\allegro5.2.4\lib\allegro_physfs.lib ^
+cl /c /DEBUG ring_allegro.c -I"..\..\extensions\libdepwin\allegro5.2.4\include" -I"..\..\include"
+link /DEBUG ring_allegro.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_image.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_dialog.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_ttf.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_acodec.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_primitives.lib ^
+..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_audio.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_color.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_font.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_video.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_primitives.lib ..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_memfile.lib ^
+		..\..\extensions\libdepwin\allegro5.2.4\lib\allegro_physfs.lib ^
 	  /DLL /OUT:..\..\bin\ring_allegro.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_allegro.obj
