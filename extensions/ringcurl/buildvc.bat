e5d6db4eda4cc6ffd5da8152524cf383a012c8cd
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-14 19:22:12 +0300

    Update RingLibCurl - extensions/ringcurl/buildvc.bat

--- extensions/ringcurl/buildvc.bat
+++ extensions/ringcurl/buildvc.bat
@@ -1,5 +1,5 @@
 cls
 call ../../src/locatevc.bat
-cl /c /DEBUG ring_libcurl.c -I"..\..\libdepwin\libcurl\include" -I"..\..\include"
-link /DEBUG ring_libcurl.obj  ..\..\lib\ring.lib ..\..\libdepwin\libcurl\lib\libcurl.lib /DLL /OUT:..\..\bin\ring_libcurl.dll /SUBSYSTEM:CONSOLE,"5.01" 
+cl /c /DEBUG ring_libcurl.c -I"..\..\extensions\libdepwin\libcurl\include" -I"..\..\include"
+link /DEBUG ring_libcurl.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\libcurl\lib\libcurl.lib /DLL /OUT:..\..\bin\ring_libcurl.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_libcurl.obj
