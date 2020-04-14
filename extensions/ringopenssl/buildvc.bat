b712ad09fd85aeb5df7050f9aa48e1ac5ea20280
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-14 19:26:01 +0300

    Update RingOpenSSL - extensions/ringopenssl/buildvc.bat

--- extensions/ringopenssl/buildvc.bat
+++ extensions/ringopenssl/buildvc.bat
@@ -1,5 +1,5 @@
 cls
 call ../../src/locatevc.bat
-cl /c /DEBUG ring_vmopenssl.c -I"..\..\include" -I"..\..\libdepwin\OPENSSL\include"
-link /DEBUG ring_vmopenssl.obj  ..\..\lib\ring.lib ..\..\libdepwin\OpenSSL\lib\libeay32.lib /DLL /OUT:..\..\bin\ring_openssl.dll /SUBSYSTEM:CONSOLE,"5.01" 
+cl /c /DEBUG ring_vmopenssl.c -I"..\..\include" -I"..\..\extensions\libdepwin\OPENSSL\include"
+link /DEBUG ring_vmopenssl.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\OpenSSL\lib\libeay32.lib /DLL /OUT:..\..\bin\ring_openssl.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_vmopenssl.obj
