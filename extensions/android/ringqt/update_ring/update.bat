72e6aad691f400ae3fcf1a6c7f3cf178491b2a8e
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-15 15:44:20 +0300

    Update extensions/android/ringqt/update_ring/update.bat

--- extensions/android/ringqt/update_ring/update.bat
+++ extensions/android/ringqt/update_ring/update.bat
@@ -1,13 +1,13 @@
 rem	Simple Batch file to update Ring and RingQt in the Android-Qt Version 
 rem	Author : Mahmoud Fayed <msfclipper@yahoo.com>
 
-copy ..\..\..\src\*.c ..\project\
+copy ..\..\..\..\src\*.c ..\project\
 copy ring_ext.c ..\project\
-copy ..\..\..\include\*.h ..\project\
+copy ..\..\..\..\include\*.h ..\project\
 copy ring_ext.h ..\project\
-cd ..\..\..\extensions\ringqt\
+cd ..\..\..\ringqt\
 call gencodeandroid.bat
-copy cpp\src\*.cpp ..\..\android\ringqt\project\
-copy cpp\include\*.h ..\..\android\ringqt\project\
+copy cpp\src\*.cpp ..\android\ringqt\project\
+copy cpp\include\*.h ..\android\ringqt\project\
 call gencodeqt512_nobluetooth.bat
-cd ..\..\android\ringqt\update_ring
+cd ..\android\ringqt\update_ring
