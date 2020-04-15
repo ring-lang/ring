f37742c125fa59aabf8db1e83013bc6de594e92c
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-15 15:44:31 +0300

    Update extensions/android/ringqt/update_ring/updateringonly.bat

--- extensions/android/ringqt/update_ring/updateringonly.bat
+++ extensions/android/ringqt/update_ring/updateringonly.bat
@@ -1,7 +1,7 @@
 rem	Simple Batch file to update Ring only Android-Qt Version 
 rem	Author : Mahmoud Fayed <msfclipper@yahoo.com>
 
-copy ..\..\..\src\*.c ..\project\
+copy ..\..\..\..\src\*.c ..\project\
 copy ring_ext.c ..\project\
-copy ..\..\..\include\*.h ..\project\
+copy ..\..\..\..\include\*.h ..\project\
 copy ring_ext.h ..\project\
