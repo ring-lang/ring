496046ae7006b34bcaf777e887fe06662d8e053d
Mahmoud Fayed <msfclipper@yahoo.com>
2020-04-21 22:33:15 +0300

    Update RingQt

---
+++ extensions/ringqt/cpp/include/gbarcategoryaxis.h
@@ -0,0 +1,61 @@
+
+/* Copyright (c) 2013-2020 Mahmoud Fayed <msfclipper@yahoo.com> */
+#ifndef GBARCATEGORYAXIS_H
+#define GBARCATEGORYAXIS_H
+#include <QApplication>
+#include "ringqt.h"
+#include <QtCharts>
+extern "C" {
+#include "ring.h"
+}
+
+class GBarCategoryAxis : public QtCharts::QBarCategoryAxis
+{
+
+  Q_OBJECT
+
+  public:
+
+    VM *pVM;
+
+    List *pParaList;
+
+    char ccategoriesChangedEvent[RINGQT_EVENT_SIZE];
+    char ccountChangedEvent[RINGQT_EVENT_SIZE];
+    char cmaxChangedEvent[RINGQT_EVENT_SIZE];
+    char cminChangedEvent[RINGQT_EVENT_SIZE];
+    char crangeChangedEvent[RINGQT_EVENT_SIZE];
+
+
+    GBarCategoryAxis(QObject *parent,VM *pVM );
+
+    ~GBarCategoryAxis();
+
+    void geteventparameters(void) ;
+
+    void setcategoriesChangedEvent(const char *cStr);
+    void setcountChangedEvent(const char *cStr);
+    void setmaxChangedEvent(const char *cStr);
+    void setminChangedEvent(const char *cStr);
+    void setrangeChangedEvent(const char *cStr);
+
+
+    const char *getcategoriesChangedEvent(void);
+    const char *getcountChangedEvent(void);
+    const char *getmaxChangedEvent(void);
+    const char *getminChangedEvent(void);
+    const char *getrangeChangedEvent(void);
+
+
+  public slots:
+
+    void categoriesChangedSlot();
+    void countChangedSlot();
+    void maxChangedSlot();
+    void minChangedSlot();
+    void rangeChangedSlot();
+
+};
+
+#endif
+
