f20e9c3d62f1d9765bc461860a3180dcbc4cbb78
Mahmoud Fayed <msfclipper@yahoo.com>
2020-06-22 23:40:31 +0300

    Update Documentation - Better Style

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,12 +116,12 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'alabaster'
+#html_theme = 'classic' 
 
-html_theme = "classic"
+html_theme = "haiku"
 html_theme_options = {
-    "rightsidebar": "true",
-    "relbarbgcolor": "black"
+    #"rightsidebar": "true",
+    #"relbarbgcolor": "black"
 }
 
 #html_theme = "sphinx_rtd_theme"
