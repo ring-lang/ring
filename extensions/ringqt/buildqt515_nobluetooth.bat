7466e59dcd25a8fa96377557dd7419f724150794
Mahmoud Fayed <msfclipper@yahoo.com>
2020-06-27 06:01:44 +0300

    Update RingQt - Support building using Qt 5.15 - Add extensions/ringqt/buildqt515_nobluetooth.bat

---
+++ extensions/ringqt/buildqt515_nobluetooth.bat
@@ -0,0 +1,4 @@
+call ..\..\src\locatevc.bat
+"C:\Qt\5.15.0\msvc2019\bin\qmake.exe" ring_qt512_nobluetooth.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+"C:\Qt\Tools\QtCreator\bin\jom.exe"
+copy release\ringqt.dll ..\..\bin
