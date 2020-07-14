c907dd3e60f2378d5fc7eea9921f63872cb08ebf
Mahmoud Fayed <msfclipper@yahoo.com>
2020-07-14 12:37:24 +0300

    Update Ring API - Add RING_API_RETLISTBYREF

--- include/ring_api.h
+++ include/ring_api.h
@@ -47,6 +47,8 @@ RING_API void * ring_vm_api_getcpointer2pointer ( void *pPointer,int x,const cha
 RING_API void ring_list_addcpointer_gc ( void *pState,List *pList,void *pGeneral,const char *cType ) ;
 
 RING_API void ring_vm_api_retcpointer2 ( void *pPointer,void *pGeneral,const char *cType, void (* pFreeFunc)(void *,void *) ) ;
+
+RING_API void ring_vm_api_retlist2 ( void *pPointer,List *pList,int lRef ) ;
 /*
 **  Library 
 **  General 
@@ -282,6 +284,7 @@ void ring_vmlib_addsublistsbyfastcopy ( void *pPointer ) ;
 #define RING_API_ISOBJECT(x) ring_vm_api_isobject(pPointer,x)
 #define RING_API_GETCPOINTER2POINTER(x,y) (ring_vm_api_getcpointer2pointer((VM *) pPointer,x,y))
 #define RING_API_RETMANAGEDCPOINTER(x,y,z) (ring_vm_api_retcpointer2((VM *) pPointer,(void *) x,y,z))
+#define RING_API_RETLISTBYREF(x) ring_vm_api_retlist2((VM *) pPointer,x,1)
 /* Constants/MACRO */
 #define RING_API_MISS1PARA "Bad parameters count, the function expect one parameter"
 #define RING_API_MISS2PARA "Bad parameters count, the function expect two parameters"
