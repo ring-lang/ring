bc9682f3d1332089306515af37950254a8eca8f8
Mahmoud Fayed <msfclipper@yahoo.com>
2020-07-21 17:59:07 +0300

    Update bin/install.bat

--- bin/install.bat
+++ bin/install.bat
@@ -1,15 +1,26 @@
 Rem Simple Batch File to copy ring DLL files and libraries to the bin folder
-Rem 2016, Mahmoud Fayed <msfclipper@yahoo.com>
+Rem 2016-2020, Mahmoud Fayed <msfclipper@yahoo.com>
 
 cd ..
 
 copy lib\*.dll bin
-copy libdepwin\allegro5.2.4\bin\*.dll bin
-copy libdepwin\freeglut\bin\*.dll bin
-copy libdepwin\glew\bin\Release\Win32\*.dll bin
-copy libdepwin\libcurl\bin\*.dll bin
-copy libdepwin\mysql\lib\*.dll bin
-copy libdepwin\openssl\bin\*.dll bin
-copy libdepwin\libuv\*.dll bin
-
-copy C:\Qt\Qt5.5.1\5.5\mingw492_32\bin\*.dll bin
+copy extensions\libdepwin\allegro5.2.4\bin\*.dll bin
+copy extensions\libdepwin\freeglut\bin\*.dll bin
+copy extensions\libdepwin\glew\bin\Release\Win32\*.dll bin
+copy extensions\libdepwin\libcurl\bin\*.dll bin
+copy extensions\libdepwin\libsdl\SDL2_vc\lib\x86\*.dll bin
+copy extensions\libdepwin\libsdl\SDL2_image\lib\x86\*.dll bin
+copy extensions\libdepwin\libsdl\SDL2_mixer\lib\x86\*.dll bin
+copy extensions\libdepwin\libsdl\SDL2_net\lib\x86\*.dll bin
+copy extensions\libdepwin\libsdl\SDL2_ttf\lib\x86\*.dll bin
+copy extensions\libdepwin\libuv\*.dll bin
+copy extensions\libdepwin\mysql\lib\*.dll bin
+copy extensions\libdepwin\openssl\bin\*.dll bin
+copy extensions\libdepwin\pgsql\lib\*.dll bin
+copy extensions\libdepwin\raylib2.5\bin\*.dll bin
+copy C:\Qt\Qt5.12.6\5.12.6\msvc2017\bin\*.dll bin
+cd bin
+rem Delete Allegro Dlls (Debug Version)
+del allegro*debug-5.2.dll 
+rem Delete Qt 5 Dlls (Debug Version)
+del qt5*d.dll
