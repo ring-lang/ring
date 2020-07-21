1242f61cdb2e63f3797fef5078bff67040352cdf
Mahmoud Fayed <msfclipper@yahoo.com>
2020-07-22 00:02:41 +0300

    Update extensions/ringmurmurhash/buildvc.bat

--- extensions/ringmurmurhash/buildvc.bat
+++ extensions/ringmurmurhash/buildvc.bat
@@ -2,6 +2,6 @@ cls
 call ../../language/src/locatevc.bat
 cl /c /DEBUG ring_murmurhash.c libmurmurhash/MurmurHash1.c ^
     libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c ^
-     -I"../../include" -I"libmurmurhash/"
+     -I"../../language/include" -I"libmurmurhash/"
 link /DEBUG *.obj ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_murmurhash.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del *.obj
