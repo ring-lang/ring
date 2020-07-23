4aa88471671fb57c288870ca1393a81f35d0665d
Mahmoud Fayed <msfclipper@yahoo.com>
2020-07-23 14:44:16 +0300

    Update RingQt - Batch Files

--- extensions/ringqt/buildqt512.bat
+++ extensions/ringqt/buildqt512.bat
@@ -1,4 +1,4 @@
-call ..\..\src\locatevc.bat
+call ..\..\language\src\locatevc.bat
 "C:\Qt\Qt5.12.6\5.12.6\msvc2017\bin\qmake.exe" ring_qt512.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
 rem "C:\Qt\Qt5.12.6\Tools\QtCreator\bin\jom.exe" -f ../build-ring_qt512-Desktop_Qt_5_12_3_MSVC2017_32bit-Release/Makefile qmake_all
 "C:\Qt\Qt5.12.6\Tools\QtCreator\bin\jom.exe"
