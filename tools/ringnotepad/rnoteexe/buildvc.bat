d34d5ce7f26117f867efbce819d45b22391eb0a0
Mahmoud Fayed <msfclipper@yahoo.com>
2020-07-25 21:42:29 +0300

    Update tools/ringnotepad/rnoteexe/buildvc.bat

--- tools/ringnotepad/rnoteexe/buildvc.bat
+++ tools/ringnotepad/rnoteexe/buildvc.bat
@@ -5,6 +5,6 @@ call ../../../language/src/locatevc.bat
 cls
 
 rc rnote.rc
-cl /DEBUG rnote.c rnote.res /link advapi32.lib shell32.lib /SUBSYSTEM:WINDOWS,"5.01" 
+cl rnote.c rnote.res /link advapi32.lib shell32.lib /SUBSYSTEM:WINDOWS,"5.01" 
 
 copy rnote.exe ..\..\..\RingNotepad.exe
