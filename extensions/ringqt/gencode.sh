3a61f0116beee36d3956c5e13237f8eea52a081d
Mahmoud Fayed <msfclipper@yahoo.com>
2020-09-21 07:26:52 +0300

    Update RingQt - Scripts

--- extensions/ringqt/gencode.sh
+++ extensions/ringqt/gencode.sh
@@ -2,7 +2,7 @@ export RINGQT_ANDROID=
 export RINGQT_NOWEBKIT=
 export RINGQT_NOBLUETOOTH=
 export RINGQT_QT512=
-../../bin/ring ../codegen/parsec.ring classes/qt.cf cpp/src/ring_qt.cpp ../../ringlibs/guilib/classes/ring_qt.ring
+../../bin/ring ../codegen/parsec.ring classes/qt.cf cpp/src/ring_qt.cpp ../../libraries/guilib/classes/ring_qt.ring
 cd events
 ../../../bin/ring qtevents.ring
 cd ..
