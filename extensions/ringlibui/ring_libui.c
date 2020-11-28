bdd824b98669ebf7df4f01635407740fb7940520
Mahmoud Fayed <msfclipper@yahoo.com>
2020-11-28 03:36:26 +0300

    Update RingLibUI - Add ui.h contents to libui.cf as comments

--- extensions/ringlibui/ring_libui.c
+++ extensions/ringlibui/ring_libui.c
@@ -5,6 +5,12 @@
 #include <ui.h>
 #include <ui_windows.h>
 
+RING_FUNC(ring_get_uipi)
+{
+	RING_API_RETNUMBER(uiPi);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
+	ring_vm_funcregister("get_uipi",ring_get_uipi);
 }
