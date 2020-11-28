54578c467bf0408326f7074fe3ba59af2bd7bc87
Mahmoud Fayed <msfclipper@yahoo.com>
2020-11-28 04:01:34 +0300

    Update RingLibUI - uiInit() function

--- extensions/ringlibui/ring_libui.c
+++ extensions/ringlibui/ring_libui.c
@@ -11,7 +11,22 @@ RING_FUNC(ring_get_uipi)
 	RING_API_RETNUMBER(uiPi);
 }
 
+
+RING_FUNC(ring_uiInit)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETSTRING(uiInit((uiInitOptions *) RING_API_GETCPOINTER(1,"uiInitOptions")));
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
+	ring_vm_funcregister("uiinit",ring_uiInit);
 	ring_vm_funcregister("get_uipi",ring_get_uipi);
 }
