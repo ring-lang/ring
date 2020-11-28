34c1b968a58c1187699cf10a6cc46d6b6395d74a
Mahmoud Fayed <msfclipper@yahoo.com>
2020-11-28 04:02:44 +0300

    Update RingLibUI - Add more functions : uiUninit, uiQuit, etc.

--- extensions/ringlibui/ring_libui.c
+++ extensions/ringlibui/ring_libui.c
@@ -25,8 +25,82 @@ RING_FUNC(ring_uiInit)
 	RING_API_RETSTRING(uiInit((uiInitOptions *) RING_API_GETCPOINTER(1,"uiInitOptions")));
 }
 
+
+RING_FUNC(ring_uiUninit)
+{
+	if ( RING_API_PARACOUNT != 0 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	uiUninit();
+}
+
+
+RING_FUNC(ring_uiFreeInitError)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISSTRING(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	uiFreeInitError(RING_API_GETSTRING(1));
+}
+
+
+RING_FUNC(ring_uiMain)
+{
+	if ( RING_API_PARACOUNT != 0 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	uiMain();
+}
+
+
+RING_FUNC(ring_uiMainSteps)
+{
+	if ( RING_API_PARACOUNT != 0 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	uiMainSteps();
+}
+
+
+RING_FUNC(ring_uiMainStep)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETNUMBER(uiMainStep( (int ) RING_API_GETNUMBER(1)));
+}
+
+
+RING_FUNC(ring_uiQuit)
+{
+	if ( RING_API_PARACOUNT != 0 ) {
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return ;
+	}
+	uiQuit();
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("uiinit",ring_uiInit);
+	ring_vm_funcregister("uiuninit",ring_uiUninit);
+	ring_vm_funcregister("uifreeiniterror",ring_uiFreeInitError);
+	ring_vm_funcregister("uimain",ring_uiMain);
+	ring_vm_funcregister("uimainsteps",ring_uiMainSteps);
+	ring_vm_funcregister("uimainstep",ring_uiMainStep);
+	ring_vm_funcregister("uiquit",ring_uiQuit);
 	ring_vm_funcregister("get_uipi",ring_get_uipi);
 }
