6324d10918704e8d204e983b6735c5eb55ee797d
Mahmoud Fayed <msfclipper@yahoo.com>
2020-11-28 03:16:24 +0300

    Update RingLibUI - Add the generated source code : extensions/ringlibui/ring_libui.c

---
+++ extensions/ringlibui/ring_libui.c
@@ -0,0 +1,10 @@
+#include "ring.h"
+
+/* Copyright (c) 2020 Mahmoud Fayed <msfclipper@yahoo.com> */
+
+#include <ui.h>
+#include <ui_windows.h>
+
+RING_API void ringlib_init(RingState *pRingState)
+{
+}
