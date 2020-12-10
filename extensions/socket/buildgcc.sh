1e9a9ccf4c3b5a90e7563cc314d2fe5633fd611f
abdallah elsharif <66518921+abdallah-elsharif@users.noreply.github.com>
2020-12-10 18:09:26 -0500

    Update buildgcc.sh

--- extensions/socket/buildgcc.sh
+++ extensions/socket/buildgcc.sh
@@ -1,4 +1,4 @@
-gcc -c -fpic ext/socket.c -o socket.o -I ../../../../ring/language/include/
-gcc -shared -o socket.so socket.o -L ../../../../ring/lib/ -lring
+gcc -c -fpic ext/socket.c -o socket.o -I ../../../ring/language/include/
+gcc -shared -o socket.so socket.o -L ../../../ring/lib/ -lring
 sudo cp socket.so /usr/lib/
 sudo cp socket.so /usr/lib64/
