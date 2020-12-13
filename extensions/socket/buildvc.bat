46e52cddbb46ad57f4003e28dbea0b13b69e097a
Abdallah Elsharif <elsharifabdallah53@gmail.com>
2020-12-13 06:06:15 -0500

    Update buildvc

--- extensions/socket/buildvc.bat
+++ extensions/socket/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call ../../../ring/language/src/locatevc.bat
-cl /c /DEBUG ext\socket.c -I"..\..\..\ring\language\include"
-link /DEBUG socket.obj  ..\ring\lib\ring.lib /DLL /OUT:socket.dll /SUBSYSTEM:CONSOLE,"5.01"
+call ../../../src/locatevc.bat
+cl /c /DEBUG ext\socket.c -I"..\..\..\include"
+link /DEBUG socket.obj  ..\..\..\lib\ring.lib /DLL /OUT:socket.dll /SUBSYSTEM:CONSOLE,"5.01"
 del socket.obj
