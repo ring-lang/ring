b3d3293a978221e4680a6376cf539e83749edc5f
Mahmoud Fayed <msfclipper@yahoo.com>
2020-12-13 08:13:26 +0300

    Update Socket Library - extensions/socket/buildvc.bat

--- extensions/socket/buildvc.bat
+++ extensions/socket/buildvc.bat
@@ -1,5 +1,5 @@
 cls
-call ../../../src/locatevc.bat
-cl /c /DEBUG ext\socket.c -I"..\..\..\include"
-link /DEBUG socket.obj  ..\..\..\lib\ring.lib /DLL /OUT:socket.dll /SUBSYSTEM:CONSOLE,"5.01"
+call ../../language/src/locatevc.bat
+cl /c /DEBUG ext\socket.c -I"..\..\language\include"
+link /DEBUG socket.obj  ..\..\lib\ring.lib /DLL /OUT:socket.dll /SUBSYSTEM:CONSOLE,"5.01"
 del socket.obj
