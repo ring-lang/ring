62625b3814b30a9b1123954600cb02f510b3910c
Mahmoud Fayed <msfclipper@yahoo.com>
2020-12-18 15:39:05 +0300

    Update Ring VM - language/src/ring_vmexpr.c - Better Code!

--- language/src/ring_vmexpr.c
+++ language/src/ring_vmexpr.c
@@ -975,17 +975,13 @@ char * ring_vm_numtostring ( VM *pVM,double nNum1,char *cStr )
 		sprintf( cOptions , "%s%df" , "%.",pVM->nDecimals ) ;
 		/* Avoid buffer overrun by using snprint() function */
 		nNum2 = snprintf(cStr , 100, cOptions , nNum1);
+		if ( nNum2 >= 100 ) {
+			/* Result truncated so print in compact format with a precision of 90 */
+			nNum2 = snprintf(cStr , 100, "%.90e" , nNum1);
+		}
 		if ( nNum2 < 0 ) {
 			/* Error */
 			cStr[0] = 0 ;
-		}
-		else if ( nNum2 >= 100 ) {
-			/* Result truncated so print in compact format with a precision of 90 */
-			nNum2 = snprintf(cStr , 100, "%.90e" , nNum1);
-			if ( nNum2 < 0 ) {
-				/* Error */
-				cStr[0] = 0 ;
-			}
 		}
 	}
 	return cStr ;
