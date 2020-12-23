ring-lang/ring

commit 4b8f07fc9c1f7e2537101dd4f68593a01e964ebf

Update Visual Source (using PWCT)

extensions/ringopenssl/ring_vmopenssl.c
@@ -1,5 +1,5 @@
 /*
-**  Copyright (c) 2013-2019 Mahmoud Fayed <msfclipper@yahoo.com> 
+**  Copyright (c) 2013-2020 Mahmoud Fayed <msfclipper@yahoo.com> 
 **  Include Files 
 */
 #include "ring.h"
@@ -181,16 +181,15 @@ void ring_vm_openssl_randbytes ( void *pPointer )
 		nNum1 = (int) RING_API_GETNUMBER(1) ;
 		if ( nNum1 > 0 ) {
 			cStr =  malloc(nNum1) ;
-			if (cStr) {
-				if ( RAND_bytes(cStr,nNum1) ) {
-					RING_API_RETSTRING2((const char *) cStr,nNum1);
-				}
-				else {
-					RING_API_ERROR(RING_API_INTERNALFAILURE);
-				}
-				free (cStr) ;
-			} else {
+			if ( cStr == NULL ) {
 				RING_API_ERROR(RING_OOM);
+				return ;
+			}
+			if ( RAND_bytes(cStr,nNum1) ) {
+				RING_API_RETSTRING2((const char *) cStr,nNum1);
+			}
+			else {
+				RING_API_ERROR(RING_API_INTERNALFAILURE);
 			}
 		} else {
 			RING_API_ERROR(RING_API_BADPARATYPE);

extensions/ringopenssl/ring_vmopenssl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2019 Mahmoud Fayed <msfclipper@yahoo.com> */
+/* Copyright (c) 2013-2020 Mahmoud Fayed <msfclipper@yahoo.com> */
 #ifndef ring_vmopenssl_h
 #define ring_vmopenssl_h
 /* Functions */

language/visualsrc/ring_vmapi.c.isense
@@ -50,6 +50,7 @@ RING_API_BADPARAVALUE
 RING_API_NOTPOINTER
 RING_API_NULLPOINTER
 RING_API_EMPTYLIST
+RING_API_EMPTYLIST
 ring_vm_funcregister(x,y)
 ring_vm_funcregister2
 Type: List *

commit 6bbf90b312838d68aa5af95ccc830aee7efeac95

Fix memory leak re-introduced by commit 4b8f07fc9c1f7e2537101dd4f68593a01e964ebf "Update Visual Source (using PWCT)"
during code refactoring, the "free" statement was lost

extensions/ringopenssl/ring_vmopenssl.c
@@ -191,6 +191,7 @@ void ring_vm_openssl_randbytes ( void *pPointer )
 			else {
 				RING_API_ERROR(RING_API_INTERNALFAILURE);
 			}
+			free (cStr) ;
 		} else {
 			RING_API_ERROR(RING_API_BADPARATYPE);
 		}