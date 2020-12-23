2655400cabe0437ca197bd7ffdefa6d730d1dc7c
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2020-12-24 00:34:02 +0100

    Implement multi-part hash functions in OpenSSL extension (XXXinit, XXXupdate, XXXfinal)
    These news functions enable to compute hash of large files and large data without the need of loading all content in a single string variable

--- extensions/ringopenssl/ring_vmopenssl.h
+++ extensions/ringopenssl/ring_vmopenssl.h
@@ -5,15 +5,51 @@
 
 RING_API void ringlib_init ( RingState *pRingState ) ;
 
+void ring_vm_openssl_md5_init ( void *pPointer ) ;
+
+void ring_vm_openssl_md5_update ( void *pPointer ) ;
+
+void ring_vm_openssl_md5_final ( void *pPointer ) ;
+
 void ring_vm_openssl_md5 ( void *pPointer ) ;
+
+void ring_vm_openssl_sha1_init ( void *pPointer ) ;
+
+void ring_vm_openssl_sha1_update ( void *pPointer ) ;
+
+void ring_vm_openssl_sha1_final ( void *pPointer ) ;
 
 void ring_vm_openssl_sha1 ( void *pPointer ) ;
 
+void ring_vm_openssl_sha256_init ( void *pPointer ) ;
+
+void ring_vm_openssl_sha256_update ( void *pPointer ) ;
+
+void ring_vm_openssl_sha256_final ( void *pPointer ) ;
+
 void ring_vm_openssl_sha256 ( void *pPointer ) ;
+
+void ring_vm_openssl_sha512_init ( void *pPointer ) ;
+
+void ring_vm_openssl_sha512_update ( void *pPointer ) ;
+
+void ring_vm_openssl_sha512_final ( void *pPointer ) ;
 
 void ring_vm_openssl_sha512 ( void *pPointer ) ;
 
+void ring_vm_openssl_sha384_init ( void *pPointer ) ;
+
+void ring_vm_openssl_sha384_update ( void *pPointer ) ;
+
+void ring_vm_openssl_sha384_final ( void *pPointer ) ;
+
 void ring_vm_openssl_sha384 ( void *pPointer ) ;
+
+void ring_vm_openssl_sha224_init ( void *pPointer ) ;
+
+void ring_vm_openssl_sha224_update ( void *pPointer ) ;
+
+void ring_vm_openssl_sha224_final ( void *pPointer ) ;
 
 void ring_vm_openssl_sha224 ( void *pPointer ) ;
 
