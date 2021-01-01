9ae5db8203f1c142be3f9f001ddd2f2aef9cb0e1
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-01 22:05:36 +0300

    Update Ring For WebAssembly

--- extensions/webassembly/ringqt/project/ring/include/ring_vmlistfuncs.h
+++ extensions/webassembly/ringqt/project/ring/include/ring_vmlistfuncs.h
@@ -1,24 +1,24 @@
-/* Copyright (c) 2013-2020 Mahmoud Fayed <msfclipper@yahoo.com> */
+/* Copyright (c) 2013-2021 Mahmoud Fayed <msfclipper@yahoo.com> */
 #ifndef ring_vmlistfuncs_h
-#define ring_vmlistfuncs_h
-/* Load Functions */
+	#define ring_vmlistfuncs_h
+	/* Load Functions */
 
-void ring_vm_listfuncs_loadfunctions ( RingState *pRingState ) ;
-/* List Functions */
+	void ring_vm_listfuncs_loadfunctions ( RingState *pRingState ) ;
+	/* List Functions */
 
-void ring_vmlib_list ( void *pPointer ) ;
+	void ring_vmlib_list ( void *pPointer ) ;
 
-void ring_vmlib_find ( void *pPointer ) ;
+	void ring_vmlib_find ( void *pPointer ) ;
 
-void ring_vmlib_max ( void *pPointer ) ;
+	void ring_vmlib_max ( void *pPointer ) ;
 
-void ring_vmlib_min ( void *pPointer ) ;
+	void ring_vmlib_min ( void *pPointer ) ;
 
-void ring_vmlib_insert ( void *pPointer ) ;
+	void ring_vmlib_insert ( void *pPointer ) ;
 
-void ring_vmlib_sort ( void *pPointer ) ;
+	void ring_vmlib_sort ( void *pPointer ) ;
 
-void ring_vmlib_binarysearch ( void *pPointer ) ;
+	void ring_vmlib_binarysearch ( void *pPointer ) ;
 
-void ring_vmlib_reverse ( void *pPointer ) ;
+	void ring_vmlib_reverse ( void *pPointer ) ;
 #endif
