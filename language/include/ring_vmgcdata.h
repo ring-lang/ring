6a1fb3990b19b85aaa52bdad3341c10298ef5999
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-01 22:01:30 +0300

    Update Ring - language/visualsrc/ring_vmgc.ssf - Better format when generating the C source code

--- language/include/ring_vmgcdata.h
+++ language/include/ring_vmgcdata.h
@@ -1,8 +1,8 @@
-/* Copyright (c) 2013-2020 Mahmoud Fayed <msfclipper@yahoo.com> */
+/* Copyright (c) 2013-2021 Mahmoud Fayed <msfclipper@yahoo.com> */
 #ifndef ring_vmgcdata
-#define ring_vmgcdata
-typedef struct GCData {
-	int nReferenceCount  ;
-	void (*pFreeFunc)(void *,void *) ;
-} GCData ;
+	#define ring_vmgcdata
+	typedef struct GCData {
+		int nReferenceCount  ;
+		void (*pFreeFunc)(void *,void *) ;
+	} GCData ;
 #endif
