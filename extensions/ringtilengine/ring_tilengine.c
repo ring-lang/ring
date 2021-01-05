ae09e383fd377182b9fb2c88543f1cc5f3cd2869
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-05 03:25:41 +0300

    Update RingTilengine - Add: TLN_Blend

--- extensions/ringtilengine/ring_tilengine.c
+++ extensions/ringtilengine/ring_tilengine.c
@@ -53,6 +53,56 @@ RING_FUNC(ring_get_flag_masked)
 	RING_API_RETNUMBER(FLAG_MASKED);
 }
 
+RING_FUNC(ring_get_blend_none)
+{
+	RING_API_RETNUMBER(BLEND_NONE);
+}
+
+RING_FUNC(ring_get_blend_mix25)
+{
+	RING_API_RETNUMBER(BLEND_MIX25);
+}
+
+RING_FUNC(ring_get_blend_mix50)
+{
+	RING_API_RETNUMBER(BLEND_MIX50);
+}
+
+RING_FUNC(ring_get_blend_mix75)
+{
+	RING_API_RETNUMBER(BLEND_MIX75);
+}
+
+RING_FUNC(ring_get_blend_add)
+{
+	RING_API_RETNUMBER(BLEND_ADD);
+}
+
+RING_FUNC(ring_get_blend_sub)
+{
+	RING_API_RETNUMBER(BLEND_SUB);
+}
+
+RING_FUNC(ring_get_blend_mod)
+{
+	RING_API_RETNUMBER(BLEND_MOD);
+}
+
+RING_FUNC(ring_get_blend_custom)
+{
+	RING_API_RETNUMBER(BLEND_CUSTOM);
+}
+
+RING_FUNC(ring_get_max_blend)
+{
+	RING_API_RETNUMBER(MAX_BLEND);
+}
+
+RING_FUNC(ring_get_blend_mix)
+{
+	RING_API_RETNUMBER(BLEND_MIX);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_tilengine_ver_maj",ring_get_tilengine_ver_maj);
@@ -65,4 +115,14 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_flag_rotate",ring_get_flag_rotate);
 	ring_vm_funcregister("get_flag_priority",ring_get_flag_priority);
 	ring_vm_funcregister("get_flag_masked",ring_get_flag_masked);
+	ring_vm_funcregister("get_blend_none",ring_get_blend_none);
+	ring_vm_funcregister("get_blend_mix25",ring_get_blend_mix25);
+	ring_vm_funcregister("get_blend_mix50",ring_get_blend_mix50);
+	ring_vm_funcregister("get_blend_mix75",ring_get_blend_mix75);
+	ring_vm_funcregister("get_blend_add",ring_get_blend_add);
+	ring_vm_funcregister("get_blend_sub",ring_get_blend_sub);
+	ring_vm_funcregister("get_blend_mod",ring_get_blend_mod);
+	ring_vm_funcregister("get_blend_custom",ring_get_blend_custom);
+	ring_vm_funcregister("get_max_blend",ring_get_max_blend);
+	ring_vm_funcregister("get_blend_mix",ring_get_blend_mix);
 }
