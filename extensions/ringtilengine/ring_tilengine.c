0cea1f2e8b7d24fdc0a6f717bf7d52c9af2a2067
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-05 03:20:11 +0300

    Update RingTilengine - Add more constants

--- extensions/ringtilengine/ring_tilengine.c
+++ extensions/ringtilengine/ring_tilengine.c
@@ -3,6 +3,30 @@
 /* Copyright (c) 2021 Mahmoud Fayed <msfclipper@yahoo.com> */
 
 #include <tilengine.h>
+RING_FUNC(ring_get_tilengine_ver_maj)
+{
+	RING_API_RETNUMBER(TILENGINE_VER_MAJ);
+}
+
+RING_FUNC(ring_get_tilengine_ver_min)
+{
+	RING_API_RETNUMBER(TILENGINE_VER_MIN);
+}
+
+RING_FUNC(ring_get_tilengine_ver_rev)
+{
+	RING_API_RETNUMBER(TILENGINE_VER_REV);
+}
+
+RING_FUNC(ring_get_tilengine_header_version)
+{
+	RING_API_RETNUMBER(TILENGINE_HEADER_VERSION);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
+	ring_vm_funcregister("get_tilengine_ver_maj",ring_get_tilengine_ver_maj);
+	ring_vm_funcregister("get_tilengine_ver_min",ring_get_tilengine_ver_min);
+	ring_vm_funcregister("get_tilengine_ver_rev",ring_get_tilengine_ver_rev);
+	ring_vm_funcregister("get_tilengine_header_version",ring_get_tilengine_header_version);
 }
