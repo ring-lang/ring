39691d96fd6f471af4e8a0312bbf5a01dd9e83a1
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-05 03:26:50 +0300

    Update RingTilengine - Add Structure: TLN_Affine

--- extensions/ringtilengine/ring_tilengine.c
+++ extensions/ringtilengine/ring_tilengine.c
@@ -103,6 +103,218 @@ RING_FUNC(ring_get_blend_mix)
 	RING_API_RETNUMBER(BLEND_MIX);
 }
 
+RING_FUNC(ring_new_tln_affine)
+{
+	TLN_Affine *pMyPointer ;
+	pMyPointer = (TLN_Affine *) ring_state_malloc(((VM *) pPointer)->pRingState,sizeof(TLN_Affine)) ;
+	if (pMyPointer == NULL) 
+	{
+		RING_API_ERROR(RING_OOM);
+		return ;
+	}
+	RING_API_RETCPOINTER(pMyPointer,"TLN_Affine");
+}
+
+RING_FUNC(ring_new_managed_tln_affine)
+{
+	TLN_Affine *pMyPointer ;
+	pMyPointer = (TLN_Affine *) ring_state_malloc(((VM *) pPointer)->pRingState,sizeof(TLN_Affine)) ;
+	if (pMyPointer == NULL) 
+	{
+		RING_API_ERROR(RING_OOM);
+		return ;
+	}
+	RING_API_RETMANAGEDCPOINTER(pMyPointer,"TLN_Affine",ring_state_free);
+}
+
+RING_FUNC(ring_destroy_tln_affine)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	if (pMyPointer != NULL) {
+		ring_state_free(((VM *) pPointer)->pRingState,pMyPointer) ;
+		RING_API_SETNULLPOINTER(1);
+	}
+}
+
+RING_FUNC(ring_get_tln_affine_angle)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	RING_API_RETNUMBER(pMyPointer->angle);
+}
+
+RING_FUNC(ring_set_tln_affine_angle)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	pMyPointer->angle = RING_API_GETNUMBER(2);
+}
+
+RING_FUNC(ring_get_tln_affine_dx)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	RING_API_RETNUMBER(pMyPointer->dx);
+}
+
+RING_FUNC(ring_set_tln_affine_dx)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	pMyPointer->dx = RING_API_GETNUMBER(2);
+}
+
+RING_FUNC(ring_get_tln_affine_dy)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	RING_API_RETNUMBER(pMyPointer->dy);
+}
+
+RING_FUNC(ring_set_tln_affine_dy)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	pMyPointer->dy = RING_API_GETNUMBER(2);
+}
+
+RING_FUNC(ring_get_tln_affine_sx)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	RING_API_RETNUMBER(pMyPointer->sx);
+}
+
+RING_FUNC(ring_set_tln_affine_sx)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	pMyPointer->sx = RING_API_GETNUMBER(2);
+}
+
+RING_FUNC(ring_get_tln_affine_sy)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	RING_API_RETNUMBER(pMyPointer->sy);
+}
+
+RING_FUNC(ring_set_tln_affine_sy)
+{
+	TLN_Affine *pMyPointer ;
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA) ;
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) { 
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pMyPointer = RING_API_GETCPOINTER(1,"TLN_Affine");
+	pMyPointer->sy = RING_API_GETNUMBER(2);
+}
+
 RING_API void ringlib_init(RingState *pRingState)
 {
 	ring_vm_funcregister("get_tilengine_ver_maj",ring_get_tilengine_ver_maj);
@@ -125,4 +337,17 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("get_blend_custom",ring_get_blend_custom);
 	ring_vm_funcregister("get_max_blend",ring_get_max_blend);
 	ring_vm_funcregister("get_blend_mix",ring_get_blend_mix);
+	ring_vm_funcregister("new_tln_affine",ring_new_tln_affine);
+	ring_vm_funcregister("new_managed_tln_affine",ring_new_managed_tln_affine);
+	ring_vm_funcregister("destroy_tln_affine",ring_destroy_tln_affine);
+	ring_vm_funcregister("get_tln_affine_angle",ring_get_tln_affine_angle);
+	ring_vm_funcregister("set_tln_affine_angle",ring_set_tln_affine_angle);
+	ring_vm_funcregister("get_tln_affine_dx",ring_get_tln_affine_dx);
+	ring_vm_funcregister("set_tln_affine_dx",ring_set_tln_affine_dx);
+	ring_vm_funcregister("get_tln_affine_dy",ring_get_tln_affine_dy);
+	ring_vm_funcregister("set_tln_affine_dy",ring_set_tln_affine_dy);
+	ring_vm_funcregister("get_tln_affine_sx",ring_get_tln_affine_sx);
+	ring_vm_funcregister("set_tln_affine_sx",ring_set_tln_affine_sx);
+	ring_vm_funcregister("get_tln_affine_sy",ring_get_tln_affine_sy);
+	ring_vm_funcregister("set_tln_affine_sy",ring_set_tln_affine_sy);
 }
