b868b009e0f0caa3797ecb7dd79414da755e65aa
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-07 07:46:21 +0300

    Update Ring For WebAssembly

--- extensions/webassembly/ringqt/project/ring/src/ring.c
+++ extensions/webassembly/ringqt/project/ring/src/ring.c
@@ -1,8 +1,7 @@
 /* Copyright (c) 2013-2021 Mahmoud Fayed <msfclipper@yahoo.com> */
 #include "ring.h"
 
-int main( int argc, char *argv[])
+int main ( int argc, char *argv[] )
 {
 	ring_state_main(argc,argv);
-	return 0;
 }
