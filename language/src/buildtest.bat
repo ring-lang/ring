38aebab606cb2eeab6ca5ec6717595e86293028d
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2021-01-14 00:37:26 +0100

    Better Visual Studio build scripts that allow selecting target platform and debug mode, and support VS 2019
    First enhancement applies to locatevc.bat which now accepts parameters in the format : locatevc.bat [x86|x64|arm|arm64|auto] [debug]
    
    If locatevc.bat is invoked without any parameter, it will default to standard x86 32bit build as before.
    The "auto" parameter instruct locatevc.bat to detect the architecture of the existing ring.exe and use it as a target.
    
    The Ring build scripts (buildvc.bat, buildvcw.bat, buildvcstatic.bat, buildtest.bat) take the same parameters as locatevc.bat.
    They were enhanced to use compilation flags automatically set by locatevc.bat depending on the arguments passed.
    
    The file buildarch.ring is used by locatevc.bat to detect the architecture of the existing ring.exe binary.
    
    All scripts now use setlocal/endlocal in order to avoid polluting the environment of the console used for the build

--- language/src/buildtest.bat
+++ language/src/buildtest.bat
@@ -1,14 +1,16 @@
 echo off
-call locatevc.bat
+setlocal
+call locatevc.bat %1 %2
 
 cls
-cl /DEBUG:FULL /Z7 ring_test.c  ^
+cl %ringcflags% ring_test.c  ^
 ring_string.c ring_list.c ring_item.c ring_items.c ring_hashtable.c ring_general.c ring_state.c ring_scanner.c ring_parser.c ring_hashlib.c ring_vmgc.c ^
 ring_stmt.c ring_expr.c ring_codegen.c ring_vm.c ring_vmexpr.c ring_vmvars.c ^
 ring_vmlists.c ring_vmfuncs.c ring_api.c ring_vmoop.c ring_vmcui.c ^
 ring_vmtrycatch.c ring_vmstrindex.c ring_vmjump.c ring_vmduprange.c ^
 ring_vmperformance.c ring_vmexit.c ring_vmstackvars.c ring_vmstate.c ring_generallib_extension.c ring_math_extension.c ring_file_extension.c ring_os_extension.c ring_list_extension.c ring_refmeta_extension.c ^
-ring_ext.c ring_dll_extension.c ring_objfile.c -I"..\include" /link /SUBSYSTEM:CONSOLE,"5.01" /OUT:test.exe 
+ring_ext.c ring_dll_extension.c ring_objfile.c -I"..\include" /link /SUBSYSTEM:CONSOLE,"%ringsubsystem%" /OUT:test.exe 
 
 del *.obj
 
+endlocal
