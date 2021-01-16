e5d34b4481dbf107a581111a8e0a5cfd4cc5c0f2
Ilir-Liburn <30291682+Ilir-Liburn@users.noreply.github.com>
2021-01-16 17:03:51 +0100

    making pull request

--- language/src/ring_os_extension.c
+++ language/src/ring_os_extension.c
@@ -278,9 +278,10 @@ void ring_vm_os_nofprocessors ( void *pPointer )
 	#endif
 }
 /* Mac OS doesn't provide clock_gettime function prior v. 10.12 */
-#if defined __MACH__ && __MAC_OS_X_VERSION_MIN_REQUIRED < 101200
+/* Custom function is going to work on all Mac OS versions */
+#if defined __MACH__
 
-	int clock_gettime ( int clk_id, struct timespec* ts )
+	int ring_clock_gettime ( int clk_id, struct timespec* ts )
 	{
 		uint64_t nsec = mach_absolute_time() ;
 		ts->tv_sec = nsec / NANOSEC ;
@@ -297,7 +298,7 @@ double ring_vm_os_calcuptime ( void )
 		return ElapsedMicroseconds.QuadPart ;
 	#else
 		struct timespec ts  ;
-		clock_gettime(CLOCK_UPTIME, &ts);
+		ring_clock_gettime(CLOCK_UPTIME, &ts);
 		/* Compensate to match 0.1 ms resolution on Windows */
 		return ( ( ts.tv_sec * NANOSEC ) + ( ts.tv_nsec ) ) / 100 ;
 	#endif
