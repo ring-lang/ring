d82d3df9780950c6034eaa3357ac8e17a4ad350f
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-20 13:52:23 +0300

    Update RingSockets - Scripts

--- extensions/ringsockets/buildclang.sh
+++ extensions/ringsockets/buildclang.sh
@@ -1,4 +1,4 @@
 clang -c -fpic -O2 ext/sockets.c -I $PWD/../../../include
-clang -dynamiclib -o ring_sockets.dylib sockets.o  -L $PWD/../../../lib  -lring
-cp ring_sockets.dylib /usr/local/lib
+clang -dynamiclib -o libring_sockets.dylib sockets.o  -L $PWD/../../../lib  -lring
+cp libring_sockets.dylib /usr/local/lib
 
--- extensions/ringsockets/buildgcc.sh
+++ extensions/ringsockets/buildgcc.sh
@@ -1,4 +1,4 @@
 gcc -c -fpic -O2 ext/sockets.c -o sockets.o -I ../../../ring/language/include/
-gcc -shared -o ring_sockets.so sockets.o -L ../../../ring/lib/ -lring
-sudo cp ring_sockets.so /usr/lib/
-sudo cp ring_sockets.so /usr/lib64/
+gcc -shared -o libring_sockets.so sockets.o -L ../../../ring/lib/ -lring
+sudo cp libring_sockets.so /usr/lib/
+sudo cp libring_sockets.so /usr/lib64/
