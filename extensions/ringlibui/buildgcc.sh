abace80b76aea8ee3ae11715eed472a7dfbae747
Mahmoud Fayed <mahmoudfayed@Mahmouds-iMac.local>
2021-01-25 12:40:39 +0300

    Update Scripts

--- extensions/ringlibui/buildgcc.sh
+++ extensions/ringlibui/buildgcc.sh
@@ -1,5 +1,5 @@
 gcc -c -fpic -O2 ring_libui.c -I $PWD/../../language/include -I linux
-gcc -shared -o $PWD/../../lib/libring_libui.so ring_libui.o -L $PWD/../../lib -lring -L $PWD/linux -llibui
+gcc -shared -o $PWD/../../lib/libring_libui.so ring_libui.o -L $PWD/../../lib -lring -L $PWD/linux -lui
 
  
 
--- extensions/ringlibui/buildgccmac.sh
+++ extensions/ringlibui/buildgccmac.sh
@@ -1,5 +1,5 @@
 gcc -c -fpic -O2 ring_libui.c -I $PWD/../../language/include -I macOS
-gcc -shared -o $PWD/../../lib/libring_libui.dylib ring_libui.o -L $PWD/../../lib -lring -L $PWD/macOS -llibui
+gcc -shared -o $PWD/../../lib/libring_libui.dylib ring_libui.o -L $PWD/../../lib -lring -L $PWD/macOS -lui
 
  
 
