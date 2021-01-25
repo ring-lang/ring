6d3f8e25deb53280a7f67c2c4f4d60e73b619e70
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-25 16:38:05 +0300

    Add: bin/uninstall.sh

---
+++ bin/uninstall.sh
@@ -0,0 +1,7 @@
+sudo rm /usr/lib/libring*.*
+sudo rm /usr/lib64/libring*.*
+sudo rm /usr/bin/ring
+sudo rm /usr/bin/ring2exe
+sudo rm /usr/bin/ringpm
+sudo rm /usr/bin/folder2qrc
+sudo rm /usr/bin/ringrepl
