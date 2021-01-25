fc0281f3906e850466043a197d32dbdb53bfa9c4
Mahmoud Fayed <mahmoudfayed@Mahmouds-iMac.local>
2021-01-25 11:35:30 +0300

    Update buildclang.sh

--- extensions/ringsockets/buildclang.sh
+++ extensions/ringsockets/buildclang.sh
@@ -1,4 +1,4 @@
-clang -c -fpic -O2 ext/sockets.c -I $PWD/../../../include
-clang -dynamiclib -o libring_sockets.dylib sockets.o  -L $PWD/../../../lib  -lring
+clang -c -fpic -O2 ext/sockets.c -I $PWD/../../language/include
+clang -dynamiclib -o libring_sockets.dylib sockets.o  -L $PWD/../../language/lib  -lring
 cp libring_sockets.dylib /usr/local/lib
 
