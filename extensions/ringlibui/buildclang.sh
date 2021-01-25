55364e2d5ed7a69dd8b8da092f53107b431fdf5f
Mahmoud Fayed <msfclipper@yahoo.com>
2021-01-25 12:55:24 +0300

    Add extensions/ringlibui/buildclang.sh

---
+++ extensions/ringlibui/buildclang.sh
@@ -0,0 +1,6 @@
+clang -c -fpic -O2 ring_libui.c -I $PWD/../../language/include -I /usr/local/include
+clang -dynamiclib -o $PWD/../../lib/libring_libui.dylib ring_libui.o macOS/libui.dylib -L $PWD/../../lib  -lring 
+
+ 
+
+
