6fad06777d00a48bb972ee784d5e445006747b18
Mahmoud Fayed <msfclipper@yahoo.com>
2021-06-14 11:58:10 +0300

    Update RingQt - Add extensions/ringqt/cpp/src/mscompleter.cpp and extensions/ringqt/cpp/include/mscompleter.h

---
+++ extensions/ringqt/cpp/include/mscompleter.h
@@ -0,0 +1,25 @@
+// Based on code from : https://www.mimec.org/blog/qcompleter-with-multi-selection
+
+#ifndef MSCOMPLETER_H
+#define MSCOMPLETER_H
+
+#include <QCompleter>
+
+extern "C" {
+#include "ring.h"
+}
+
+class MultiSelectCompleter : public QCompleter
+{
+    Q_OBJECT
+public:
+    MultiSelectCompleter( const QStringList& items, QObject* parent );
+    ~MultiSelectCompleter();
+
+public:
+    QString pathFromIndex( const QModelIndex& index ) const;
+    QStringList splitPath( const QString& path ) const;
+};
+
+
+#endif 
