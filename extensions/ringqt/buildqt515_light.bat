8bd8fadf8ca78127dd6dd09baafc0d6ddb64876a
Mahmoud Fayed <msfclipper@yahoo.com>
2021-09-28 17:51:29 +0300

    Update RingQt - Add: extensions/ringqt/buildqt515_light.bat

---
+++ extensions/ringqt/buildqt515_light.bat
@@ -0,0 +1,4 @@
+call ..\..\language\src\locatevc.bat
+"C:\Qt\5.15.0\msvc2019\bin\qmake.exe" ring_qt512_light.pro -spec win32-msvc "CONFIG+=release"
+"C:\Qt\Tools\QtCreator\bin\jom.exe"
+copy release\ringqt.dll ..\..\bin
