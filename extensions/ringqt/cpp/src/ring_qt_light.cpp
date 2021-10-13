34e78b8b6846c5fd411d884ce3a2fb0497ca73db
Mahmoud Fayed <msfclipper@yahoo.com>
2021-10-13 11:57:20 +0300

    Update RingQt - QListWidget Class - AddItem() Method - Convert Numbers to Strings

--- extensions/ringqt/cpp/src/ring_qt_light.cpp
+++ extensions/ringqt/cpp/src/ring_qt_light.cpp
@@ -77649,10 +77649,10 @@ RING_FUNC(ring_QTextEdit_setactivelinecolor)
 	extras << highlight;
 	editor->setExtraSelections( extras );
 }
-
 RING_FUNC(ring_QListWidget_addItem)
 {
 	GListWidget *pObject ;
+	char cStr[100]  ;
 	if ( RING_API_PARACOUNT != 2 ) {
 		RING_API_ERROR(RING_API_MISS2PARA);
 		return ;
@@ -77663,13 +77663,18 @@ RING_FUNC(ring_QListWidget_addItem)
 		return ;
 	}
 	pObject = (GListWidget *) RING_API_GETCPOINTER(1,"QListWidget");
-	if ( ! RING_API_ISSTRING(2) ) {
+	if ( ( ! RING_API_ISSTRING(2) ) && ( ! RING_API_ISNUMBER(2) ) ) {
 		RING_API_ERROR(RING_API_BADPARATYPE);
 		return ;
 	}
-	pObject->addItem(RING_API_GETSTRING(2));
+	if ( RING_API_ISSTRING(2) )
+		pObject->addItem(RING_API_GETSTRING(2));
+	else {
+		ring_vm_numtostring((VM *) pPointer, RING_API_GETNUMBER(2),cStr);
+		pObject->addItem(cStr);
+	}
+		
 }
-
 
 RING_FUNC(ring_QListWidget_count)
 {
