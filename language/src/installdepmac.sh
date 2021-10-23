eb8dc71c4bed100b77d4e7fa652ae3a9e9f5f288
Mahmoud Fayed <msfclipper@yahoo.com>
2021-10-23 09:59:00 +0300

    Update language/src/installdepmac.sh

--- language/src/installdepmac.sh
+++ language/src/installdepmac.sh
@@ -1,2 +1,2 @@
-brew install -k git gcc unixodbc mysql-connector-c allegro openssl qt glew libuv libpq sdl2 sdl2_image sdl2_mixer sdl2_net sdl2_ttf httpd
+brew install -k git gcc unixodbc mysql-connector-c allegro openssl qt@5 glew libuv libpq sdl2 sdl2_image sdl2_mixer sdl2_net sdl2_ttf httpd
 brew link --force qt
