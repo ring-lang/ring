0f96116ef9c9893679e3993c927b1b80ec07105c
Mahmoud Fayed <msfclipper@yahoo.com>
2021-10-25 23:11:08 +0300

    Update extensions/ringopenssl/buildclang.sh

--- extensions/ringopenssl/buildclang.sh
+++ extensions/ringopenssl/buildclang.sh
@@ -1,5 +1,5 @@
 clang -c -fpic -O2 ring_vmopenssl.c -I $PWD/../../language/include -I /usr/local/include -I /usr/local/opt/openssl/include
-clang -dynamiclib -o $PWD/../../lib/libring_openssl.dylib ring_vmopenssl.o  -L $PWD/../../lib  -lring -lssl -lcrypto
+clang -dynamiclib -o $PWD/../../lib/libring_openssl.dylib ring_vmopenssl.o  -L $PWD/../../lib -L /usr/local/opt/openssl/lib -lring -lssl -lcrypto
 
  
 
