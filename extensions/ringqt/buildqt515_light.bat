3efd5bfd68a72a1be7c26e63a6fa7495fa570401
Mahmoud Fayed <msfclipper@yahoo.com>
2022-01-19 04:15:41 +0300

    Update RingQt - Batch Files - Support Qt Creator 6

--- extensions/ringqt/buildqt515_light.bat
+++ extensions/ringqt/buildqt515_light.bat
@@ -1,4 +1,4 @@
 call ..\..\language\src\locatevc.bat
 "C:\Qt\5.15.0\msvc2019\bin\qmake.exe" ring_qt512_light.pro -spec win32-msvc "CONFIG+=release"
-"C:\Qt\Tools\QtCreator\bin\jom.exe"
+"C:\Qt\Tools\QtCreator\bin\jom\jom.exe"
 copy release\ringqt_light.dll ..\..\bin
