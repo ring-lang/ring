1e81288ab13e491588f18f575da1b59979aa4342
Mahmoud Fayed <msfclipper@yahoo.com>
2022-03-06 21:31:34 +0300

    Update language/src/ring_items.c - Code Format

--- language/src/ring_items.c
+++ language/src/ring_items.c
@@ -3,40 +3,40 @@
 
 Items * ring_items_new_gc ( void *pState )
 {
-	Items *pItems  ;
-	pItems = (Items *) ring_state_malloc(pState,sizeof(Items));
-	if ( pItems == NULL ) {
-		printf( RING_OOM ) ;
-		exit(0);
-	}
-	pItems->pValue = ring_item_new_gc(pState,ITEMTYPE_NOTHING) ;
-	pItems->pNext = NULL ;
-	pItems->pPrev = NULL ;
-	return pItems ;
+    Items *pItems  ;
+    pItems = (Items *) ring_state_malloc(pState,sizeof(Items));
+    if ( pItems == NULL ) {
+        printf( RING_OOM ) ;
+        exit(0);
+    }
+    pItems->pValue = ring_item_new_gc(pState,ITEMTYPE_NOTHING) ;
+    pItems->pNext = NULL ;
+    pItems->pPrev = NULL ;
+    return pItems ;
 }
 
 Items * ring_items_delete_gc ( void *pState,Items *pItems )
 {
-	assert(pItems != NULL);
-	pItems->pValue = ring_item_delete_gc(pState,pItems->pValue);
-	ring_state_free(pState,pItems);
-	pItems = NULL ;
-	return pItems ;
+    assert(pItems != NULL);
+    pItems->pValue = ring_item_delete_gc(pState,pItems->pValue);
+    ring_state_free(pState,pItems);
+    pItems = NULL ;
+    return pItems ;
 }
 
 void ring_items_print ( Items *pItems )
 {
-	assert(pItems != NULL);
-	ring_item_print(pItems->pValue);
+    assert(pItems != NULL);
+    ring_item_print(pItems->pValue);
 }
 /* Functions without State pointer */
 
 Items * ring_items_new ( void )
 {
-	return ring_items_new_gc(NULL) ;
+    return ring_items_new_gc(NULL) ;
 }
 
 Items * ring_items_delete ( Items *pItems )
 {
-	return ring_items_delete_gc(NULL,pItems) ;
+    return ring_items_delete_gc(NULL,pItems) ;
 }
