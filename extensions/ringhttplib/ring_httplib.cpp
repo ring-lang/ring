d9be6ec6f284a8865172e0f55a0228fceec66919
Mahmoud Fayed <msfclipper@yahoo.com>
2022-03-08 00:38:34 +0300

    Update RingHTTPLib - Request Class - Add Method: get_header_value()

--- extensions/ringhttplib/ring_httplib.cpp
+++ extensions/ringhttplib/ring_httplib.cpp
@@ -1203,6 +1203,52 @@ RING_FUNC(ring_HTTPLib_Request_has_header)
 }
 
 
+RING_FUNC(ring_HTTPLib_Request_get_header_value)
+{
+	Request *pObject ;
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	RING_API_IGNORECPOINTERTYPE ;
+	if ( ! RING_API_ISCPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pObject = (Request *) RING_API_GETCPOINTER(1,"HTTPLib_Request");
+	if ( ! RING_API_ISSTRING(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETSTRING(pObject->get_header_value(RING_API_GETSTRING(2)).c_str());
+}
+
+
+RING_FUNC(ring_HTTPLib_Request_get_header_value_2)
+{
+	Request *pObject ;
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	RING_API_IGNORECPOINTERTYPE ;
+	if ( ! RING_API_ISCPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pObject = (Request *) RING_API_GETCPOINTER(1,"HTTPLib_Request");
+	if ( ! RING_API_ISSTRING(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETSTRING(pObject->get_header_value(RING_API_GETSTRING(2), (size_t ) RING_API_GETNUMBER(3)).c_str());
+}
+
+
 RING_FUNC(ring_HTTPLib_Request_get_header_value_count)
 {
 	Request *pObject ;
@@ -1571,6 +1617,8 @@ RING_API void ringlib_init(RingState *pRingState)
 	ring_vm_funcregister("httplib_response_get_header_value",ring_HTTPLib_Response_get_header_value);
 	ring_vm_funcregister("httplib_response_get_header_value_2",ring_HTTPLib_Response_get_header_value_2);
 	ring_vm_funcregister("httplib_request_has_header",ring_HTTPLib_Request_has_header);
+	ring_vm_funcregister("httplib_request_get_header_value",ring_HTTPLib_Request_get_header_value);
+	ring_vm_funcregister("httplib_request_get_header_value_2",ring_HTTPLib_Request_get_header_value_2);
 	ring_vm_funcregister("httplib_request_get_header_value_count",ring_HTTPLib_Request_get_header_value_count);
 	ring_vm_funcregister("httplib_request_set_header",ring_HTTPLib_Request_set_header);
 	ring_vm_funcregister("httplib_request_has_param",ring_HTTPLib_Request_has_param);
