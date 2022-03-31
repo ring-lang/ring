362a736fe0e86c7ed3fc258df63c2cbc3f37716b
Mahmoud Fayed <msfclipper@yahoo.com>
2022-03-31 18:55:02 +0300

    Update RingHTTPLib - buildgcc.sh

--- extensions/ringhttplib/buildgcc.sh
+++ extensions/ringhttplib/buildgcc.sh
@@ -1,2 +1,2 @@
-gcc -c -fpic -O2 ring_httplib.cpp -I $PWD/../../language/include
-gcc -shared -o $PWD/../../lib/libring_httplib.so ring_httplib.o -L $PWD/../../lib -lring
+g++ -c -fpic -O2 ring_httplib.cpp -I $PWD/../../language/include
+g++ -shared -o $PWD/../../lib/libring_httplib.so ring_httplib.o -L $PWD/../../lib -lring -lpthread
