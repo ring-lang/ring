923ab8af7a3d16c56ce4a8251c464406e8e35206
Mounir IDRASSI <mounir.idrassi@idrix.fr>
2022-05-24 00:23:00 +0200

    Fix stack overflow crash in parser on Windows when loading large Ring Qt files by setting stack reserved size to 8 MiB
    Linux uses 8 MiB reserved stack size by default whereas Windows uses 1 MiB. Setting it explicitly to 8 MiB to match Linux and to fix the parser crash.

--- language/src/buildvcstatic.bat
+++ language/src/buildvcstatic.bat
@@ -9,7 +9,7 @@ ring_stmt.c ring_expr.c ring_codegen.c ring_vm.c ring_vmexpr.c ring_vmvars.c ^
 ring_vmlists.c ring_vmfuncs.c ring_api.c ring_vmoop.c ring_vmcui.c ^
 ring_vmtrycatch.c ring_vmstrindex.c ring_vmjump.c ring_vmduprange.c ^
 ring_vmperformance.c ring_vmexit.c ring_vmstackvars.c ring_vmstate.c ring_generallib_extension.c ring_math_extension.c ring_file_extension.c ring_os_extension.c ring_list_extension.c ring_refmeta_extension.c ^
-ring_ext.c ring_dll_extension.c ring_objfile.c -I"..\include" /link /SUBSYSTEM:CONSOLE,"%ringsubsystem%" /OUT:..\..\bin\ring.exe 
+ring_ext.c ring_dll_extension.c ring_objfile.c -I"..\include" /link /SUBSYSTEM:CONSOLE,"%ringsubsystem%" /STACK:8388608 /OUT:..\..\bin\ring.exe 
 
 del *.obj
 
