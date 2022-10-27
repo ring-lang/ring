cdb0aedc34a71cf79484f4ddcffa27680d265b32
Mahmoud Fayed <msfclipper@yahoo.com>
2022-10-28 02:25:28 +0300

    Update extensions/tutorial/integerpointer/mylib.c - Code Format

--- extensions/tutorial/integerpointer/mylib.c
+++ extensions/tutorial/integerpointer/mylib.c
@@ -3,27 +3,27 @@
 
 void changeValue(int *var)
 {
-    *var = (*var) + 1;
+	*var = (*var) + 1;
 }
 
 RING_FUNC(ring_changevalue)
 {
 
-    if (RING_API_PARACOUNT != 1)
-    {
-        RING_API_ERROR(RING_API_BADPARACOUNT);
-        return;
-    }
+	if (RING_API_PARACOUNT != 1)
+	{
+		RING_API_ERROR(RING_API_BADPARACOUNT);
+		return;
+	}
 
-    if (!RING_API_ISSTRING(1))
-    {
-        RING_API_ERROR(RING_API_BADPARATYPE);
-        return;
-    }
+	if (!RING_API_ISSTRING(1))
+	{
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return;
+	}
 
-    int *p1 = RING_API_GETINTPOINTER(1);
-    changeValue(p1);
-    RING_API_ACCEPTINTVALUE(1);
+	int *p1 = RING_API_GETINTPOINTER(1);
+	changeValue(p1);
+	RING_API_ACCEPTINTVALUE(1);
 
 }
 
