31e599cda3e8ab9fcc8a53cc211ea0b79d96b38b
Mahmoud Fayed <msfclipper@yahoo.com>
2022-10-29 20:43:31 +0300

    Add extensions/tutorial/stringpointer

---
+++ extensions/tutorial/stringpointer/buildvc.bat
@@ -0,0 +1,5 @@
+cls
+call ../../../language/src/locatevc.bat
+cl /c /DEBUG mylib.c -I"..\..\..\language\include"
+link /DEBUG mylib.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylib.dll /SUBSYSTEM:CONSOLE,"5.01" 
+del mylib.obj
---
+++ extensions/tutorial/stringpointer/test.ring
@@ -0,0 +1,7 @@
+? "Loading Library"
+loadlib("mylib.dll")
+
+cString = space(100)
+changevalue(:cString)
+? cString
+
