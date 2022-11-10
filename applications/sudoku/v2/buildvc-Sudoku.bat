f2009c1832ffbae6760e08828975d2ffacc46837
Mahmoud Fayed <msfclipper@yahoo.com>
2022-11-10 03:02:12 +0300

    Update Batch files for applications/sudoku

--- applications/sudoku/v2/buildvc-Sudoku.bat
+++ applications/sudoku/v2/buildvc-Sudoku.bat
@@ -1,7 +1,7 @@
 echo build Sudoku.c  code using Visual Studio C
 cls
-call ..\..\..\src\locatevc.bat
-cl /c /DEBUG mylibSudoku.c -I"..\..\..\include"
+call ..\..\..\language\src\locatevc.bat
+cl /c /DEBUG mylibSudoku.c -I"..\..\..\language\include"
 link /DEBUG mylibSudoku.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylibSudoku.dll /SUBSYSTEM:CONSOLE,"5.01"
 del mylibSudoku.obj
 pause
--- applications/sudoku/v3/buildvc-Sudoku.bat
+++ applications/sudoku/v3/buildvc-Sudoku.bat
@@ -1,9 +1,9 @@
 echo build Sudoku.c  code using Visual Studio C
 cls
-call ..\..\..\src\locatevc.bat
-cl /c /DEBUG mylibSudoku.c -I"..\..\..\include"
+call ..\..\..\language\src\locatevc.bat
+cl /c /DEBUG mylibSudoku.c -I"..\..\..\language\include"
 link /DEBUG mylibSudoku.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylibSudoku.dll /SUBSYSTEM:CONSOLE,"5.01"
-cl /c /DEBUG mylibSudokuGenerator.c -I"..\..\..\include"
+cl /c /DEBUG mylibSudokuGenerator.c -I"..\..\..\language\include"
 link /DEBUG mylibSudokuGenerator.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylibSudokuGenerator.dll /SUBSYSTEM:CONSOLE,"5.01"
 
 del mylibSudoku.obj
