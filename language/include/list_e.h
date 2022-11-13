32cd6f3a3998fbb3f30896c6b6b31f094fb3cf21
Mahmoud Fayed <msfclipper@yahoo.com>
2022-11-13 04:00:49 +0300

    Update Ring VM - Add Function: RefCount()

--- language/include/list_e.h
+++ language/include/list_e.h
@@ -29,4 +29,6 @@
     void ring_vm_listfuncs_reverse ( void *pPointer ) ;
 
     void ring_vm_listfuncs_reference ( void *pPointer ) ;
+
+    void ring_vm_listfuncs_refcount ( void *pPointer ) ;
 #endif
--- language/src/list_e.c
+++ language/src/list_e.c
@@ -21,6 +21,7 @@ void ring_vm_listfuncs_loadfunctions ( RingState *pRingState )
     RING_API_REGISTER("binarysearch",ring_vm_listfuncs_binarysearch);
     RING_API_REGISTER("ref",ring_vm_listfuncs_reference);
     RING_API_REGISTER("reference",ring_vm_listfuncs_reference);
+    RING_API_REGISTER("refcount",ring_vm_listfuncs_refcount);
     /* Instead of NewList() function from StdLib (Just to support Old Code until converting it to List() ) */
     RING_API_REGISTER("newlist",ring_vm_listfuncs_list);
 }
@@ -670,6 +671,22 @@ void ring_vm_listfuncs_reference ( void *pPointer )
         if ( RING_API_ISLIST(1) ) {
             pList = RING_API_GETLIST(1) ;
             RING_API_RETLISTBYREF(pList);
+        }
+        else {
+            RING_API_ERROR(RING_API_BADPARATYPE);
+        }
+        return ;
+    }
+    RING_API_ERROR(RING_API_BADPARACOUNT);
+}
+
+void ring_vm_listfuncs_refcount ( void *pPointer )
+{
+    List *pList  ;
+    if ( RING_API_PARACOUNT == 1 ) {
+        if ( RING_API_ISLIST(1) ) {
+            pList = RING_API_GETLIST(1) ;
+            RING_API_RETNUMBER(pList->nReferenceCount);
         }
         else {
             RING_API_ERROR(RING_API_BADPARATYPE);
