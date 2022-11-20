ffb408d33eaa1b51812483359d426ad535d663ee
Mahmoud Fayed <msfclipper@yahoo.com>
2022-11-20 21:58:34 +0300

    Update Visual Source (Using PWCT) - language/src/vmfuncs.c - When calling a C function that return a List - Move it to the previous scope

--- extensions/android/ringqt/project/ring/src/vmfuncs.c
+++ extensions/android/ringqt/project/ring/src/vmfuncs.c
@@ -316,10 +316,14 @@ void ring_vm_call2 ( VM *pVM )
             }
         }
         /*
-        **  We don't need to move the list to the previous scope 
-        **  Because RING_API_RETLIST() will do this for us 
-        **  Return (Delete Scope, Restore ActiveMem) 
+        **  We move the list to the previous scope 
+        **  Because we may have nested functions calls like refcount( list(nSize) ) 
+        **  Move returned List to the previous scope 
         */
+        if ( RING_VM_STACK_ISPOINTER ) {
+            ring_vm_movetoprevscope(pVM);
+        }
+        /* Return (Delete Scope, Restore ActiveMem) */
         ring_list_deleteitem_gc(pVM->pRingState,pVM->pFuncCallList,ring_list_getsize(pVM->pFuncCallList));
         ring_vm_deletescope(pVM);
         /* Restore ActiveMem */
