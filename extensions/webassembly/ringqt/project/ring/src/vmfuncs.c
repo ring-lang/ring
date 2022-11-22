d347601a5f7ad907347e6e624e5d282e775cd8ee
Mahmoud Fayed <msfclipper@yahoo.com>
2022-11-22 21:53:22 +0300

    Update Ring For Android and WebAssembly

--- extensions/webassembly/ringqt/project/ring/src/vmfuncs.c
+++ extensions/webassembly/ringqt/project/ring/src/vmfuncs.c
@@ -704,15 +704,15 @@ void ring_vm_freetemplists ( VM *pVM )
     if ( ring_list_getsize(pVM->pFuncCallList) > 0 ) {
         pTempMem = ring_list_getlist(pVM->pFuncCallList,ring_list_getsize(pVM->pFuncCallList)) ;
         pTempMem = ring_list_getlist(pTempMem,RING_FUNCCL_TEMPMEM) ;
-        if ( (RING_VM_IR_READI == 0) || (RING_VM_IR_READIVALUE(2) !=pVM->nActiveScopeID) ) {
-            RING_VM_IR_READI = ring_list_getsize(pTempMem) + 1 ;
-            RING_VM_IR_READIVALUE(2) = pVM->nActiveScopeID ;
-        }
-        nStart = RING_VM_IR_READI ;
     }
     else {
-        return ;
+        pTempMem = pVM->pTempMem ;
     }
+    if ( (RING_VM_IR_READI == 0) || (RING_VM_IR_READIVALUE(2) !=pVM->nActiveScopeID) ) {
+        RING_VM_IR_READI = ring_list_getsize(pTempMem) + 1 ;
+        RING_VM_IR_READIVALUE(2) = pVM->nActiveScopeID ;
+    }
+    nStart = RING_VM_IR_READI ;
     /* Delete Temp. Lists created during the function call */
     if ( nStart == 1 ) {
         /* No Temp. Lists are created before the code execution of the function */
