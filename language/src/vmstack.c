895123fdc4718dd5e716954cc27fb618b4c06126
Mahmoud Fayed <msfclipper@yahoo.com>
2023-02-20 17:59:20 +0300

    Update Visual Source (Using PWCT) - Ring VM - ring_vm_len() - Pass numbers - Based on a report by Ilir Liburn

--- language/src/vmstack.c
+++ language/src/vmstack.c
@@ -714,7 +714,8 @@ void ring_vm_len ( VM *pVM )
         RING_VM_STACK_PUSHNVALUE(nSize);
     }
     else if ( RING_VM_STACK_ISNUMBER ) {
-        ring_vm_error(pVM,RING_VM_ERROR_FORLOOPDATATYPE);
+        RING_VM_STACK_POP ;
+        RING_VM_STACK_PUSHNVALUE(0.0);
     }
     else if ( RING_VM_STACK_ISPOINTER ) {
         if ( RING_VM_STACK_OBJTYPE == RING_OBJTYPE_VARIABLE ) {
