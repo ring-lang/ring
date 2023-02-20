f95d7dba0ac7eb360aa11953dd3811fb0f27ac2b
Mahmoud Fayed <msfclipper@yahoo.com>
2023-02-20 11:56:00 +0300

    Update Ring For Android & WebAssembly

--- extensions/webassembly/ringqt/project/ring/src/vmtry.c
+++ extensions/webassembly/ringqt/project/ring/src/vmtry.c
@@ -7,6 +7,7 @@ void ring_vm_try ( VM *pVM )
     List *pList  ;
     pList = ring_list_newlist_gc(pVM->pRingState,pVM->pTry);
     ring_list_addint_gc(pVM->pRingState,pList,RING_VM_IR_READI);
+    ring_list_addint_gc(pVM->pRingState,pList,pVM->lInsideEval);
     ring_vm_savestate(pVM,pList);
     pVM->nActiveCatch = 0 ;
 }
@@ -16,10 +17,11 @@ void ring_vm_catch ( VM *pVM,const char *cError )
     List *pList  ;
     pList = ring_list_getlist(pVM->pTry,ring_list_getsize(pVM->pTry));
     pVM->nPC = ring_list_getint(pList,1) ;
+    pVM->lInsideEval = ring_list_getint(pList,2) ;
     /* Define variable cCatchError to contain the error message */
     ring_list_setstring_gc(pVM->pRingState,ring_list_getlist(ring_vm_getglobalscope(pVM),6),3,cError);
     /* Avoid invalidated cError (variable) content by restore */
-    ring_vm_restorestate(pVM,pList,2,RING_STATE_TRYCATCH);
+    ring_vm_restorestate(pVM,pList,3,RING_STATE_TRYCATCH);
     /* Tell C-API caller (CALL command) that catch happens! */
     pVM->nActiveCatch = 1 ;
     /* Catch Statements must be executed without try effects */
