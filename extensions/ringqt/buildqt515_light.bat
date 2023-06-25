69c10d9536c39debda3ea94ee04b0c55ad569479
Mahmoud Fayed <msfclipper@yahoo.com>
2023-06-25 06:43:59 +0300

    Update RingQt - Batch files - Use Qt 5.15.0

--- extensions/ringqt/buildqt515_light.bat
+++ extensions/ringqt/buildqt515_light.bat
@@ -1,6 +1,6 @@
 setlocal enableextensions enabledelayedexpansion
 call ..\..\language\src\locatevc.bat auto %1
-"C:\Qt\5.15.2\msvc2019\bin\qmake.exe" ring_qt512_light.pro -spec win32-msvc "CONFIG+=release"
+"C:\Qt5.15.0\\msvc2019\bin\qmake.exe" ring_qt512_light.pro -spec win32-msvc "CONFIG+=release"
 "C:\Qt\Tools\QtCreator\bin\jom\jom.exe"
 copy release\ringqt_light.dll ..\..\bin
 endlocal
