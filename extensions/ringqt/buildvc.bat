232c44d1da116b6e248dd1cf092e381f48c5ab19
Mahmoud Fayed <msfclipper@yahoo.com>
2023-06-25 06:50:28 +0300

    Update RingQt - Remove old files

--- extensions/ringqt/buildvc.bat
+++
@@ -1,5 +0,0 @@
-setlocal enableextensions enabledelayedexpansion
-call ../../language/src/locatevc.bat auto %1
-"C:\Qt\Qt5.5.0\5.5\msvc2013\bin\qmake.exe" B:\ring\extensions\ringqt\ring_qt.pro -r -spec win32-msvc2013
-"C:\Qt\Qt5.5.0\Tools\QtCreator\bin\jom.exe" 
-endlocal
--- extensions/ringqt/gencode.bat
+++
@@ -1,9 +0,0 @@
-set RINGQT_WEBASSEMBLY=
-set RINGQT_ANDROID=
-set RINGQT_NOWEBKIT=
-set RINGQT_NOBLUETOOTH=
-ring ..\codegen\parsec.ring classes\qt.cf cpp\src\ring_qt.cpp ..\..\libraries\guilib\classes\ring_qt.ring
-cd events
-ring qtevents.ring
-cd ..
-
