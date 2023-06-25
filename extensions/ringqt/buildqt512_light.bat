ab98b8e7f370076a4c32f92724edaa6acfff0440
Mahmoud Fayed <msfclipper@yahoo.com>
2023-06-25 06:54:42 +0300

    Update RingQt - Remove old batch files & use new project files

--- extensions/ringqt/buildqt512_light.bat
+++
@@ -1,7 +0,0 @@
-setlocal enableextensions enabledelayedexpansion
-call ..\..\language\src\locatevc.bat auto %1
-"C:\Qt\Qt5.12.6\5.12.6\msvc2017\bin\qmake.exe" ring_qt512_light.pro -spec win32-msvc "CONFIG+=release"
-rem "C:\Qt\Qt5.12.6\Tools\QtCreator\bin\jom.exe" -f ../build-ring_qt512-Desktop_Qt_5_12_6_MSVC2017_32bit-Release/Makefile qmake_all
-"C:\Qt\Qt5.12.6\Tools\QtCreator\bin\jom.exe"
-copy release\ringqt_light.dll ..\..\bin
-endlocal
--- extensions/ringqt/buildqt515.bat
+++ extensions/ringqt/buildqt515.bat
@@ -1,6 +1,6 @@
 setlocal enableextensions enabledelayedexpansion
 call ..\..\language\src\locatevc.bat auto %1
-"C:\Qt\5.15.0\msvc2019\bin\qmake.exe" ring_qt512.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
+"C:\Qt\5.15.0\msvc2019\bin\qmake.exe" ring_qt515.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
 "C:\Qt\Tools\QtCreator\bin\jom\jom.exe"
 copy release\ringqt.dll ..\..\bin
 endlocal
