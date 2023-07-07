0878d663d23d08d311c3375cd8f1daca9578b557
Mahmoud Fayed <msfclipper@yahoo.com>
2023-07-07 21:05:50 +0300

    Update RingQt - Enable Qt3D & QQuickWidget on Linux

--- extensions/ringqt/gencodeqt515.sh
+++ extensions/ringqt/gencodeqt515.sh
@@ -2,7 +2,7 @@ export RINGQT_ANDROID=
 export RINGQT_NOWEBKIT=1
 export RINGQT_NOBLUETOOTH=
 export RINGQT_QT512=1
-export RINGQT_NOQT3D=1
+export RINGQT_NOQT3D=
 ../../bin/ring ../codegen/parsec.ring classes/qt.cf cpp/src/ring_qt.cpp ../../libraries/guilib/classes/ring_qt.ring
 cd events
 ../../../bin/ring qtevents.ring
