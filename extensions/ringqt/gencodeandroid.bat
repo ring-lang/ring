5a11f5604ccaf2883de5be70352096feea14b551
Mahmoud Fayed <msfclipper@yahoo.com>
2023-07-09 11:15:19 +0300

    Update RingQt - Avoid flag: RINGQT_ANDROID_QT512

--- extensions/ringqt/gencodeandroid.bat
+++ extensions/ringqt/gencodeandroid.bat
@@ -1,6 +1,5 @@
 set RINGQT_WEBASSEMBLY=
 set RINGQT_ANDROID=1
-set RINGQT_ANDROID_QT512=1
 set RINGQT_NOWEBKIT=
 set RINGQT_NOBLUETOOTH=1
 ring ..\codegen\parsec.ring classes\qt.cf cpp\src\ring_qt.cpp ..\..\libraries\guilib\classes\ring_qt.ring
