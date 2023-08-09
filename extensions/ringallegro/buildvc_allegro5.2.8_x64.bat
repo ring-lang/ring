df6eff743895247c9927f29c970902decb1192d4
Mahmoud Fayed <msfclipper@yahoo.com>
2023-08-09 21:51:15 +0300

    Update extensions/ringallegro/buildvc_allegro5.2.8_x64.bat

--- extensions/ringallegro/buildvc_allegro5.2.8_x64.bat
+++ extensions/ringallegro/buildvc_allegro5.2.8_x64.bat
@@ -1,7 +1,7 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/src/locatevc.bat auto %1
-cl /c /DEBUG ring_allegro.c -I"..\..\extensions\libdepwin\allegro5.2.8_x64\include" -I"..\..\language\include"
+cl /c /DEBUG ring_allegro.c -I"..\..\extensions\libdepwin\allegro5.2.8\include" -I"..\..\language\include"
 link /DEBUG ring_allegro.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_image.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_dialog.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_ttf.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_acodec.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_primitives.lib ^
 ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_audio.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_color.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_font.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_video.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_primitives.lib ..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_memfile.lib ^
 		..\..\extensions\libdepwin\allegro5.2.8\lib\x64\allegro_physfs.lib ^
