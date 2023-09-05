cceb752939a392141d73458fb2a2e34af56b3c97
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-05 10:32:33 +0300

    Update extensions/android/ringlibsdl/project/build.bat

--- extensions/android/ringlibsdl/project/build.bat
+++ extensions/android/ringlibsdl/project/build.bat
@@ -3,11 +3,11 @@ cls
 
 rem You will need to modify this batch file based on your environment
 
-set JAVA_HOME=C:\Program Files (x86)\Java\jdk1.8.0_05
-set ANDROID_HOME=B:\mahmoud\Tools\JavaAndroid\adt-bundle-windows-x86-20140702\sdk
-set NDK_ROOT=B:\mahmoud\Tools\JavaAndroid\android-ndk-r10c
+set JAVA_HOME=C:\PL\Java\jdk1.8.0_05
+set ANDROID_HOME=C:\PL\adt-bundle-windows-x86-20140702\sdk
+set NDK_ROOT=c:\PL\android-ndk-r10c
 
-set path=%path%;B:\mahmoud\Tools\JavaAndroid\android-ndk-r10c
-set path=%path%;;B:\mahmoud\Tools\JavaAndroid\apache-ant-1.9.4\bin
+set path=%path%;c:\PL\android-ndk-r10c
+set path=%path%;;C:\PL\apache-ant-1.9.4\bin
 
 ndk-build
