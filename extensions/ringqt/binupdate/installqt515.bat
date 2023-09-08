ee3cf8a12aa6a8a75ef4dac9e2815b5373af60c6
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-08 09:14:22 +0300

    Update extensions/ringqt/binupdate/installqt515.bat - Use Qt 5.15.15

--- extensions/ringqt/binupdate/installqt515.bat
+++ extensions/ringqt/binupdate/installqt515.bat
@@ -1,14 +1,14 @@
-Rem Simple Batch File to copy Qt 5.15.0 DLL files to the bin folder
-Rem 2020-2022, Mahmoud Fayed <msfclipper@yahoo.com>
+Rem Simple Batch File to copy Qt 5.15.15 DLL files to the bin folder
+Rem 2020-2023, Mahmoud Fayed <msfclipper@yahoo.com>
 
 cd ..\..\..\bin
 
-copy C:\Qt\5.15.0\msvc2019\bin\*.dll .
-copy C:\Qt\5.15.0\msvc2019\bin\QtWebEngineProcess.exe .
-xcopy C:\Qt\5.15.0\msvc2019\plugins . /E /H /C /R /Q /Y
+copy C:\Qt\5.15.15\msvc2019\bin\*.dll .
+copy C:\Qt\5.15.15\msvc2019\bin\QtWebEngineProcess.exe .
+xcopy C:\Qt\5.15.15\msvc2019\plugins . /E /H /C /R /Q /Y
 md qml
-xcopy C:\Qt\5.15.0\msvc2019\qml qml /E /H /C /R /Q /Y
+xcopy C:\Qt\5.15.15\msvc2019\qml qml /E /H /C /R /Q /Y
 md translations
-xcopy C:\Qt\5.15.0\msvc2019\translations translations /E /H /C /R /Q /Y
+xcopy C:\Qt\5.15.15\msvc2019\translations translations /E /H /C /R /Q /Y
 md resources
-xcopy C:\Qt\5.15.0\msvc2019\resources resources /E /H /C /R /Q /Y
+xcopy C:\Qt\5.15.15\msvc2019\resources resources /E /H /C /R /Q /Y
