233a2e0127230ae3ca26a256825386b0250a8100
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-14 17:15:13 +0300

    Update language/src/installdepmac.sh - Use mysql-client

--- language/src/installdepmac.sh
+++ language/src/installdepmac.sh
@@ -1,2 +1,2 @@
-brew install -k git gcc unixodbc mysql-connector-c allegro openssl qt@5 glew libuv libpq sdl2 sdl2_image sdl2_mixer sdl2_net sdl2_ttf httpd
+brew install git gcc unixodbc mysql-client allegro openssl qt@5 glew libuv libpq sdl2 sdl2_image sdl2_mixer sdl2_net sdl2_ttf httpd
 brew link --force qt@5
