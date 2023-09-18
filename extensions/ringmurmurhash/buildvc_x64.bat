12d72936a3d7e0963afcbd12c2630f18e4e10ab4
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-18 05:36:20 +0300

    Update RingMurmurHash - Add: extensions/ringmurmurhash/buildvc_x64.bat

---
+++ extensions/ringmurmurhash/buildvc_x64.bat
@@ -0,0 +1,9 @@
+cls
+setlocal enableextensions enabledelayedexpansion
+call ../../language/src/locatevc.bat x64
+cl /c /DEBUG ring_murmurhash.c libmurmurhash/MurmurHash1.c ^
+    libmurmurhash/MurmurHash2.c libmurmurhash/MurmurHash3.c ^
+     -I"../../language/include" -I"libmurmurhash/"
+link /DEBUG *.obj ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_murmurhash.dll 
+del *.obj
+endlocal
