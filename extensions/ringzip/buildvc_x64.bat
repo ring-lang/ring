a07aa969f03d3b98601fd391ce323f72a4793685
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-18 05:54:44 +0300

    Update RingZIP - Add: extensions/ringzip/buildvc_x64.bat

---
+++ extensions/ringzip/buildvc_x64.bat
@@ -0,0 +1,7 @@
+cls
+setlocal enableextensions enabledelayedexpansion
+call ../../language/src/locatevc.bat x64
+cl /c /DEBUG ring_libzip.c -I"..\ringzip" -I"..\..\language\include"
+link /DEBUG ring_libzip.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_libzip.dll 
+del ring_libzip.obj
+endlocal
