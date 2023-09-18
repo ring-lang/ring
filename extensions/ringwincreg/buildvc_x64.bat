8b57ed30ddc11c8ba98d6c169955c86c3357124f
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-18 05:59:50 +0300

    Update RingWinCREG - Add: extensions/ringwincreg/buildvc_x64.bat

---
+++ extensions/ringwincreg/buildvc_x64.bat
@@ -0,0 +1,20 @@
+echo off
+setlocal enableextensions enabledelayedexpansion
+call ../../language/src/locatevc.bat x64
+
+cl /c /DEBUG /EHsc creg_registry.cpp ring_wincreg.cpp -I"..\..\language\include"
+
+link /DEBUG creg_registry.obj advapi32.lib shlwapi.lib ring_wincreg.obj  ..\..\lib\ring.lib  /DLL /OUT:ring_wincreg.dll
+
+mt.exe -manifest ring_wincreg.dll.manifest -outputresource:ring_wincreg.dll;2
+
+copy ring_wincreg.dll ..\..\bin\ring_wincreg.dll
+
+del *.obj
+del ring_wincreg.ilk
+del ring_wincreg.exp
+del ring_wincreg.pdb
+del ring_wincreg.dll
+del ring_wincreg.lib
+
+endlocal
