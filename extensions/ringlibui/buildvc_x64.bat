f1db47a68365516c07bcc883c55b687f61d293d4
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-18 05:20:36 +0300

    Update RingLibui - Add: extensions/ringlibui/buildvc_x64.bat - Build RingLibui for Windows 64bit

---
+++ extensions/ringlibui/buildvc_x64.bat
@@ -0,0 +1,10 @@
+cls
+setlocal enableextensions enabledelayedexpansion
+call ../../language/src/locatevc.bat x64
+cl /c  ring_libui.c -I "..\..\extensions\libdepwin\libuishared_x64" -I"..\..\language\include"
+link  ring_libui.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\libuishared_x64\libui.lib User32.lib kernel32.lib gdi32.lib uxtheme.lib msimg32.lib comdlg32.lib d2d1.lib dwrite.lib comctl32.lib ole32.lib oleaut32.lib oleacc.lib windowscodecs.lib /DLL /OUT:..\..\bin\ring_libui.dll 
+
+del ring_libui.obj
+endlocal
+
+
