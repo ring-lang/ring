62e32b42f01e96c9a39163b9be7b40a9db36899f
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-18 05:34:01 +0300

    Update RingHTTPLib - Add: extensions/ringhttplib/buildvc_x64.bat

---
+++ extensions/ringhttplib/buildvc_x64.bat
@@ -0,0 +1,7 @@
+cls
+setlocal enableextensions enabledelayedexpansion
+call ../../language/src/locatevc.bat x64
+cl /EHsc /c ring_httplib.cpp -I"..\..\language\include"
+link ring_httplib.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_httplib.dll 
+del ring_httplib.obj
+endlocal
