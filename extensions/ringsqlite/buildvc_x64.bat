43d85f867f269fffdd4daca1f8b511e80ea50ff4
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-18 05:53:30 +0300

    Update RingSQLite - Add: extensions/ringsqlite/buildvc_x64.bat

---
+++ extensions/ringsqlite/buildvc_x64.bat
@@ -0,0 +1,7 @@
+cls
+setlocal enableextensions enabledelayedexpansion
+call ../../language/src/locatevc.bat x64
+cl /c /DEBUG ring_vmsqlite.c sqlite3.c -I"..\..\language\include"  
+link /DEBUG ring_vmsqlite.obj sqlite3.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_sqlite.dll 
+del *.obj
+endlocal
