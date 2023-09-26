2d13cd0eca69581a60b1e603bd6a569db074cdfb
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-26 14:18:45 +0300

    Update extensions/tutorial - Batch files

--- extensions/tutorial/filterlist/buildvc_x64.bat
+++ extensions/tutorial/filterlist/buildvc_x64.bat
@@ -1,5 +1,7 @@
 cls
+setlocal enableextensions enabledelayedexpansion
 call ../../../language/src/locatevc.bat x64
 cl /c /DEBUG mylib.c -I"..\..\..\language\include"
 link /DEBUG mylib.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylib.dll
 del mylib.obj
+endlocal
