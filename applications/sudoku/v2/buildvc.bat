973b6d1439a30eaab4f19867ca6501a882e2da2c
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-26 12:11:08 +0300

    Add: applications/sudoku/v2/buildvc_x64.bat

--- applications/sudoku/v2/buildvc.bat
+++ applications/sudoku/v2/buildvc.bat
@@ -4,5 +4,3 @@ call ..\..\..\language\src\locatevc.bat
 cl /c /DEBUG mylibSudoku.c -I"..\..\..\language\include"
 link /DEBUG mylibSudoku.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylibSudoku.dll /SUBSYSTEM:CONSOLE,"5.01"
 del mylibSudoku.obj
-pause
-
