8c996ea4eea8654005eb8f4bd0235b4680b68305
Mahmoud Fayed <msfclipper@yahoo.com>
2023-09-27 16:10:57 +0300

    Update extension/ringqt/binupdate - Batch Files

--- extensions/ringqt/binupdate/installqt515_x64.bat
+++ extensions/ringqt/binupdate/installqt515_x64.bat
@@ -1,16 +1,19 @@
 Rem Simple Batch File to copy Qt 5.15.15 DLL files to the bin folder
 Rem 2020-2023, Mahmoud Fayed <msfclipper@yahoo.com>
 
+IF "%RING_QT_DIR%"=="" SET RING_QT_DIR=C:\Qt
+IF "%RING_QT_VERSION%"=="" SET RING_QT_VERSION=5.15.15
+
 cd ..\..\..\bin
 
-copy C:\Qt\5.15.15\msvc2019_64\bin\*.dll .
-copy C:\Qt\5.15.15\msvc2019_64\bin\QtWebEngineProcess.exe .
-xcopy C:\Qt\5.15.15\msvc2019_64\plugins . /E /H /C /R /Q /Y
+copy %RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\bin\*.dll .
+copy %RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\bin\QtWebEngineProcess.exe .
+xcopy %RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\plugins . /E /H /C /R /Q /Y
 md qml
-xcopy C:\Qt\5.15.15\msvc2019_64\qml qml /E /H /C /R /Q /Y
+xcopy %RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\qml qml /E /H /C /R /Q /Y
 md translations
-xcopy C:\Qt\5.15.15\msvc2019_64\translations translations /E /H /C /R /Q /Y
+xcopy %RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\translations translations /E /H /C /R /Q /Y
 md resources
-xcopy C:\Qt\5.15.15\msvc2019_64\resources resources /E /H /C /R /Q /Y
+xcopy %RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\resources resources /E /H /C /R /Q /Y
 
 cd ..\extensions\ringqt\binupdate
