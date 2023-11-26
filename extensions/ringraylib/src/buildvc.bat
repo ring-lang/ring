48b0cda9a366865f8df25bca6eac01ea59d1b383
Mahmoud Fayed <msfclipper@yahoo.com>
2023-11-26 08:09:12 +0300

    Update Batch Files for RingRayLib & RingTilengine

--- extensions/ringraylib/src/buildvc.bat
+++ extensions/ringraylib/src/buildvc.bat
@@ -1,7 +1,7 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../../language/src/locatevc.bat auto %1
-cl /W2 /O2 /c /DEBUG ring_raylib.c -I"..\..\..\extensions\libdepwin\raylib2.5\include" -I"..\..\..\language\include"
-link /DEBUG ring_raylib.obj  ..\..\..\lib\ring.lib ..\..\..\extensions\libdepwin\raylib2.5\lib\raylib.lib  /DLL /OUT:..\..\..\bin\ring_raylib.dll 
+cl /c %ringcflags% /DEBUG ring_raylib.c -I"..\..\..\extensions\libdepwin\raylib2.5\include" -I"..\..\..\language\include"
+link %ringldflags% ring_raylib.obj  ..\..\..\lib\ring.lib ..\..\..\extensions\libdepwin\raylib2.5\lib\raylib.lib  /DLL /OUT:..\..\..\bin\ring_raylib.dll 
 del ring_raylib.obj
 endlocal
--- extensions/ringtilengine/buildvc.bat
+++ extensions/ringtilengine/buildvc.bat
@@ -1,8 +1,8 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/src/locatevc.bat auto %1
-cl /W2 /O2 /c /DEBUG ring_tilengine.c -I "..\..\extensions\libdepwin\tilengine\include" -I"..\..\language\include"
-link /DEBUG ring_tilengine.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\tilengine\lib\tilengine.lib /DLL /OUT:..\..\bin\ring_tilengine.dll /SUBSYSTEM:CONSOLE,"5.01" 
+cl /c %ringcflags% ring_tilengine.c -I "..\..\extensions\libdepwin\tilengine\include" -I"..\..\language\include"
+link %ringldflags% ring_tilengine.obj  ..\..\lib\ring.lib ..\..\extensions\libdepwin\tilengine\lib\tilengine.lib /DLL /OUT:..\..\bin\ring_tilengine.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_tilengine.obj
 endlocal
 
