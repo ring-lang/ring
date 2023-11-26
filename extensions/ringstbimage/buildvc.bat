18e06b69f54a68f18075b1deaac99917ec847e54
Mahmoud Fayed <msfclipper@yahoo.com>
2023-11-26 08:03:42 +0300

    Update RingStbImage - Batch files

--- extensions/ringstbimage/buildvc.bat
+++ extensions/ringstbimage/buildvc.bat
@@ -1,7 +1,7 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/src/locatevc.bat auto %1
-cl /c /DEBUG ring_stbimage.c -I"..\..\language\include"
-link /DEBUG ring_stbimage.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_stbimage.dll  /SUBSYSTEM:CONSOLE,"5.01" 
+cl /c %ringcflags% ring_stbimage.c -I"..\..\language\include"
+link %ringldflags% ring_stbimage.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_stbimage.dll  /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_stbimage.obj
 endlocal
--- extensions/ringstbimage/buildvc_x64.bat
+++ extensions/ringstbimage/buildvc_x64.bat
@@ -1,7 +1,7 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/src/locatevc.bat auto x64
-cl /c /DEBUG ring_stbimage.c -I"..\..\language\include"
-link /DEBUG ring_stbimage.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_stbimage.dll 
+cl /c %ringcflags% ring_stbimage.c -I"..\..\language\include"
+link %ringldflags% ring_stbimage.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_stbimage.dll 
 del ring_stbimage.obj
 endlocal
