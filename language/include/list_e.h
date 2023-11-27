056e3e25b1ab7e78868b555775f505789d3ef94f
Mahmoud Fayed <msfclipper@yahoo.com>
2023-11-27 23:56:23 +0300

    Update Visual Source (Using PWCT) - Remove updateList() - Will be added as separate extension.

--- language/include/list_e.h
+++ language/include/list_e.h
@@ -31,7 +31,4 @@
     void ring_vm_listfuncs_reference ( void *pPointer ) ;
 
     void ring_vm_listfuncs_refcount ( void *pPointer ) ;
-    /* Update List */
-
-    void ring_vm_listfuncs_updatelist ( void *pPointer ) ;
 #endif
