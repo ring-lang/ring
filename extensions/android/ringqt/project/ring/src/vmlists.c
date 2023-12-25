c8f39d5127fbbac1c33845898d2af7f7d8f94fd0
Mahmoud Fayed <msfclipper@yahoo.com>
2023-12-25 12:41:48 +0300

    Update Visual Source (Using PWCT) - Ring VM - language/src/vmlists.c - Support adding items to a list during assignment.

--- extensions/android/ringqt/project/ring/src/vmlists.c
+++ extensions/android/ringqt/project/ring/src/vmlists.c
@@ -69,7 +69,8 @@ void ring_vm_liststart ( VM *pVM )
             else {
                 pVar = (List *) RING_VM_STACK_READP ;
             }
-            RING_VM_STACK_POP ;
+            /* Support code like  aList = [1,2,3] + 4 */
+            ring_vm_dup(pVM);
         }
         if ( nType == RING_OBJTYPE_VARIABLE ) {
             /* Check error on assignment */
