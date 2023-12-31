7cf937a767a379381cbd4b88ca9858b19c8ad297
Mahmoud Fayed <msfclipper@yahoo.com>
2023-12-31 22:07:40 +0300

    Update RingOpenGL - Add batch files!

---
+++ extensions/ringopengl/opengl13/buildvc_x64.bat
@@ -0,0 +1,5 @@
+cls
+call ../../../language/src/locatevc.bat x64
+cl /c /DEBUG ring_opengl13.c -I "..\..\..\extensions\libdepwin\glew\include" -I "..\..\..\extensions\libdepwin\freeglut\include" -I"..\..\..\language\include"
+link /DEBUG ring_opengl13.obj  ..\..\..\lib\ring.lib ..\..\..\extensions\libdepwin\glew\lib\release\x64\glew32.lib ..\..\..\extensions\libdepwin\freeglut\lib\x64\freeglut.lib /DLL /OUT:..\..\..\bin\ring_opengl13.dll  
+del ring_opengl13.obj
