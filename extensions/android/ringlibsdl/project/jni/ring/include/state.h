c51f752cd7e6d11a2d1bb6e395eddb49e51e5123
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-02-19 06:56:26 +0300

    Update Visual Source (Using PWCT) - language/include/state.h - Ring State Structure - Members Order - Requires rebuilding all Ring extensions (i.e. run ring/buildvc_x64.bat).

--- extensions/android/ringlibsdl/project/jni/ring/include/state.h
+++ extensions/android/ringlibsdl/project/jni/ring/include/state.h
@@ -3,18 +3,36 @@
 #ifndef ring_state_h
 	#define ring_state_h
 	typedef struct RingState {
+		/* Virtual Machine */
+		struct VM *pVM  ;
+		/* Log File */
+		FILE *pLogFile;
+		/* Command line parameters */
+		char  **argv  ;
 		/* First use by ring_state_runfile */
 		List *pRingFilesList  ;
 		List *pRingFilesStack  ;
+		/* File Tokens */
+		List *pRingFileTokens  ;
 		/* First use by ring_parser_new */
 		List *pRingGenCode  ;
 		List *pRingFunctionsMap  ;
 		List *pRingClassesMap  ;
 		List *pRingPackagesMap  ;
+		/* Custom global scope list */
+		List *aCustomGlobalScopeStack  ;
 		/* First use by ring_vm_new */
 		List *pRingCFunctions  ;
-		/* ISCGI is 1 if we are using the language for CGI development */
-		unsigned int nISCGI : 1  ;
+		/* Command line parameters count */
+		int argc  ;
+		/* Scanner Error Code */
+		unsigned int nScannerError  ;
+		/* Counter to know if we are using (Load Again) command */
+		int nLoadAgain  ;
+		/* Custom global scope counter */
+		int nCustomGlobalScopeCounter  ;
+		/* VM Instructions Count */
+		int nInstructionsCount  ;
 		/* set to 0 if we need to compile only */
 		unsigned int nRun : 1  ;
 		/* set to 1 if we need to print byte code before execution */
@@ -32,47 +50,33 @@
 		unsigned int nGenCObj : 1  ;
 		/* set to 1 if we need to display warnings */
 		unsigned int nWarning : 1  ;
+		/* ISCGI is 1 if we are using the language for CGI development */
+		unsigned int nISCGI : 1  ;
+		/* Startup File */
+		unsigned int lStartup : 1  ;
 		/* Set to 1 to tell the scanner to don't delete the VM after execution */
 		unsigned int nDontDeleteTheVM : 1  ;
 		/* Set to 1 to tell the state that we are running Ring from Ring (to avoid exit() on error) */
 		unsigned int nRingInsideRing : 1  ;
 		/* Set to 1 to tell the scanner that we need the tokens only */
 		unsigned int nOnlyTokens : 1  ;
-		List *pRingFileTokens  ;
-		/* command line parameters */
-		int argc  ;
-		char  **argv  ;
-		/* Virtual Machine */
-		struct VM *pVM  ;
-		/* Startup File */
-		unsigned int lStartup : 1  ;
-		/* Pool Manager */
-		PoolManager vPoolManager  ;
-		unsigned int lStartPoolManager : 1  ;
-		unsigned int lDisablePoolManager : 1  ;
-		unsigned int lCreateListsUsingBlocks : 1  ;
-		unsigned int lDontCheckStateBlocks : 1  ;
 		/* Avoid line number ( when we use eval() from VM ) */
 		unsigned int lNoLineNumber : 1  ;
-		/* Custom Global Scope */
-		int nCustomGlobalScopeCounter  ;
-		List *aCustomGlobalScopeStack  ;
 		/* Flag to know if we are running from thread (Useful for the Pool Manager) */
 		unsigned int lRunFromThread : 1  ;
-		/* Counter to know if we are using (Load Again) command */
-		int nLoadAgain  ;
-		/* Log File */
-		FILE *pLogFile;
 		/* Flag (Not Case Sensitive) */
 		unsigned int lNotCaseSensitive : 1  ;
 		/* Flag (Comments As Tokens From Scanner) */
 		unsigned int lCommentsAsTokens : 1  ;
 		/* Flag to know if we are running from Object File (RINGO file) */
 		unsigned int lRunFromObjectFile : 1  ;
-		/* Scanner Error Code */
-		unsigned int nScannerError  ;
-		/* VM Instructions Count */
-		int nInstructionsCount  ;
+		/* Pool Manager Flags */
+		unsigned int lStartPoolManager : 1  ;
+		unsigned int lDisablePoolManager : 1  ;
+		unsigned int lCreateListsUsingBlocks : 1  ;
+		unsigned int lDontCheckStateBlocks : 1  ;
+		/* Pool Manager */
+		PoolManager vPoolManager  ;
 	} RingState ;
 	#define RING_STATE_VERSION "1.20"
 	#define RING_STATE_CHECKPRINTRULES if ( pParser->pRingState->nPrintRules )
