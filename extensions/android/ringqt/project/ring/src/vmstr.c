91744299441543351826beca87600725292172fc
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-02-20 11:45:21 +0300

    Update Visual Source (Using PWCT) - language/src/vmstr.c - Rename (newstr) to (cNewStr).

--- extensions/android/ringqt/project/ring/src/vmstr.c
+++ extensions/android/ringqt/project/ring/src/vmstr.c
@@ -4,22 +4,22 @@
 
 void ring_vm_string_pushv ( VM *pVM )
 {
-	char *newstr  ;
+	char *cNewStr  ;
 	char cStr[RING_CHARBUF]  ;
 	if ( (pVM->nRetItemRef>=1)  && (ring_vm_isstackpointertoobjstate(pVM)==1) ) {
 		pVM->nRetItemRef-- ;
 		return ;
 	}
-	newstr = (char *) RING_VM_STACK_READP ;
+	cNewStr = (char *) RING_VM_STACK_READP ;
 	RING_VM_STACK_POP ;
-	cStr[0] = newstr[0] ;
+	cStr[0] = cNewStr[0] ;
 	cStr[1] = '\0' ;
 	RING_VM_STACK_PUSHCVALUE2(cStr,RING_ONE);
 }
 
 void ring_vm_string_assignment ( VM *pVM )
 {
-	char *newstr  ;
+	char *cNewStr  ;
 	char cChar  ;
 	int nSize  ;
 	double nNum  ;
@@ -28,9 +28,9 @@ void ring_vm_string_assignment ( VM *pVM )
 		if ( nSize == 1 ) {
 			cChar = RING_VM_STACK_READC[0] ;
 			RING_VM_STACK_POP ;
-			newstr = (char *) RING_VM_STACK_READP ;
+			cNewStr = (char *) RING_VM_STACK_READP ;
 			RING_VM_STACK_POP ;
-			newstr[0] = cChar ;
+			cNewStr[0] = cChar ;
 			return ;
 		}
 		else {
@@ -43,8 +43,8 @@ void ring_vm_string_assignment ( VM *pVM )
 		nNum = RING_VM_STACK_READN ;
 		cChar = (char) nNum ;
 		RING_VM_STACK_POP ;
-		newstr = (char *) RING_VM_STACK_READP ;
-		newstr[0] = cChar ;
+		cNewStr = (char *) RING_VM_STACK_READP ;
+		cNewStr[0] = cChar ;
 	}
 	else {
 		ring_vm_error(pVM,RING_VM_ERROR_VARISNOTSTRING);
@@ -54,14 +54,14 @@ void ring_vm_string_assignment ( VM *pVM )
 
 void ring_vm_string_index ( VM *pVM, String *pString, double nNum1 )
 {
-	char *newstr  ;
+	char *cNewStr  ;
 	/* We will push a pointer of the sub char to the stack */
 	if ( nNum1 < 1 || nNum1 > ring_string_size(pString) ) {
 		ring_vm_error(pVM,RING_VM_ERROR_INDEXOUTOFRANGE);
 		return ;
 	}
-	newstr = pString->cStr ;
-	newstr = newstr + ((int) (nNum1 - 1) ) ;
-	RING_VM_STACK_PUSHPVALUE(newstr);
+	cNewStr = pString->cStr ;
+	cNewStr = cNewStr + ((int) (nNum1 - 1) ) ;
+	RING_VM_STACK_PUSHPVALUE(cNewStr);
 	RING_VM_STACK_OBJTYPE = RING_OBJTYPE_SUBSTRING ;
 }
