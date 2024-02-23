7aa6d634b4a999423bf7dd9d27ceb2a43c96c3e2
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-02-23 17:02:44 +0300

    Update Visual Source (Using PWCT) - language/src/stmt.c - Comments.

--- language/src/stmt.c
+++ language/src/stmt.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2RING_PARSER_FAIL13-2RING_PARSER_FAIL24 Mahmoud Fayed <msfclipper@yahoo.com> */
+/* Copyright (c) 2013-2024 Mahmoud Fayed <msfclipper@yahoo.com> */
 
 #include "ring.h"
 
