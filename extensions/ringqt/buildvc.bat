8c921ec0a6ff3b351c3a29fc9730768d51635038
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-02-26 11:38:00 +0300

    Update extensions/ringqt - Batch files - Use ring/language/build path to find locatevc.bat

--- extensions/ringqt/buildvc.bat
+++ extensions/ringqt/buildvc.bat
@@ -2,7 +2,7 @@ IF "%RING_QT_DIR%"=="" SET RING_QT_DIR=C:\Qt
 IF "%RING_QT_VERSION%"=="" SET RING_QT_VERSION=5.15.15
 
 setlocal enableextensions enabledelayedexpansion
-call ..\..\language\src\locatevc.bat auto %1
+call ..\..\language\build\locatevc.bat auto %1
 "%RING_QT_DIR%\%RING_QT_VERSION%\msvc2019\bin\qmake.exe" ring_qt515.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
 "%RING_QT_DIR%\Tools\QtCreator\bin\jom\jom.exe"
 copy release\ringqt.dll ..\..\bin
--- extensions/ringqt/buildvc_core_x64.bat
+++ extensions/ringqt/buildvc_core_x64.bat
@@ -2,7 +2,7 @@ IF "%RING_QT_DIR%"=="" SET RING_QT_DIR=C:\Qt
 IF "%RING_QT_VERSION%"=="" SET RING_QT_VERSION=5.15.16
 
 setlocal enableextensions enabledelayedexpansion
-call ..\..\language\src\locatevc.bat x64
+call ..\..\language\build\locatevc.bat x64
 "%RING_QT_DIR%\%RING_QT_VERSION%\msvc2019_64\bin\qmake.exe" ring_qt515_core.pro -spec win32-msvc "CONFIG+=qtquickcompiler"
 "%RING_QT_DIR%\Tools\QtCreator\bin\jom\jom.exe"
 copy release\ringqt_core.dll ..\..\bin
