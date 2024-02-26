af216c39acb42fdc07a8e3d51a6c4f1cc71ecdbb
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-02-26 11:25:11 +0300

    Update ring/applications/sudoku - locatevc.bat path

--- applications/sudoku/v2/buildvc.bat
+++ applications/sudoku/v2/buildvc.bat
@@ -1,6 +1,6 @@
 echo build Sudoku.c  code using Visual Studio C
 cls
-call ..\..\..\language\src\locatevc.bat
+call ..\..\..\language\build\locatevc.bat
 cl /c /DEBUG mylibSudoku.c -I"..\..\..\language\include"
 link /DEBUG mylibSudoku.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylibSudoku.dll /SUBSYSTEM:CONSOLE,"5.01"
 del mylibSudoku.obj
--- applications/sudoku/v2/buildvc_x64.bat
+++ applications/sudoku/v2/buildvc_x64.bat
@@ -1,6 +1,6 @@
 echo build Sudoku.c  code using Visual Studio C
 cls
-call ..\..\..\language\src\locatevc.bat x64
+call ..\..\..\language\build\locatevc.bat x64
 cl /c /DEBUG mylibSudoku.c -I"..\..\..\language\include"
 link /DEBUG mylibSudoku.obj  ..\..\..\lib\ring.lib /DLL /OUT:mylibSudoku.dll
 del mylibSudoku.obj
