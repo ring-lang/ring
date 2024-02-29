38c39615c70539dc6ddfbd756b5ca69421c78bd0
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-02-29 18:45:21 +0300

    Add: language/build/buildzig.bat - Building Ring Compiler/VM using Zig as a drop-in C compiler.

---
+++ language/build/buildzig.bat
@@ -0,0 +1,9 @@
+rem Building Ring Compiler/VM using Zig
+rem Tested using zig-windows-x86_64-0.12.0-dev.3079+a1b083b66
+rem Add Zig to your path, then run this batch file to build ring.exe
+
+cd ..\src 
+
+zig cc -Ofast ring.c general.c state.c ext.c hashlib.c rhtable.c vmgc.c os_e.c rstring.c rlist.c ritem.c ritems.c scanner.c parser.c stmt.c expr.c codegen.c vm.c vmerror.c vmeval.c vmthreads.c vmexpr.c vmvars.c vmlists.c vmfuncs.c ringapi.c vmoop.c vmcui.c vmtry.c vmstr.c vmjump.c vmrange.c list_e.c meta_e.c vmperf.c vmexit.c vmstack.c vmstate.c genlib_e.c math_e.c file_e.c dll_e.c objfile.c -I ../include -o ../../bin/ring.exe -lm 
+
+ 
