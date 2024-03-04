2e49e70db6a2f1aba6baf433ea80fe302d4e3b45
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-03-04 17:55:07 +0300

    Update Visual Source (Using PWCT) - Ring VM - Don't start the For-Loop/For-In-Loop if the Step value is Zero to avoid infinite loops - Based on a report by Ilir Liburn.

--- language/src/vmjump.c
+++ language/src/vmjump.c
@@ -65,8 +65,8 @@ void ring_vm_jumpfor ( VM *pVM )
 	**  nNum2 = Step value that can be positive or negative 
 	**  nNum1 = Items Count , nNum3 = Index 
 	*/
-	if ( nNum2 < 0 ) {
-		if ( ! ( nNum3 >= nNum1 ) ) {
+	if ( nNum2 <= 0 ) {
+		if ( ( ! ( nNum3 >= nNum1 ) ) || ( nNum2 == 0 ) ) {
 			RING_VM_JUMP ;
 		}
 	}
