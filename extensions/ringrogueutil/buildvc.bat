352e876d42eb8672d068ce149ca37404d6506608
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-03-08 16:07:04 +0300

    Update RingRogueUtil - Batch Files - Add the DLL file to ring/bin.

--- extensions/ringrogueutil/buildvc.bat
+++ extensions/ringrogueutil/buildvc.bat
@@ -2,6 +2,6 @@ cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/build/locatevc.bat auto %1
 cl /c /DEBUG ring_rogueutil.c -I"..\..\language\include"
-link /DEBUG ring_rogueutil.obj  ..\..\lib\ring.lib kernel32.lib Advapi32.lib  /DLL /OUT:ring_rogueutil.dll /SUBSYSTEM:CONSOLE,"5.01" 
+link /DEBUG ring_rogueutil.obj  ..\..\lib\ring.lib kernel32.lib Advapi32.lib  /DLL /OUT:..\..\bin\ring_rogueutil.dll /SUBSYSTEM:CONSOLE,"5.01" 
 del ring_rogueutil.obj
 endlocal
--- extensions/ringrogueutil/buildvc_x64.bat
+++ extensions/ringrogueutil/buildvc_x64.bat
@@ -2,6 +2,6 @@ cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/build/locatevc.bat x64
 cl /c /DEBUG ring_rogueutil.c -I"..\..\language\include"
-link /DEBUG ring_rogueutil.obj  ..\..\lib\ring.lib kernel32.lib Advapi32.lib  /DLL /OUT:ring_rogueutil.dll 
+link /DEBUG ring_rogueutil.obj  ..\..\lib\ring.lib kernel32.lib Advapi32.lib  /DLL /OUT:..\..\bin\ring_rogueutil.dll 
 del ring_rogueutil.obj
 endlocal
