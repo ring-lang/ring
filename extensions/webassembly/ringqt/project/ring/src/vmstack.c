b97a1a8702968e1c5ca566fe15bf28f6afdf3c52
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-03-26 01:20:10 +0300

    Update Visual Source (Using PWCT) - language/src/vmstack.c - Always use ring_list_genarray() when we copy lists.

--- extensions/webassembly/ringqt/project/ring/src/vmstack.c
+++ extensions/webassembly/ringqt/project/ring/src/vmstack.c
@@ -416,8 +416,8 @@ void ring_vm_list_copy ( VM *pVM,List *pNewList, List *pList )
 		pItem = ring_list_getitem(pList,RING_CPOINTER_POINTER) ;
 		pNewList->pFirst->pValue = pItem ;
 		ring_vm_gc_newitemreference(pItem);
-		ring_list_genarray(pList);
 	}
+	ring_list_genarray(pList);
 }
 
 void ring_vm_beforeequallist ( VM *pVM,List *pVar,double nNum1 )
