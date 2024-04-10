28d26bfd154014cc37f3c0b18a0830b0dfdb71ca
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-04-11 00:52:01 +0300

    Update documents/source/conf.py

--- documents/source/conf.py
+++ documents/source/conf.py
@@ -249,7 +249,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'Ring.tex', u'Ring Documentation',
-   u'Ring Team', 'manual'),
+   u'Mahmoud Fayed', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
