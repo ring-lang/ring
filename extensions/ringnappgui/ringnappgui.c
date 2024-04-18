cb7b34f1bae61aa21bd09c83e6ffa78c4e9de186
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-04-18 13:24:50 +0300

    Add: extensions/ringnappgui/ringnappgui.c

---
+++ extensions/ringnappgui/ringnappgui.c
@@ -0,0 +1,14 @@
+
+#define _WINDOWS 1
+
+#include "inet/inet.h"
+#include "osapp/osmain.h"
+#include "osgui/osgui.h"
+#include "nappgui.h"
+
+#define String RingString
+#include "ring.h"
+
+RING_LIBINIT
+{
+}
