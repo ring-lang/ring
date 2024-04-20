5ecf3c3395a936503a9cd27e827bbae79bc3bd5b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-04-20 13:51:33 +0300

    Update RingPDFGen - Add: pdf_create() function

--- extensions/ringpdfgen/ring_pdfgen.c
+++ extensions/ringpdfgen/ring_pdfgen.c
@@ -1,6 +1,30 @@
 #include "ring.h"
 
 #include "lib/pdfgen.h"
+#include "lib/pdfgen.c"
+
+RING_FUNC(ring_pdf_create)
+{
+	if ( RING_API_PARACOUNT != 3 ) {
+		RING_API_ERROR(RING_API_MISS3PARA);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISNUMBER(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(3) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETCPOINTER(pdf_create( (float ) RING_API_GETNUMBER(1), (float ) RING_API_GETNUMBER(2),(struct pdf_info *) RING_API_GETCPOINTER(3,"struct pdf_info")),"struct pdf_doc");
+}
+
 RING_LIBINIT
 {
+	RING_API_REGISTER("pdf_create",ring_pdf_create);
 }
