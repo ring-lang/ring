b16c1d5df003a60e1280a09bc46d75aec10121a0
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-04-20 16:50:46 +0300

    Update RingPDFGen - buildvc.bat and buildvc_x64.bat - Generate the dll file in ring/bin folder.

--- extensions/ringpdfgen/buildvc.bat
+++ extensions/ringpdfgen/buildvc.bat
@@ -2,6 +2,6 @@ cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/build/locatevc.bat
 cl /c  ring_pdfgen.c lib/pdfgen.c -I"..\..\language\include" -I"..\libdepwin\pdfgen\src"
-link ring_pdfgen.obj  ..\..\lib\ring.lib /DLL /OUT:ring_pdfgen.dll 
+link ring_pdfgen.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_pdfgen.dll 
 del ring_pdfgen.obj
 endlocal
