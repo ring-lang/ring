7c9f9f7a494083bde7f861f8cd27b3125b6fb0b7
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-04-20 13:53:30 +0300

    Update RingPDFGen - Add: pdf_get_err() and pdf_clear_err() functions

--- extensions/ringpdfgen/ring_pdfgen.c
+++ extensions/ringpdfgen/ring_pdfgen.c
@@ -38,8 +38,43 @@ RING_FUNC(ring_pdf_destroy)
 	pdf_destroy((struct pdf_doc *) RING_API_GETCPOINTER(1,"struct pdf_doc"));
 }
 
+
+RING_FUNC(ring_pdf_get_err)
+{
+	if ( RING_API_PARACOUNT != 2 ) {
+		RING_API_ERROR(RING_API_MISS2PARA);
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	if ( ! RING_API_ISSTRING(2) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	RING_API_RETSTRING(pdf_get_err((struct pdf_doc *) RING_API_GETCPOINTER(1,"struct pdf_doc"),RING_API_GETINTPOINTER(2)));
+	RING_API_ACCEPTINTVALUE(2) ;
+}
+
+
+RING_FUNC(ring_pdf_clear_err)
+{
+	if ( RING_API_PARACOUNT != 1 ) {
+		RING_API_ERROR(RING_API_MISS1PARA);
+		return ;
+	}
+	if ( ! RING_API_ISCPOINTER(1) ) {
+		RING_API_ERROR(RING_API_BADPARATYPE);
+		return ;
+	}
+	pdf_clear_err((struct pdf_doc *) RING_API_GETCPOINTER(1,"struct pdf_doc"));
+}
+
 RING_LIBINIT
 {
 	RING_API_REGISTER("pdf_create",ring_pdf_create);
 	RING_API_REGISTER("pdf_destroy",ring_pdf_destroy);
+	RING_API_REGISTER("pdf_get_err",ring_pdf_get_err);
+	RING_API_REGISTER("pdf_clear_err",ring_pdf_clear_err);
 }
