81bcbb2fb404eddec1a041373e062f88e3ff108e
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-06-11 14:56:31 +0300

    Update Qt version fro Qt 5.15.16 to Qt 5.15.17

--- extensions/ringqt/buildvc_nobluetooth_x64.bat
+++ extensions/ringqt/buildvc_nobluetooth_x64.bat
@@ -1,5 +1,5 @@
 IF "%RING_QT_DIR%"=="" SET RING_QT_DIR=C:\Qt
-IF "%RING_QT_VERSION%"=="" SET RING_QT_VERSION=5.15.16
+IF "%RING_QT_VERSION%"=="" SET RING_QT_VERSION=5.15.17
 
 setlocal enableextensions enabledelayedexpansion
 call ..\..\language\build\locatevc.bat x64
