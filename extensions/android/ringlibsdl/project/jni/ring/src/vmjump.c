176ba4f1fce5572c51406fed1ad1b307183b40d8
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-06-23 18:40:44 +0300

    Update Visual Source (Using PWCT) - Ring VM - language/src/vmjump.c - ring_vm_jumpfor() function - Avoid: RING_VM_IR_REG3 (i.e. RING_VM_IR_REG2 is enough)

--- extensions/android/ringlibsdl/project/jni/ring/src/vmjump.c
+++ extensions/android/ringlibsdl/project/jni/ring/src/vmjump.c
@@ -78,7 +78,7 @@ void ring_vm_jumpfor ( VM *pVM )
 	}
 	/* CALL FreeTempLists */
 	if ( ring_vm_timetofreetemplists(pVM) ) {
-		ring_vm_freetemplists(pVM,& RING_VM_IR_READIVALUE(RING_VM_IR_REG2), & RING_VM_IR_READIVALUE(RING_VM_IR_REG3));
+		ring_vm_freetemplists(pVM,& RING_VM_IR_READLOWIVALUE(RING_VM_IR_REG2), & RING_VM_IR_READHIGHIVALUE(RING_VM_IR_REG2));
 	}
 }
 
