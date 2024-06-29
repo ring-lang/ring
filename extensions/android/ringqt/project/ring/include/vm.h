755047cb71db2a074b612d236b9f99014185d289
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-06-29 23:12:49 +0300

    Update Visual Source (Using PWCT) - Ring VM - ByteCode structure - Members Order.

--- extensions/android/ringqt/project/ring/include/vm.h
+++ extensions/android/ringqt/project/ring/include/vm.h
@@ -30,14 +30,14 @@
 		void *pPointer  ;
 	} Register ;
 	typedef struct ByteCode {
-		Register aReg[RING_VM_BC_ITEMS_COUNT]  ;
-		unsigned int nIntReg: 32  ;
-		unsigned int nSmallIntReg: 16  ;
 		unsigned int nOPCode: 8  ;
-		unsigned int nReg1Type:3  ;
-		unsigned int nReg2Type:3  ;
 		unsigned int nFlagReg: 1  ;
 		unsigned int nFlagReg2: 1  ;
+		unsigned int nReg1Type:3  ;
+		unsigned int nReg2Type:3  ;
+		unsigned int nSmallIntReg: 16  ;
+		unsigned int nIntReg: 32  ;
+		Register aReg[RING_VM_BC_ITEMS_COUNT]  ;
 	} ByteCode ;
 	typedef struct VM {
 		RingState *pRingState  ;
