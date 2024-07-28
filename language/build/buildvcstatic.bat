7f1fc31e141be6b1b01f2b7738b9476db4c62c91
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-07-29 01:44:24 +0300

    Update language/build scripts - Use vminfo_e.c

--- language/build/buildvcstatic.bat
+++ language/build/buildvcstatic.bat
@@ -10,7 +10,7 @@ rstring.c rlist.c ritem.c ritems.c rhtable.c general.c state.c scanner.c parser.
 stmt.c expr.c codegen.c vm.c vmerror.c vmeval.c vmthread.c vmexpr.c vmvars.c ^
 vmlists.c vmfuncs.c ringapi.c vmoop.c  ^
 vmtry.c vmstr.c vmjump.c vmrange.c ^
-vmperf.c vmexit.c vmstack.c vmstate.c genlib_e.c math_e.c file_e.c os_e.c list_e.c meta_e.c ^
+vmperf.c vmexit.c vmstack.c vmstate.c genlib_e.c math_e.c file_e.c os_e.c list_e.c meta_e.c vminfo_e.c ^
 ext.c dll_e.c objfile.c -I"..\include" /link /SUBSYSTEM:CONSOLE,"%ringsubsystem%" /STACK:8388608 /OUT:..\..\bin\ring.exe 
 
 del *.obj
