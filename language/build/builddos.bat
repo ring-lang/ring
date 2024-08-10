0f559048aed4b83a4467dd81d22ea3f1a3928358
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-08-10 09:51:12 +0300

    Update Batch File: language/build/builddos.bat

--- language/build/builddos.bat
+++ language/build/builddos.bat
@@ -7,11 +7,13 @@ set __oinc=%include%
 set include=C:\WATCOM\h;..\include
 set watcom=C:\WATCOM\.
 rename dll_e.c dll_e.no
+rename vmthread.c vmthread.no
 rename ringw.c ringw.no
 wcl386 ring.c *.c
 wcl386 ring.obj *.obj
 rename dll_e.no dll_e.c
 rename ringw.no ringw.c
+rename vmthread.no vmthread.c
 set include=%__oinc%
 set __oinc=
 set path=%__opath%
