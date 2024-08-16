e8d163b07c6d2f098d5a0820ab54157b76c951b3
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-08-16 15:45:01 +0300

    Update Visual Source (Using PWCT) - language/src/scanner.c - Revise comments.

--- language/src/scanner.c
+++ language/src/scanner.c
@@ -619,7 +619,7 @@ void ring_scanner_addreturn2 ( RingState *pRingState )
 void ring_scanner_addreturn3 ( RingState *pRingState, int aPara[2] )
 {
 	List *pList  ;
-	/* Add Return NULL */
+	/* Push Empty String */
 	pList = ring_list_newlist_gc(pRingState,pRingState->pRingGenCode);
 	ring_list_addint_gc(pRingState,pList,ICO_PUSHC);
 	ring_list_addstring_gc(pRingState,pList,RING_CSTR_EMPTY);
