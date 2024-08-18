76300e0b124f0f9148ca1d62ab18b8c951828192
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-08-18 17:57:03 +0300

    Update RingRayLib 5 - buildgcc.sh and buildgccmac.sh

--- extensions/ringraylib5/src/buildgccmac.sh
+++ extensions/ringraylib5/src/buildgccmac.sh
@@ -1,5 +1,5 @@
-gcc -c -fpic -O2 ring_raylib.c -I $PWD/../../../language/include -I macOS_raylib-2.5/include
-gcc -shared -o $PWD/../../../lib/libringraylib.dylib ring_raylib.o -L $PWD/../../../lib -lring -L $PWD/macOS_raylib-2.5/lib -lraylib
+gcc -c -fpic -O2 ring_raylib.c -I $PWD/../../../language/include -I macOS_raylib-5/include
+gcc -shared -o $PWD/../../../lib/libringraylib.dylib ring_raylib.o -L $PWD/../../../lib -lring -L $PWD/macOS_raylib-5/lib -lraylib
 
  
 
