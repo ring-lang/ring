6733dafc03850f9e98c976232d9a836fa362c448
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-08-19 16:36:18 +0300

    Update build/buildvc.bat - Path for RayLib 5 Dlls

--- build/buildvc.bat
+++ build/buildvc.bat
@@ -257,7 +257,7 @@ copy ..\extensions\libdepwin\openssl\bin\*.dll .
 copy ..\extensions\libdepwin\Allegro5.2.8\bin\*.dll .
 copy ..\extensions\libdepwin\freeglut\bin\*.dll .
 copy ..\extensions\libdepwin\glew\bin\Release\win32\*.dll .
-copy ..\extensions\libdepwin\raylib5\bin\*.dll .
+copy ..\extensions\libdepwin\raylib5\lib\*.dll .
 copy ..\extensions\libdepwin\libuishared\*.dll .
 copy ..\extensions\libdepwin\tilengine\lib\*.dll .
 copy ..\extensions\libdepwin\libsdl\SDL2_vc\lib\x86\*.dll .
