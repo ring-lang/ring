fa96ca614ca2faad0e3e72df93a45170ce0f7f6e
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-10-07 14:15:50 +0300

    Update Visual Source (Using PWCT) - language/tests/tryccode/ring_test.ssf - Take in mind changing the visual source file path.

--- language/tests/tryccode/test.c
+++ language/tests/tryccode/test.c
@@ -1,5 +1,4 @@
 /* Copyright (c) 2013-2024 Mahmoud Fayed <msfclipper@yahoo.com> */
-
 #include "ring.h"
 #include "test.h"
 
--- language/tests/tryccode/test.h
+++ language/tests/tryccode/test.h
@@ -1,5 +1,4 @@
 /* Copyright (c) 2013-2024 Mahmoud Fayed <msfclipper@yahoo.com> */
-
 #ifndef ring_test_h
 	#define ring_test_h
 
