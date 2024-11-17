7f715538160e7eb1adbf013958b6cd91a4457d41
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-11-17 08:17:58 +0300

    Add: marketing/research/benchmarks/fibdp_1000

---
+++ marketing/research/benchmarks/fibdp_1000.py
@@ -0,0 +1,26 @@
+import time
+
+def Fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    aFibRes = {0: 0, 1: 1}
+    if n > 1:
+        for t in range(2, n + 1):
+            aFibRes[t] = aFibRes[t - 1] + aFibRes[t - 2]
+        return aFibRes[n]
+
+
+# Start the clock
+t1 = time.time()
+
+# Call the Fibonacci function
+print(Fibonacci(1000))
+
+# Stop the clock
+t2 = time.time()
+
+# Print the elapsed time
+print(t2 - t1)
+
