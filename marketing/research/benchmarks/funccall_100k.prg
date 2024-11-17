617f952906500e93c1cb42ff1997fb95acbe4332
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-11-17 08:12:12 +0300

    Add: marketing/research/benchmarks/funccall_100k

---
+++ marketing/research/benchmarks/funccall_100k.prg
@@ -0,0 +1,11 @@
+CLEAR
+SET DECIMALS TO 3
+t1=seconds()
+for t=1 to 100000 
+    test() 
+next
+t2 = seconds()
+? t2-t1
+
+function test() 
+RETURN 0
---
+++ marketing/research/benchmarks/funccall_100k.py
@@ -0,0 +1,18 @@
+import time
+
+# Define the test function
+def test():
+    return 0
+
+# Start the clock
+t1 = time.time()
+
+# Run the loop and call the test function
+for t in range(1, 100_001):
+    test()
+
+# Stop the clock
+t2 = time.time()
+
+# Print the elapsed time
+print(t2 - t1)
