206c030318624f6a9eadbf2cec15653c54f1551b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-11-17 08:16:41 +0300

    Add: marketing/research/benchmarks/listfill_1m

---
+++ marketing/research/benchmarks/listfill_1m.py
@@ -0,0 +1,20 @@
+import time
+
+# Define the size
+size = 1_000_000
+
+# Create a list with the specified size
+aList = [0] * size
+
+# Start the clock
+t1 = time.time()
+
+# Fill the list
+for t in range(0, size):
+    aList[t] = t
+
+# Stop the clock
+t2 = time.time()
+
+# Print the elapsed time
+print(t2 - t1)
