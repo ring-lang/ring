606d704471fac2c087445d70bb8cdd46878b04b4
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-11-17 08:14:21 +0300

    Add: marketing/research/benchmarks/fibrec_20

---
+++ marketing/research/benchmarks/fibrec_20.py
@@ -0,0 +1,19 @@
+import time
+
+def fib(n):
+    if n <= 2:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+# Start the clock
+t1 = time.time()
+
+# Call the Fibonacci function
+print(fib(20))
+
+# Stop the clock
+t2 = time.time()
+
+# Print the elapsed time
+print(t2 - t1)
+
