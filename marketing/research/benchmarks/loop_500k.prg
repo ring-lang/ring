5d633fe00b86c05c4d9618eceff6d5b9c8ddd0e5
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2024-11-17 08:08:22 +0300

    Add: marketing/research/benchmarks/loop_500k

---
+++ marketing/research/benchmarks/loop_500k.prg
@@ -0,0 +1,11 @@
+Clear
+SET DECIMALS TO 3
+
+t1 = seconds()
+
+for t=1 to 500000 
+next
+
+t2 = seconds()
+
+? t2-t1
---
+++ marketing/research/benchmarks/loop_500k.py
@@ -0,0 +1,14 @@
+import time
+
+# Start the clock
+t1 = time.time()
+
+# Run the loop
+for t in range(1, 500001):
+    pass
+
+# Stop the clock
+t2 = time.time()
+
+# Print the elapsed time
+print("Time: " + str(t2 - t1))
