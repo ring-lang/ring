7f132b8799e4d43b4b98bc00d239d96e8244d145
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-05-15 02:05:19 +0300

    Update extensions/ringfastpro/buildvc.bat and extensions/ringfastpro/buildvc_x64.bat - Revise scripts.

--- extensions/ringfastpro/buildvc_x64.bat
+++ extensions/ringfastpro/buildvc_x64.bat
@@ -1,7 +1,7 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/build/locatevc.bat x64
-cl /c /O2 fastpro.c -I"..\..\language\include"
-link fastpro.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_fastpro.dll
+cl /c %ringcflags% fastpro.c -I"..\..\language\include"
+link %ringldflags% fastpro.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_fastpro.dll
 del fastpro.obj
 endlocal
