bdf2f47e5ceafaababe37b09aa5e1c24393a2f14
ysdragon <youssefelkholey@gmail.com>
2025-05-27 19:40:50 +0300

    Update extensions/ringsdl/buildgcc.sh - Add missing required SDL2 libraries

--- extensions/ringsdl/buildgcc.sh
+++ extensions/ringsdl/buildgcc.sh
@@ -1,6 +1,3 @@
 gcc -c -fpic -O2 ring_libsdl.c -I $PWD/../../language/include -I /usr/include/SDL2
-gcc -shared -o $PWD/../../lib/libringsdl.so ring_libsdl.o -L $PWD/../../lib -lring `pkg-config --cflags --libs sdl2`
-
- 
-
-
+gcc -shared -o $PWD/../../lib/libringsdl.so ring_libsdl.o -L $PWD/../../lib -lring `pkg-config --cflags --libs sdl2 SDL2_ttf SDL2_image SDL2_mixer SDL2_net SDL2_gfx`
+[ -f ring_libsdl.o ] && rm ring_libsdl.o
