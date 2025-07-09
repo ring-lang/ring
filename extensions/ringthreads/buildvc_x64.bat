fbcf00b42b56cc083b0d4d77e7b9dc2745035bc1
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-07-10 01:00:58 +0300

    Support RingThreads when using Ring for Windows 64bit

--- extensions/ringthreads/buildvc_x64.bat
+++ extensions/ringthreads/buildvc_x64.bat
@@ -1,7 +1,7 @@
 cls
 setlocal enableextensions enabledelayedexpansion
 call ../../language/build/locatevc.bat auto x64
-cl /nologo /W2 /O2 /c /DEBUG ring_threads.c -I"..\..\language\include"
-link /DEBUG ring_threads.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_threads.dll 
+cl /nologo /W2 /c ring_threads.c -I"..\..\language\include"
+link ring_threads.obj  ..\..\lib\ring.lib /DLL /OUT:..\..\bin\ring_threads.dll 
 del ring_threads.obj
 endlocal
