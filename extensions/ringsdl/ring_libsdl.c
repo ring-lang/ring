8fc2778c404c0b25e07e6117225337f0af21dbad
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-07-24 14:51:15 +0300

    Update RingLibSDL - generate extensions/ringsdl/ring_libsdl.c using latest configuration file.

--- extensions/ringsdl/ring_libsdl.c
+++ extensions/ringsdl/ring_libsdl.c
@@ -2,7 +2,7 @@
 
 /* Copyright (c) 2013-2020 Mahmoud Fayed <msfclipper@yahoo.com> */
 #include "SDL.h"
-#include "SDL_image.h"
+#include "SDL_image.h"
 #include "SDL_ttf.h"
 #include "SDL_mixer.h"
 #include "SDL_net.h"
@@ -17875,6 +17875,22 @@ RING_FUNC(ring_circleRGBA)
 }
 
 
+static void * sdl_mutex_create(void) {
+	return SDL_CreateMutex();
+}
+
+static void sdl_mutex_lock(void *mutex) {
+	SDL_LockMutex((SDL_mutex *)mutex);
+}
+
+static void sdl_mutex_unlock(void *mutex) {
+	SDL_UnlockMutex((SDL_mutex *)mutex);
+}
+
+static void sdl_mutex_destroy(void *mutex) {
+	SDL_DestroyMutex((SDL_mutex *)mutex);
+}
+
 int SDL_Thread_Function(void *pPointer) {
 	VM *pVM;
 	List *pList;
@@ -17898,8 +17914,8 @@ return ;
 	pList = ring_list_new(0) ;
 	ring_list_addpointer(pList,pPointer);
 	ring_list_addstring(pList,RING_API_GETSTRING(1));
-	ring_vm_mutexfunctions((VM *) pPointer,SDL_CreateMutex,
-SDL_LockMutex,SDL_UnlockMutex,SDL_DestroyMutex);
+	ring_vm_mutexfunctions((VM *) pPointer, sdl_mutex_create,
+	sdl_mutex_lock, sdl_mutex_unlock, sdl_mutex_destroy);
 	SDL_CreateThread(SDL_Thread_Function, RING_API_GETSTRING(2), (void *) pList);
 	
 }
