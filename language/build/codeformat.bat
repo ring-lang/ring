03754236f20655b186a3b86244f559481735cc6d
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-08-05 14:44:02 +0300

    Update language/build/codeformat.bat - Use: IndentPPDirectives

--- language/build/codeformat.bat
+++ language/build/codeformat.bat
@@ -1,4 +1,4 @@
 rem Just if you want to format the C code.
 
-clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8}" ..\src\*.c -i
-clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8}" ..\include\*.h -i
+clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash}" ..\src\*.c -i
+clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash}" ..\include\*.h -i
