25f24a421c9983d7be17c3f37e1a580f54892f28
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-08-05 15:14:07 +0300

    Update language/build/codeformat.bat - Use: SortIncludes

--- language/build/codeformat.bat
+++ language/build/codeformat.bat
@@ -1,4 +1,4 @@
 rem Just if you want to format the C code.
 
-clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash}" ..\src\*.c -i
-clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash}" ..\include\*.h -i
+clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash, SortIncludes: Never}" ..\src\*.c -i
+clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash, SortIncludes: Never}" ..\include\*.h -i
