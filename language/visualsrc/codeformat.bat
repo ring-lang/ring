fcee3f095ef547623e21ea1fa46cd162fb8ecc1b
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-08-06 13:51:49 +0300

    Update language/visualsrc/codeformat.bat - Call: commentsnl.ring

--- language/visualsrc/codeformat.bat
+++ language/visualsrc/codeformat.bat
@@ -1,4 +1,4 @@
-rem Just if you want to format the C code.
+ring commentsnl.ring
 
 clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash, SortIncludes: Never}" ..\src\*.c -i
 clang-format -style="{ColumnLimit: 120, UseTab: Always, IndentWidth: 8, TabWidth: 8, IndentPPDirectives: BeforeHash, SortIncludes: Never}" ..\include\*.h -i
