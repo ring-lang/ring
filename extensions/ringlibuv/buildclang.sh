39b7cf6499d691e0b4e7ed86a2bc7fa55be89245
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-08-07 03:26:27 +0300

    Update extensions/ringlibuv/buildclang.sh and extensions/ringodbc/buildclang.sh - Use homebrew prefix

--- extensions/ringlibuv/buildclang.sh
+++ extensions/ringlibuv/buildclang.sh
@@ -1,7 +1,7 @@
 BREW_PREFIX=$(brew --prefix)
 
 clang -c -fpic -O2 ring_libuv.c -I $PWD/../../language/include -I $BREW_PREFIX/include
-clang -dynamiclib -o $PWD/../../lib/libring_uv.dylib ring_libuv.o  -L $PWD/../../lib  -lring -luv
+clang -dynamiclib -o $PWD/../../lib/libring_uv.dylib ring_libuv.o  -L $PWD/../../lib -L $BREW_PREFIX/lib  -lring -luv
 
  
 
--- extensions/ringodbc/buildclang.sh
+++ extensions/ringodbc/buildclang.sh
@@ -1,7 +1,7 @@
 BREW_PREFIX=$(brew --prefix)
 
 clang -c -fpic -O2 ring_vmodbc.c -I $PWD/../../language/include -I $BREW_PREFIX/include
-clang -dynamiclib -o $PWD/../../lib/libring_odbc.dylib ring_vmodbc.o  -L $PWD/../../lib  -lring -lodbc 
+clang -dynamiclib -o $PWD/../../lib/libring_odbc.dylib ring_vmodbc.o  -L $PWD/../../lib -L $BREW_PREFIX/lib -lring -lodbc 
 
  
 
