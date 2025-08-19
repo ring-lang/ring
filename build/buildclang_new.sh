40965387adf851d4f5f03aa7774c65f33075a544
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2025-08-19 05:28:01 +0300

    Update buildclang_new.sh - Hide output.

--- build/buildclang_new.sh
+++ build/buildclang_new.sh
@@ -5,7 +5,7 @@ cd ..
 echo "Building Ring for macOS..."
 echo "Building Ring Compiler/VM..."
 cd language/build
-sudo ./buildclang.sh > /dev/null
+sudo ./buildclang.sh > /dev/null 2>&1
 cd ../..
 
 cd extensions
@@ -13,149 +13,149 @@ cd extensions
 echo "Building Extensions..."
 echo "Building RingAllegro..."
 cd ringallegro
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingFreeGLUT..."
 cd ../ringfreeglut
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingMurmurHash..."
 cd ../ringmurmurhash
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingQt (Core)..."
 cd ../ringqt
-./gencode_core.sh > /dev/null
-./buildclang_core.sh > /dev/null
+./gencode_core.sh > /dev/null 2>&1
+./buildclang_core.sh > /dev/null 2>&1
 
 echo "Building RingQt (LightGUILib)..."
-./gencode_light.sh > /dev/null
-./buildclang_light.sh > /dev/null
+./gencode_light.sh > /dev/null 2>&1
+./buildclang_light.sh > /dev/null 2>&1
 
 echo "Building RingQt (GUILib)..."
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingStbimage..."
 cd ../ringstbimage
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingZip..."
 cd ../ringzip
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingHTTPLib..."
 cd ../ringhttplib
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingMySQL..."
 cd ../ringmysql
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingRayLib..."
 cd ../ringraylib5/src
-./gencode.sh > /dev/null
-./buildgccmac.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildgccmac.sh > /dev/null 2>&1
 
 echo "Building RingThreads..."
 cd ../../ringthreads
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingCJSON..."
 cd ../ringcjson
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingInternet..."
 cd ../ringinternet
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingODBC..."
 cd ../ringodbc
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingRogueUtil..."
 cd ../ringrogueutil
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingConsoleColors..."
 cd ../ringconsolecolors
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingLibUI..."
 cd ../ringlibui
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingOpenGL..."
 cd ../ringopengl/opengl21
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingCurl..."
 cd ../../ringcurl
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingLibuv..."
 cd ../ringlibuv
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingOpenSSL..."
 cd ../ringopenssl
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingSockets..."
 cd ../ringsockets
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingPDFGen..."
 cd ../ringpdfgen
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingFastPro..."
 cd ../ringfastpro
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingPostgreSQL..."
 cd ../ringpostgresql
-./gencode.sh > /dev/null
-./buildclang.sh > /dev/null
+./gencode.sh > /dev/null 2>&1
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building RingSQLite..."
 cd ../ringsqlite
-./buildclang.sh > /dev/null
+./buildclang.sh > /dev/null 2>&1
 
 echo "Building Ring2EXE..."
 cd ../../tools/ring2exe
-./build.sh > /dev/null
+./build.sh > /dev/null 2>&1
 
 echo "Building RingPM..."
 cd ../ringpm
-./build.sh > /dev/null
+./build.sh > /dev/null 2>&1
 
 echo "Building RingREPL..."
 cd ../ringrepl
-./build.sh > /dev/null
+./build.sh > /dev/null 2>&1
 
 echo "Building Folder2QRC..."
 cd ../folder2qrc
-./build.sh > /dev/null
+./build.sh > /dev/null 2>&1
 
 echo "Installing Ring..."
 cd ../../bin
-./install.sh > /dev/null
+./install.sh > /dev/null 2>&1
 
 echo "Building Ring Tests..."
 cd ../language/tests
-./build.sh > /dev/null
+./build.sh > /dev/null 2>&1
