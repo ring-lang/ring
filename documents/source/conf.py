d4f66f3d9cdbd27ee4b8f270e0d1fecda4539ab6
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2026-02-28 08:32:59 +0300

    Update Documentation - Remove sphinx_rtd_dark_mode

--- documents/source/conf.py
+++ documents/source/conf.py
@@ -38,8 +38,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     "sphinx_rtd_theme",
-    "sphinx_copybutton",
-    "sphinx_rtd_dark_mode"
+    "sphinx_copybutton"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
