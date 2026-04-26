1fefe029d1a36d5afa5f854184070fbe393d1409
Mahmoud Samir Fayed <msfclipper@yahoo.com>
2026-04-26 14:30:52 +0300

    Update Visual Source (Using PWCT) - language/visualsrc/ring_math_extension.ssf - Revise: unsigned() function - Check variable size before the shift operation

--- language/src/math_e.c
+++ language/src/math_e.c
@@ -297,8 +297,16 @@ void ring_vm_math_unsigned(void *pPointer) {
 	}
 	/* Processing */
 	if (strcmp(cStr, ">>") == 0) {
+		if (nNum2 >= sizeof(RING_UNSIGNEDLONGLONG)) {
+			RING_API_ERROR(RING_VM_ERROR_VALUEERROR);
+			return;
+		}
 		nNum3 = nNum1 >> nNum2;
 	} else if (strcmp(cStr, "<<") == 0) {
+		if (nNum2 >= sizeof(RING_UNSIGNEDLONGLONG)) {
+			RING_API_ERROR(RING_VM_ERROR_VALUEERROR);
+			return;
+		}
 		nNum3 = nNum1 << nNum2;
 	} else if (strcmp(cStr, "+") == 0) {
 		nNum3 = nNum1 + nNum2;
